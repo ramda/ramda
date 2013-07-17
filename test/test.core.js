83d881ffa87eae024ed763bc65db9b05b0e07b2b
Scott Sauyet <scott@sauyet.com>
2013-07-17 17:35:21 -0400

    Adding `countBy`, `sortBy`, and `groupBy`, as well as `sort` and `comparator`. Minor cleanup as well (currying `prop`, `props`, and `func`, adding `identity`)

--- test/test.core.js
+++ test/test.core.js
@@ -61,3 +61,30 @@ describe('size', function() {
     });
 });
 
+describe('sort', function() {
+    var sort = Lib.sort;
+
+    it('sorts the elements of a list', function() {
+        assert.deepEqual(sort(function(a, b) {return a - b;}, [3, 1, 8, 1, 2, 5]), [1, 1, 2, 3, 5, 8]);
+    });
+
+    it('does not affect the list passed supplied', function() {
+        var list = [3, 1, 8, 1, 2, 5];
+        assert.deepEqual(sort(function(a, b) {return a - b;}, list), [1, 1, 2, 3, 5, 8]);
+        assert.deepEqual(list, [3, 1, 8, 1, 2, 5]);
+    });
+
+    it('is automatically curried', function() {
+        var sortByLength = sort(function(a, b) {return a.length - b.length;});
+        assert.deepEqual(sortByLength(["one", "two", "three", "four", "five", "six"]),
+                                      ["one", "two", "six", "four", "five", "three"]);
+    });
+});
+
+describe('comparator', function() {
+    var comparator = Lib.comparator;
+    it('builds a comparator function for sorting out of a simple predicate that reports whether the first param is smaller', function() {
+        assert.deepEqual([3, 1, 8, 1, 2, 5].sort(comparator(function(a, b) {return a < b;})), [1, 1, 2, 3, 5, 8]);
+    });
+});
+
--- test/test.sql.js
+++ test/test.sql.js
@@ -25,3 +25,127 @@ describe('project', function() {
     });
 });
 
+(function() {
+    var albums = [
+        {title: 'Art of the Fugue', artist: 'Glenn Gould', genre: 'Baroque'},
+        {title: 'A Farewell to Kings', artist: 'Rush', genre: 'Rock'},
+        {title: 'Timeout', artist: 'Dave Brubeck Quartet', genre: 'Jazz'},
+        {title: 'Fly By Night', artist: 'Rush', genre: 'Rock'},
+        {title: 'Goldberg Variations', artist: 'Daniel Barenboim', genre: 'Baroque'},
+        {title: 'New World Symphony', artist: 'Leonard Bernstein', genre: 'Romantic'},
+        {title: 'Romance with the Unseen', artist: 'Don Byron', genre: 'Jazz'},
+        {title: 'Somewhere In Time', artist: 'Iron Maiden', genre: 'Metal'},
+        {title: 'In Times of Desparation', artist: 'Danny Holt', genre: 'Modern'},
+        {title: 'Evita', artist: 'Various', genre: 'Broadway'},
+        {title: 'Five Leaves Left', artist: 'Nick Drake', genre: 'Folk'},
+        {title: 'The Magic Flute', artist: 'John Eliot Gardiner', genre: 'Classical'}
+    ];
+    var prop = Lib.prop;
+    var derivedGenre = (function() {
+        var remap = {
+            Baroque: 'Classical',
+            Modern: 'Classical',
+            Romantic: 'Classical',
+            Metal: 'Rock'  /*, etc */
+        };
+        return function(album) {
+            var genre = prop("genre", album);
+            return remap[genre] || genre;
+        };
+    }());
+
+    describe('sortBy', function() {
+        var sortBy = Lib.sortBy;
+
+        it('should sort by a simple property of the objects', function() {
+            var sortedAlbums = sortBy(prop("title"), albums);
+            assert.equal(sortedAlbums.length, albums.length);
+            assert.equal(sortedAlbums[0].title, "A Farewell to Kings");
+            assert.equal(sortedAlbums[11].title, "Timeout");
+        });
+
+        it('should be automatically curried', function() {
+            var sorter = sortBy(prop("title"));
+            var sortedAlbums = sorter(albums);
+            assert.equal(sortedAlbums.length, albums.length);
+            assert.equal(sortedAlbums[0].title, "A Farewell to Kings");
+            assert.equal(sortedAlbums[11].title, "Timeout");
+        });
+    });
+    describe('countBy', function() {
+        var countBy = Lib.countBy;
+
+        it('should count by a simple property of the objects', function() {
+            assert.deepEqual(countBy(prop("genre"), albums), {
+                Baroque: 2, Rock: 2, Jazz: 2, Romantic: 1, Metal: 1, Modern: 1, Broadway: 1, Folk: 1, Classical: 1
+            });
+        });
+
+        it('should count by a more complex function on the objects', function() {
+            assert.deepEqual(countBy(derivedGenre, albums), {
+                Classical: 5, Rock: 3, Jazz: 2, Broadway: 1, Folk: 1
+            });
+        });
+
+        it('should be automatically curried', function() {
+            var counter = countBy(prop("genre"));
+            assert.deepEqual(counter(albums), {
+                Baroque: 2, Rock: 2, Jazz: 2, Romantic: 1, Metal: 1, Modern: 1, Broadway: 1, Folk: 1, Classical: 1
+            });
+        });
+    });
+
+    describe('groupBy', function() {
+        var groupBy = Lib.groupBy;
+        it('should group by a simple property of the objects', function() {
+            assert.deepEqual(groupBy(prop("genre"), albums), {
+                Baroque: [{title: 'Art of the Fugue', artist: 'Glenn Gould', genre: 'Baroque'}, {title: 'Goldberg Variations', artist: 'Daniel Barenboim', genre: 'Baroque'}],
+                Rock: [{title: 'A Farewell to Kings', artist: 'Rush', genre: 'Rock'}, {title: 'Fly By Night', artist: 'Rush', genre: 'Rock'}],
+                Jazz: [{title: 'Timeout', artist: 'Dave Brubeck Quartet', genre: 'Jazz'}, {title: 'Romance with the Unseen', artist: 'Don Byron', genre: 'Jazz'}],
+                Romantic: [{title: 'New World Symphony', artist: 'Leonard Bernstein', genre: 'Romantic'}],
+                Metal: [{title: 'Somewhere In Time', artist: 'Iron Maiden', genre: 'Metal'}],
+                Modern: [{title: 'In Times of Desparation', artist: 'Danny Holt', genre: 'Modern'}],
+                Broadway: [{title: 'Evita', artist: 'Various', genre: 'Broadway'}],
+                Folk: [{title: 'Five Leaves Left', artist: 'Nick Drake', genre: 'Folk'}],
+                Classical: [{title: 'The Magic Flute', artist: 'John Eliot Gardiner', genre: 'Classical'}]
+            });
+        });
+
+        it('should group by a more complex function on the objects', function() {
+            assert.deepEqual(groupBy(derivedGenre, albums), {
+                Classical: [
+                    {title: 'Art of the Fugue', artist: 'Glenn Gould', genre: 'Baroque'},
+                    {title: 'Goldberg Variations', artist: 'Daniel Barenboim', genre: 'Baroque'},
+                    {title: 'New World Symphony', artist: 'Leonard Bernstein', genre: 'Romantic'},
+                    {title: 'In Times of Desparation', artist: 'Danny Holt', genre: 'Modern'},
+                    {title: 'The Magic Flute', artist: 'John Eliot Gardiner', genre: 'Classical'}
+                ],
+                Rock: [
+                    {title: 'A Farewell to Kings', artist: 'Rush', genre: 'Rock'},
+                    {title: 'Fly By Night', artist: 'Rush', genre: 'Rock'},
+                    {title: 'Somewhere In Time', artist: 'Iron Maiden', genre: 'Metal'}
+                ],
+                Jazz: [{title: 'Timeout', artist: 'Dave Brubeck Quartet', genre: 'Jazz'}, {title: 'Romance with the Unseen', artist: 'Don Byron', genre: 'Jazz'}],
+                Broadway: [{title: 'Evita', artist: 'Various', genre: 'Broadway'}],
+                Folk: [{title: 'Five Leaves Left', artist: 'Nick Drake', genre: 'Folk'}]
+            });
+        });
+
+        it('should be automatically curried', function() {
+            var grouper = groupBy(prop("genre"));
+            assert.deepEqual(grouper(albums), {
+                Baroque: [{title: 'Art of the Fugue', artist: 'Glenn Gould', genre: 'Baroque'}, {title: 'Goldberg Variations', artist: 'Daniel Barenboim', genre: 'Baroque'}],
+                Rock: [{title: 'A Farewell to Kings', artist: 'Rush', genre: 'Rock'}, {title: 'Fly By Night', artist: 'Rush', genre: 'Rock'}],
+                Jazz: [{title: 'Timeout', artist: 'Dave Brubeck Quartet', genre: 'Jazz'}, {title: 'Romance with the Unseen', artist: 'Don Byron', genre: 'Jazz'}],
+                Romantic: [{title: 'New World Symphony', artist: 'Leonard Bernstein', genre: 'Romantic'}],
+                Metal: [{title: 'Somewhere In Time', artist: 'Iron Maiden', genre: 'Metal'}],
+                Modern: [{title: 'In Times of Desparation', artist: 'Danny Holt', genre: 'Modern'}],
+                Broadway: [{title: 'Evita', artist: 'Various', genre: 'Broadway'}],
+                Folk: [{title: 'Five Leaves Left', artist: 'Nick Drake', genre: 'Folk'}],
+                Classical: [{title: 'The Magic Flute', artist: 'John Eliot Gardiner', genre: 'Classical'}]
+            });
+        });
+
+    });
+}());
+
