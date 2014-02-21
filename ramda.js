320fcb6bf6b0b8f4f5f3525163b21c48e6e6f8ad
Scott Sauyet <scott.sauyet@gmail.com>
2014-02-20 23:03:36 -0500

    Updating documentation to include 'filter.idx' and 'reject.idx'

--- ramda.js
+++ ramda.js
@@ -574,8 +574,7 @@
         // Reports the number of elements in the list
         R.size = function(arr) {return arr.length;};
 
-
-        // Returns a new list containing only those items that match a given predicate function.
+        // (Internal use only) The basic implementation of filter.
         var internalFilter = _(function(useIdx, fn, list) {
             if (list && list.length === Infinity) {
                 return list.filter(fn); // TODO: figure out useIdx
@@ -588,13 +587,36 @@
             }
             return result;
         });
+
+        // Returns a new list containing only those items that match a given predicate function.
         var filter = R.filter = internalFilter(false);
+
+        // Like `filter`, but passes additional parameters to the predicate function.  Parameters are
+        // `list item`, `index of item in list`, `entire list`.
+        //
+        // Example:
+        //
+        //     var lastTwo = function(val, idx, list) {
+        //         return list.length - idx <= 2;
+        //     };
+        //     filter.idx(lastTwo, [8, 6, 7, 5, 3, 0 ,9]); //=> [0, 9]
         filter.idx = internalFilter(true);
 
         // Similar to `filter`, except that it keeps only those that **don't** match the given predicate functions.
         var reject = R.reject = _(function(fn, list) {
             return filter(notFn(fn), list);
         });
+
+        // Like `reject`, but passes additional parameters to the predicate function.  Parameters are
+        // `list item`, `index of item in list`, `entire list`.
+        //
+        // Example:
+        //
+        //     var lastTwo = function(val, idx, list) {
+        //         return list.length - idx <= 2;
+        //     };
+        //     reject.idx(lastTwo, [8, 6, 7, 5, 3, 0 ,9]);
+        //     //=> [8, 6, 7, 5, 3]
         reject.idx = _(function(fn, list) {
             return filter.idx(notFn(fn), list);
         });
