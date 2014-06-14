a990fbed3c8ad42def915fca92e0de99e665645d
buzzdecafe <m_hur@yahoo.com>
2014-06-14 14:04:02 -0400

    replace elegant  impl with one that performs. sigh.

---
+++ ext/types/Maybe.js
@@ -0,0 +1,52 @@
+var isNil = require('./util').isNil;
+
+function Maybe(x) {
+  if (!(this instanceof Maybe)) {
+    return new Maybe(x);
+  }
+  this.value = x;
+}
+
+Maybe.of = function(x) {
+  return new Maybe(x);
+};
+
+// functor
+Maybe.prototype.map = function(f) {
+  return isNil(this.value) ? this : new Maybe(f(this.value));
+};
+
+// apply
+// takes a Maybe that wraps a function (`app`) and applies its `map`
+// method to this Maybe's value, which must be a function.
+Maybe.prototype.ap = function(m) {
+  if (typeof this.value !== 'function') {
+    throw new TypeError("Calling ap on a Maybe requires that the Maybe is wrapping a function");
+  }
+  return m.map(this.value);
+};
+
+// applicative
+Maybe.prototype.of = Maybe.of;
+
+// chain
+//  f must be a function which returns a value
+//  f must return a value of the same Chain
+//  chain must return a value of the same Chain
+//
+Maybe.prototype.chain = function(f) {
+   return this.value ? f(this.value) : this;
+};
+
+// monad
+// A value that implements the Monad specification must also implement the Applicative and Chain specifications.
+// see above.
+
+// equality method to enable testing
+Maybe.prototype.equals = function(that) {
+  return this.value === that.value;
+};
+
+module.exports = Maybe;
+
+
---
+++ ext/types/util.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  isNil: function(x) { 
+    return x === null || x === undefined || x !== x; // crazy NaN check
+  }
+};
+
+
