fd542e64cb5ea013ba046c7a3d64b1a5e6a3cea3
cgarciae <cgarcia.e88@gmail.com>
2014-06-21 13:17:14 -0500

    Corrected length definition in _slice

--- ramda.js
+++ ramda.js
@@ -42,7 +42,7 @@
             from = from || 0;
             to = to || args.length;
 
-            arr = new Array (from - to);
+            arr = new Array (to - from);
 
             i = from - 1;
             while (++i < to) {
