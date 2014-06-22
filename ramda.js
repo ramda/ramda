616d099f671600072080fd21d371a88107c630ae
megawac <yeatesgraeme@gmail.com>
2014-06-21 22:48:32 -0400

    predicateWrap: Slight optimization and improved comments

--- ramda.js
+++ ramda.js
@@ -1131,16 +1131,19 @@
 
 
         // Create a predicate wrapper which will call a pick function (all/any) for each predicate
-        var predicateWrap = function(method) {
+        var predicateWrap = function(predPicker) {
             return function(preds /* , args */) {
-                var args = _slice(arguments, 1);
-                var iterator = function() {
+                var predIterator = function() {
                     var args = arguments;
-                    return method(function(predicate) {
+                    return predPicker(function(predicate) {
                         return predicate.apply(null, args);
                     }, preds);
                 };
-                return args.length ? iterator.apply(null, args) : arity(max(pluck("length", preds)), iterator);
+                return arguments.length > 1 ?
+                        // Call function imediately if given arguments
+                        predIterator.apply(null, _slice(arguments, 1)) :
+                        // Return a function which will call the predicates with the provided arguments
+                        arity(max(pluck("length", preds)), predIterator);
             };
         };
 
