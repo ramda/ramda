8d6b311d024679f9a7100559ddc17e2a92d66388
buzzdecafe <m_hur@yahoo.com>
2014-06-26 10:05:26 -0400

    rename allKeys, allValues to keysIn, valuesIn

--- test/test.keysvals.js
+++ test/test.keysvals.js
@@ -17,18 +17,18 @@ describe("keys", function() {
     });
 });
 
-describe("allKeys", function() {
-    var keys = Lib.allKeys;
+describe("keysIn", function() {
+    var keysIn = Lib.keysIn;
     var obj = {a: 100, b: [1,2,3], c: { x: 200, y: 300}, d: "D", e: null, f: (function(){}())};
     function C() { this.a = 100; this.b = 200; }
     C.prototype.x = function() { return "x"; };
     C.prototype.y = "y";
     var cobj = new C();
     it("returns an array of the given object's keys", function() {
-        assert.deepEqual(keys(obj), ['a','b','c','d','e','f']);
+        assert.deepEqual(keysIn(obj), ['a','b','c','d','e','f']);
     });
     it("includes the given object's prototype properties", function() {
-        assert.deepEqual(keys(cobj), ['a', 'b', 'x', 'y']);
+        assert.deepEqual(keysIn(cobj), ['a', 'b', 'x', 'y']);
     });
 });
 
@@ -49,18 +49,18 @@ describe("values", function() {
     });
 });
 
-describe("allValues", function() {
-    var allValues = Lib.allValues;
+describe("valuesIn", function() {
+    var valuesIn = Lib.valuesIn;
     var obj = {a: 100, b: [1,2,3], c: { x: 200, y: 300}, d: "D", e: null, f: (function(){}())};
     function C() { this.a = 100; this.b = 200; }
     C.prototype.x = function() { return "x"; };
     C.prototype.y = "y";
     var cobj = new C();
     it("returns an array of the given object's values", function() {
-        assert.deepEqual(allValues(obj), [100,[1,2,3],{x: 200, y: 300},'D',null,undefined]);
+        assert.deepEqual(valuesIn(obj), [100,[1,2,3],{x: 200, y: 300},'D',null,undefined]);
     });
     it("includes the given object's prototype properties", function() {
-        assert.deepEqual(allValues(cobj), [100, 200, C.prototype.x, 'y']);
+        assert.deepEqual(valuesIn(cobj), [100, 200, C.prototype.x, 'y']);
     });
 });
 
