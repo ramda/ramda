241cc431e4e1f0ce7abde22cfe6f14ef8a9d210d
buzzdecafe <m_hur@yahoo.com>
2014-06-29 09:30:32 -0400

    mapObj.idx

--- test/test.map.js
+++ test/test.map.js
@@ -57,3 +57,34 @@ describe('mapObj', function() {
         assert.deepEqual(mapObj(square, obj), {a: 1, b: 4, c: 9});
     })
 });
+
+describe('mapObj.idx', function() {
+    var mapObj = Lib.mapObj;
+    var times2 = function(x) {return x * 2;};
+    var addIdx = function(x, key) {return x + key;};
+    var squareVowels = function(x, key, obj) {
+        var vowels = ['a', 'e', 'i', 'o', 'u'];
+        return Lib.contains(key, vowels) ? x * x : x;
+    };
+
+    it('works just like a normal mapObj', function() {
+        assert.deepEqual(mapObj.idx(times2, {a: 1, b: 2, c: 3, d: 4}), {a: 2, b: 4, c: 6, d: 8});
+    });
+
+    it('passes the index as a second parameter to the callback', function() {
+        assert.deepEqual(mapObj.idx(addIdx, {a: 8, b: 6, c: 7, d: 5, e: 3, f: 0, g: 9}), 
+          {a: '8a', b: '6b', c: '7c', d: '5d', e: '3e', f: '0f', g: '9g'});
+    });
+
+    it('passes the entire list as a third parameter to the callback', function() {
+        assert.deepEqual(mapObj.idx(squareVowels, {a: 8, b: 6, c: 7, d: 5, e: 3, f: 0, g: 9}), 
+          {a: 64, b: 6, c: 7, d: 5, e: 9, f: 0, g: 9});
+    });
+
+    it('is automatically curried', function() {
+        var makeSquareVowels = mapObj.idx(squareVowels);
+        assert.deepEqual(makeSquareVowels({a: 8, b: 6, c: 7, d: 5, e: 3, f: 0, g: 9}), 
+          {a: 64, b: 6, c: 7, d: 5, e: 9, f: 0, g: 9});
+    });
+});
+
