1046ef44c86a4532c346a442ee226d33738959fe
buzzdecafe <mh@buzzdecafe.com>
2014-07-04 15:20:12 +0000

    add lodash as default benchmark to compare

--- bench/map.bench.js
+++ bench/map.bench.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var map = require('../ramda').map;
 var Benchmark = require('benchmark');
 var fs = require('fs');
@@ -8,11 +9,17 @@ var nums = [8,2,85,2,34,3,23,247,57,8,0,6,5,46,54,643];
 function sq(x) { return x * x; }
 var mapSq = map(sq);
 
-suite.add('map(sq, nums)', function() {
+suite
+.add('_.map(nums, sq)', function() {
+  _.map(nums, sq);
+})
+.add('map(sq, nums)', function() {
   map(sq, nums);
-}).add('map(sq)(nums)', function() {
+})
+.add('map(sq)(nums)', function() {
   map(sq)(nums);
-}).add('mapSq(nums)', function() {
+})
+.add('mapSq(nums)', function() {
   mapSq(nums);
 })
 .on('cycle', function(event) {
