1023b657530496c4dd7b74dc70f8978d541a9e75
buzzdecafe <m_hur@yahoo.com>
2014-07-08 20:28:11 -0400

    benchmark task; start orch integration

--- bench/filter-where.bench.js
+++ bench/filter-where.bench.js
@@ -3,10 +3,6 @@ var R = require('../ramda');
 var filter = R.filter;
 var where = R.where;
 var isEmpty = R.isEmpty;
-var Benchmark = require('benchmark');
-var fs = require('fs');
-var out = './report/filter-' + (new Date()).toISOString() + '.json';
-var suite = new Benchmark.Suite;
 
 var objs = [
   {x: [1,2], y: true}, {x: [1,3], y: true}, {x: [], y: false}, {x: [2], y: false}, 
@@ -16,40 +12,33 @@ var objs = [
 var filterEmptyX = filter(where({x: R.isEmpty}));
 var filterFalseY = filter(where({y: false}));
 
-suite.add('_.filter(objs, {x: []})', function() {
-  _.filter(objs, {x: []});
-})
-.add('filter(where({x: isEmpty}), objs)', function() {
-  filter(where({x: isEmpty}), objs);
-})
-.add('filter(where({x: isEmpty}))(objs)', function() {
-  filter(where({x: isEmpty}))(objs);
-})
-.add('filterEmptyX(objs)', function() {
-  filterEmptyX(objs);
-})
-.add('_.filter(objs, {y: false})', function() {
-  _.filter(objs, {y: false});
-})
-.add('filter(where({y: false}), objs)', function() {
-  filter(where({y: false}), objs);
-})
-.add('filter(where({y: false}))(objs)', function() {
-  filter(where({y: false}))(objs);
-})
-.add('filterFalseY(objs)', function() {
-  filterFalseY(objs);
-})
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-  fs.writeFile(out, JSON.stringify(this), function(err) {
-    if (err) { console.log('failed to write ' + out); }
-    else { console.log('saved ' + out); }
-  });
-})
-.run();
-
+module.exports = {
+  name: "filter where",
+  tests: {
+    '_.filter(objs, {x: []})': function() {
+      _.filter(objs, {x: []});
+    },
+    'filter(where({x: isEmpty}), objs)': function() {
+      filter(where({x: isEmpty}), objs);
+    },
+    'filter(where({x: isEmpty}))(objs)': function() {
+      filter(where({x: isEmpty}))(objs);
+    },
+    'filterEmptyX(objs)': function() {
+      filterEmptyX(objs);
+    },
+    '_.filter(objs, {y: false})': function() {
+      _.filter(objs, {y: false});
+    },
+    'filter(where({y: false}), objs)': function() {
+      filter(where({y: false}), objs);
+    },
+    'filter(where({y: false}))(objs)': function() {
+      filter(where({y: false}))(objs);
+    },
+    'filterFalseY(objs)': function() {
+      filterFalseY(objs);
+    }
+  }
+};
 
