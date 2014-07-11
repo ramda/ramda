7581f39dd3898639bb8741ba06f039db01762ab2
Nathan Houle <nathan@nathanhoule.com>
2014-07-11 15:07:57 -0700

    Expand `filter` documentation

--- ramda.js
+++ ramda.js
@@ -721,6 +721,27 @@
             return arr.length;
         };
 
+        /**
+         * Returns a new list containing only those items that match a given predicate function.
+         * The predicate function is passed one argument: *(value)*.
+         *
+         * Note that `ramda.filter` does not skip deleted or unassigned indices, unlike the native
+         * `Array.prototype.filter` method. For more details on this behavior, see:
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Description
+         *
+         * @static
+         * @memberOf R
+         * @category Collection
+         * @param {Function} fn The function called per iteration.
+         * @param {Array} list The collection to iterate over.
+         * @return {Array} Returns the new filtered array.
+         * @example
+         *
+         * var isEven = function(n) {
+         *     return n % 2 === 0;
+         * };
+         * var evens = ramda.filter(isEven, [1, 2, 3, 4]); // => [2, 4]
+         */
         var filter = function(fn, list) {
             if (hasMethod('filter', list)) {
                 return list.filter(fn);
