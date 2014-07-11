9c7af733f32bc77b7c264146ae12be25b19e74ab
Nathan Houle <nathan@nathanhoule.com>
2014-07-11 16:48:43 -0700

    Expand documentation for

--- ramda.js
+++ ramda.js
@@ -910,10 +910,25 @@
         });
 
         /**
-         * XXX
+         * Returns a new list containing the last `n` elements of a given list, passing each value
+         * to the supplied predicate function, beginning when the predicate function returns
+         * `true`. Excludes the element that caused the predicate function to fail. The predicate
+         * function is passed one argument: *(value)*.
+         *
+         * @static
+         * @memberOf R
+         * @category List
+         * @param {Function} fn The function called per iteration.
+         * @param {Array} list The collection to iterate over.
+         * @return {Array} Returns a new array.
+         * @example
+         *
+         * var isNotTwo = function(x) {
+         *   return !(x === 2);
+         * };
+         *
+         * skipUntil(isNotFour, [1, 2, 3, 4]); //=> [1, 2, 3]
          */
-        // Returns a new list containing the elements of the given list starting with the first one where the function
-        // supplied returns `false` when passed the element.
         R.skipUntil = curry2(function (fn, list) {
             var idx = -1, len = list.length;
             while (++idx < len && !fn(list[idx])) {}
