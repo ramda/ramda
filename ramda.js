710f1e74135aece0c2b8175da8ed23abf04aa1ef
Nathan Houle <nathan@nathanhoule.com>
2014-07-11 19:26:15 -0700

    Extend documentation for `map`

--- ramda.js
+++ ramda.js
@@ -895,11 +895,27 @@
         });
 
         /**
-         * XXX
+         * Returns a new list, constructed by applying the supplied function to every element of the
+         * supplied list.
+         *
+         * Note: `ramda.map` does not skip deleted or unassigned indices (sparse arrays), unlike the
+         * native `Array.prototype.filter` method. For more details on this behavior, see:
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Description
+         *
+         * @static
+         * @memberOf R
+         * @category List
+         * @param {Function} fn The function to be called on every element of the input `list`.
+         * @param {Array} list The list to be iterated over.
+         * @return {Array} Returns the new list.
+         * @example
+         *
+         * var double = function(x) {
+         *   return x * 2;
+         * };
+         *
+         * ramda.map(double, [1, 2, 3]); //=> [2, 4, 6]
          */
-        // Returns a new list constructed by applying the function to every element of the list supplied.
-        // n.b.: `ramda.map` differs from `Array.prototype.map` in that it does not distinguish "sparse
-        // arrays" (cf. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Description).
         function map(fn, list) {
             if (hasMethod('map', list)) {
                 return list.map(fn);
