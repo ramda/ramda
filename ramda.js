28aad3d8fa2522657542a5f9fc967e7b67fa8ffe
Nathan Houle <nathan@nathanhoule.com>
2014-07-11 19:09:18 -0700

    Extend docs for `foldr.idx`

--- ramda.js
+++ ramda.js
@@ -828,7 +828,32 @@
         aliasFor("foldr").is("reduceRight");
 
         /**
-         * XXX
+         * Like `foldr`, but passes additional parameters to the predicate function. Moves through
+         * the input list from the right to the left.
+         *
+         * The iterator function receives four values: *(acc, value, index, list)*.
+         *
+         * Note: `ramda.foldr.idx` does not skip deleted or unassigned indices (sparse arrays),
+         * unlike the native `Array.prototype.filter` method. For more details on this behavior,
+         * see:
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Description
+         *
+         * @static
+         * @memberOf R
+         * @category List
+         * @param {Function} fn The iterator function. Receives four values: the accumulator, the
+         * current element from `list`, that element's index, and the entire `list` itself.
+         * @param {*} acc The accumulator value.
+         * @param {Array} list The list to iterate over.
+         * @return {*} The final, accumulated value.
+         * @example
+         *
+         * var letters = ['a', 'b', 'c'];
+         * var objectify = function(accObject, elem, idx, list) {
+         *   return accObject[elem] = idx;
+         * };
+         *
+         * foldr.idx(letters, objectify, {}); //=> { 'c': 2, 'b': 1, 'a': 0 }
          */
         R.foldr.idx = curry3(function (fn, acc, list) {
             if (hasMethod('foldr', list)) {
