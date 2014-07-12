e2b656e4f2d5067c74e1ce4ec19cdf5644d3771e
Nathan Houle <nathan@nathanhoule.com>
2014-07-11 19:20:51 -0700

    WIP extension to `foldr` documentation

--- ramda.js
+++ ramda.js
@@ -867,10 +867,23 @@
         });
 
         /**
-         * XXX
+         * Builds a list from a seed value. Accepts an iterator function, which returns either false
+         * to stop iteration or an array of length 2 containing the value to add to the resulting
+         * list and the seed to be used in the next call to the iterator function.
+         *
+         * The iterator function receives one value: *(seed)*.
+         *
+         * @static
+         * @memberOf R
+         * @category List
+         * @param {Function} fn The iterator function. Receives one value, `seed`, and returns
+         * either false to quit iteration or an array of length two to proceed. The element at index
+         * 0 of this array will be added to the resulting array, and the element at index 1 will be
+         * passed to the next call to `fn`.
+         * @param {*} seed The seed value.
+         * @return {Array} The final list.
+         * @example TODO
          */
-        // Builds a list from a seed value, using a function that returns falsy to quit and a pair otherwise,
-        // consisting of the current value and the seed to be used for the next value.
         R.unfoldr = curry2(function (fn, seed) {
             var pair = fn(seed);
             var result = [];
