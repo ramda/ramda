5407ad0bca5cd437af3e1310c05b7aecd6135055
buzzdecafe <m_hur@yahoo.com>
2014-08-09 20:21:44 -0400

    try this again: unnesst/flatten array-like

--- test/test.flatten.js
+++ test/test.flatten.js
@@ -20,6 +20,11 @@ describe('flatten', function() {
     it("handles ridiculously large inputs", function() {
         assert.equal(flatten([new Array(1000000), R.range(0, 56000), 5, 1, 3]).length, 1056003);
     });
+
+    it("handles array-like objects", function() {
+      var o = { length: 3, "0": [1, 2, [3]], "1": [], "2": ["a", "b", "c", ["d", "e"]] };
+      assert.deepEqual(flatten(o), [1, 2, 3, "a", "b", "c", "d", "e"]);
+    });
 });
 
 describe('unnest', function() {
@@ -38,4 +43,8 @@ describe('unnest', function() {
         assert.notEqual(unnest(nest), nest);
     });
 
+    it("handles array-like objects", function() {
+      var o = { length: 3, "0": [1, 2, [3]], "1": [], "2": ["a", "b", "c", ["d", "e"]]};
+      assert.deepEqual(unnest(o), [1, 2, [3], "a", "b", "c", ["d", "e"]]);
+    });
 });
