a1de7cf670f4914fd414cb0e9b30ba8acb0187e6
David Chambers <dc@davidchambers.me>
2014-08-20 19:05:57 -0700

    throw if R.func is called with no arguments

--- ramda.js
+++ ramda.js
@@ -3160,12 +3160,12 @@
          *
          */
         R.func = function func(fn, obj) {
-            function _func(obj) {
-                return obj[fn].apply(obj, _slice(arguments, 1));
+            switch (arguments.length) {
+                case 0: throw NO_ARGS_EXCEPTION;
+                case 1: return function(obj) { return obj[fn].apply(obj, _slice(arguments, 1)); };
+                case 2: return obj[fn];
+                default: return obj[fn].apply(obj, _slice(arguments, 2));
             }
-            return arguments.length < 2 ? _func : 
-                   arguments.length === 2 ? obj[fn] :
-                   obj[fn].apply(obj, _slice(arguments, 2));
         };
 
 
