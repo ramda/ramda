82b70c9a7cd853a9708a398395e4aa166499724c
buzzdecafe <m_hur@yahoo.com>
2014-09-05 09:12:47 -0400

    wire up browsers for sauce

--- sauce/conf.js
+++ sauce/conf.js
@@ -1,14 +1,11 @@
+var browsers = require('./browsers');
 module.exports  = {
   all: {
     options: {
       urls: ['localhost:3210/test/index.html'],
       build: process.env.CI_BUILD_NUMBER || 0,
       testname: 'Ramda Sauce Unit Test',
-      browsers: [{
-        browserName: 'firefox',
-        version: '19',
-        platform: 'XP'
-      }]
+      browsers: browsers
     }
   }
 }
