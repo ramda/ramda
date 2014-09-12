322be5da753ea583dd22e84b2a02049f2b0fdf3d
buzzdecafe <m_hur@yahoo.com>
2014-09-11 20:46:05 -0400

    fix version in browser config

--- sauce/ios.js
+++ sauce/ios.js
@@ -8,7 +8,7 @@ module.exports = [
   },
   {
     browserName: 'ipad',
-    version: '7.1',
+    version: '4.3',
     deviceName: 'iPad',
     'device-orientation': 'portrait',
     platform: 'OS X 10.6'
