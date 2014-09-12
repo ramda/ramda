7ff2763021b5aeb611d105d549fe98280a3939c9
buzzdecafe <m_hur@yahoo.com>
2014-09-12 19:45:54 -0400

    add cloneObj (shallow)

--- ramda.js
+++ ramda.js
@@ -3786,6 +3786,24 @@
 
 
     /**
+     * Creates a shallow copy of an object's own properties.
+     *
+     * @func
+     * @memberOf R
+     * @category Object
+     * @sig {*} -> {*}
+     * @param {Object} obj The object to clone
+     * @returns {Object} A new object
+     * @example
+     *
+     *     R.cloneObj({a: 1, b: 2, c: [1, 2, 3]}); // {a: 1, b: 2, c: [1, 2, 3]}
+     */
+    R.cloneObj = function(obj) {
+        return extend({}, obj);
+    };
+
+
+    /**
      * Reports whether two functions have the same value for the specified property.  Useful as a curried predicate.
      *
      * @func
--- test/test.clone.js
+++ test/test.clone.js
@@ -14,6 +14,23 @@ describe('clone', function() {
         var o2 = {x: 2};
         var o3 = {x: 3};
         var c = R.clone([o1, o2, o3]);
-        assert.equal(c[0], o1);
+        assert.strictEqual(c[0], o1);
+    });
+});
+
+describe('cloneiObj', function() {
+    it('returns a copy of an object', function() {
+        var input = {a: 1, b: 2, c: 3, z: 100};
+        var output = R.cloneObj(input);
+        assert.deepEqual(output, input);
+        assert.notStrictEqual(output, input);
+    });
+
+    it('copies objects in the array by reference', function() {
+        var o1 = {x: 1};
+        var o2 = {x: 2};
+        var o3 = {x: 3};
+        var c = R.cloneObj({a: o1, b: o2, c: o3});
+        assert.strictEqual(c.a, o1);
     });
 });
