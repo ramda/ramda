ad094f4891953d2fe255d57d67a5ce21303a1dd5
buzzdecafe <m_hur@yahoo.com>
2014-09-20 08:39:18 -0400

    curry Either constructor

--- ext/types/Either.js
+++ ext/types/Either.js
@@ -1,51 +1,44 @@
-(function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(factory);
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
+function Either(left, right) {
+    if (arguments.length === 0) {
+        throw new TypeError('Either called with no arguments');
+    } else if (arguments.length === 1) {
+        return function(right) {
+            return new Either(left, right);
+        };
     } else {
-        // Browser globals (root is window)
-        root.returnExports = factory();
-    }
-}(this, function() {
-    function Either(left, right) {
         if (!(this instanceof Either)) {
             return new Either(left, right);
         }
         this.left = left;
         this.right = right;
     }
+}
 
-    Either.of = function(value, err) {
-        return new Either(err, value);
-    };
+Either.of = function(value, err) {
+    return new Either(err, value);
+};
 
-    Either.prototype.map = function(f) {
-        return this.right == null ? this : new Either(this.left, f(this.right));
-    };
+Either.prototype.map = function(f) {
+    return this.right == null ? this : new Either(this.left, f(this.right));
+};
 
-    Either.prototype.ap = function(app) {
-        return this.right == null ? this : app.map(this.right);
-    };
+Either.prototype.ap = function(app) {
+    return this.right == null ? this : app.map(this.right);
+};
 
-    // `f` must return a new Either; not sure if this impl is sufficient
-    Either.prototype.chain = function(f) {
-        return this.right == null ? this : f(this.right);
-    };
+// `f` must return a new Either; not sure if this impl is sufficient
+Either.prototype.chain = function(f) {
+    return this.right == null ? this : f(this.right);
+};
 
-    Either.prototype.of = Either.of;
+Either.prototype.of = Either.of;
 
-    Either.prototype.equals = function(that) {
-        return this.right === that.right;
-    };
+Either.prototype.equals = function(that) {
+    return this.right === that.right;
+};
 
-    Either.equals = function(e1, e2) {
-        return e1.equals(e2);
-    };
+Either.equals = function(e1, e2) {
+    return e1.equals(e2);
+};
 
-    return Either;
-}));
+module.exports = Either;
