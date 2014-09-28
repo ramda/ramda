6706456d8c239f3c8ed9efc698c691b475bddd1b
buzzdecafe <m_hur@yahoo.com>
2014-09-28 17:52:03 -0400

    port es5-shim trim tests

--- ramda.js
+++ ramda.js
@@ -4846,6 +4846,7 @@
 
     /**
      * Removes whitespace from both ends of the string.
+     *
      * @func
      * @memberOf R
      * @category string
@@ -4857,9 +4858,22 @@
      *      R.trim('   xyz  '); //=> 'xyz'
      *      R.map(R.trim, R.split(',', 'x, y, z')); //=> ['x', 'y', 'z']
      */
-    R.trim = typeof String.prototype.trim === 'function' ?
-        function(str) { return str.trim(); } :
-        function(str) { return str.replace(/^[\s\xA0]+|[\s\xA0]+$/g, ''); };
+    R.trim = (function() {
+        var ws = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003' +
+            '\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028' +
+            '\u2029\uFEFF';
+        var zeroWidth = '\u200b';
+        var hasProtoTrim = (typeof String.prototype.trim === 'function');
+        var hasTrimBug = ws.trim() || !zeroWidth.trim();
+        if (!hasProtoTrim || hasTrimBug) {
+            return function _trim(str) {
+                var beginRx = new RegExp('^[' + ws + '][' + ws + ']*');
+                var endRx = new RegExp('[' + ws + '][' + ws + ']*$');
+                return str.replace(beginRx, '').replace(endRx, '');
+            };
+        }
+        return function trim(str) { return str.trim(str); };
+    }());
 
 
     /**
--- test/test.stringBasics.js
+++ test/test.stringBasics.js
@@ -116,9 +116,31 @@ describe('toLowerCase', function() {
 });
 
 describe('trim', function() {
+    var test = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFFHello, World!\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+
     it('should trim a string', function() {
         assert.equal(R.trim('   xyz  '), 'xyz');
     });
+
+    it('trims all ES5 whitespace', function() {
+        assert.equal(R.trim(test), 'Hello, World!');
+        assert.equal(R.trim(test).length, 13);
+    });
+
+    it('does not trim the zero-width space', function() {
+        assert.equal(R.trim('\u200b'), '\u200b');
+        assert.equal(R.trim('\u200b').length, 1);
+    });
+
+    if (typeof String.prototype.trim !== 'function') {
+        it('falls back to a shim if String.prototype.trim is not present', function() {
+            assert.equal(R.trim('   xyz  '), 'xyz');
+            assert.equal(R.trim(test), 'Hello, World!');
+            assert.equal(R.trim(test).length, 13);
+            assert.equal(R.trim('\u200b'), '\u200b');
+            assert.equal(R.trim('\u200b').length, 1);
+        });
+    }
 });
 
 describe('split', function() {
