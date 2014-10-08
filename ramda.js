1014d876a1dedfdd9e0ae2c00cd06b3ea6277daf
buzzdecafe <m_hur@yahoo.com>
2014-10-08 18:39:33 -0400

    generic n-ary lift and liftN

--- ramda.js
+++ ramda.js
@@ -1886,6 +1886,66 @@
 
 
     /**
+     * "lifts" a function to be the specified arity, so that it may "map over" that many
+     * lists (or other Functors).
+     *
+     * @func
+     * @memberOf R
+     * @see R.lift
+     * @category Function
+     * @sig Number -> (*... -> *) -> ([*]... -> [*])
+     * @param {Function} fn The function to lift into higher context
+     * @return {Function} The function `fn` applicable to mappable objects.
+     * @example
+     *
+     *     var madd3 = R.liftN(3, R.curryN(3, function() {
+     *         return R.foldl(R.add, 0, arguments);
+     *     }));
+     *     madd3([1,2,3], [1,2,3], [1]); //=> [3, 4, 5, 4, 5, 6, 5, 6, 7]
+     */
+    R.liftN = curry2(function liftN(arity, fn) {
+        var lifted = curryN(arity, fn);
+        if (arguments.length === 0) {
+            throw noArgsException();
+        }
+        return R.curryN(arity, function() {
+            return R.foldl(R.ap, R.map(lifted, arguments[0]), _slice(arguments, 1));
+        });
+    });
+
+
+    /**
+     * "lifts" a function of arity > 1 so that it may "map over" an Array or
+     * other Functor.
+     *
+     * @func
+     * @memberOf R
+     * @see R.liftN
+     * @category Function
+     * @sig (*... -> *) -> ([*]... -> [*])
+     * @param {Function} fn The function to lift into higher context
+     * @return {Function} The function `fn` applicable to mappable objects.
+     * @example
+     *
+     *     var madd3 = R.lift(R.curryN(3, function(a, b, c) {
+     *         return a + b + c;
+     *     }));
+     *     madd3([1,2,3], [1,2,3], [1]); //=> [3, 4, 5, 4, 5, 6, 5, 6, 7]
+     *
+     *     var madd5 = R.lift(R.curryN(5, function(a, b, c, d, e) {
+     *         return a + b + c + d + e;
+     *     }));
+     *     madd5([1,2], [3], [4, 5], [6], [7, 8]); //=> [21, 22, 22, 23, 22, 23, 23, 24]
+     */
+    R.lift = function lift(fn) {
+        if (arguments.length === 0) {
+            throw noArgsException();
+        }
+        return R.liftN(fn.length, fn);
+    };
+
+
+    /**
      * ap applies a list of functions to a list of values.
      *
      * @func
