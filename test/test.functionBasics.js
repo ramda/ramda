f0721feb941749b98a27c86cdd06dbb57d48e6f3
David Chambers <dc@davidchambers.me>
2014-10-20 16:28:06 -0700

    add R.apply

--- test/test.functionBasics.js
+++ test/test.functionBasics.js
@@ -1,6 +1,22 @@
 var assert = require('assert');
 var R = require('..');
 
+describe('apply', function() {
+    it('applies function to argument list', function() {
+        assert.strictEqual(R.apply(Math.max, [1, 2, 3, -99, 42, 6, 7]), 42);
+    });
+
+    it('is automatically curried', function() {
+        assert.strictEqual(R.apply(Math.max)([1, 2, 3, -99, 42, 6, 7]), 42);
+    });
+
+    it('provides no way to specify context', function() {
+        var obj = {method: function() { return this === obj; }};
+        assert.strictEqual(R.apply(obj.method, []), false);
+        assert.strictEqual(R.apply(R.bind(obj.method, obj), []), true);
+    });
+});
+
 describe('flip', function() {
     it('should return a function which inverts the first two arguments to the supplied function', function() {
         var f = function(a, b, c) {return a + ' ' + b + ' ' + c;};
