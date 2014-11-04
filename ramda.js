a25d6c63ae7c4bd4b93e18acf94d366cc9c42bae
David Chambers <dc@davidchambers.me>
2014-11-03 18:54:51 -0800

    add R.nth

--- ramda.js
+++ ramda.js
@@ -919,6 +919,30 @@
 
 
     /**
+     * Returns the nth element in a list.
+     * If n is negative the element at index length + n is returned.
+     *
+     * @func
+     * @memberOf R
+     * @category core
+     * @category List
+     * @sig [a] -> a
+     * @param {Number} idx
+     * @param {Array} list
+     * @return {*} The nth element of the list.
+     * @example
+     *
+     *      var list = ['foo', 'bar', 'baz', 'quux'];
+     *      R.nth(1, list); //=> 'bar'
+     *      R.nth(-1, list); //=> 'quux'
+     *      R.nth(-99, list); //=> undefined
+     */
+    R.nth = curry2(function nth(n, list) {
+        return n < 0 ? list[list.length + n] : list[n];
+    });
+
+
+    /**
      * Returns the first element in a list.
      * In some libraries this function is named `first`.
      *
