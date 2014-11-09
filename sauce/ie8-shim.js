1750d05522547ebba8f4edcf27254f80b7a1f68b
David Chambers <dc@davidchambers.me>
2014-11-08 17:09:16 -0800

    lint sauce files

--- sauce/ie8-shim.js
+++ sauce/ie8-shim.js
@@ -1,33 +1,37 @@
-// redefine `require` since old IE is too dumb to handle shadowing the `R` variable in the required modules.
-function require(path) {
-  switch (path.substr(path.lastIndexOf('/') + 1)) {
-    case 'assert':
-      return assert;
-    case '..':
-      return this.R ? this.R : this.ramda;
-    case 'lazylist':
-      return this.R ? this.R.lazylist : this.ramda.lazylist;
-    default:
-      throw new Error('Unexpected require path "' + path + '"');
-  }
-}
+/* jshint browser: true */
 
-if( typeof document.getElementsByClassName !== "function"){
-  document.getElementsByClassName = function( className, nodeName )
-  {
-    var result = [], tag = nodeName||'*', node, seek, i;
-    if( document.evaluate ){
-      seek = '//'+ tag +'[@class="'+ className +'"]';
-      seek = document.evaluate( seek, document, null, 0, null );
-      while( (node = seek.iterateNext()) )
-        result.push( node );
-    }else{
-      var rightClass = new RegExp( '(^| )'+ className +'( |$)' );
-      seek = document.getElementsByTagName( tag );
-      for( i=0; i<seek.length; i++ )
-        if( rightClass.test( (node = seek[i]).className ) )
-          result.push( seek[i] );
+// redefine `require` since old IE is too dumb to handle shadowing the `R` variable in the required modules.
+window.require = function require(path) {
+    switch (path.substr(path.lastIndexOf('/') + 1)) {
+        case 'assert':
+            return window.assert;
+        case '..':
+            return this.R ? this.R : this.ramda;
+        case 'lazylist':
+            return this.R ? this.R.lazylist : this.ramda.lazylist;
+        default:
+            throw new Error('Unexpected require path "' + path + '"');
     }
-    return result;
-  };
+};
+
+if (typeof document.getElementsByClassName !== 'function') {
+    document.getElementsByClassName = function(className, nodeName) {
+        var result = [], tag = nodeName || '*', node, seek, i;
+        if (document.evaluate) {
+            seek = '//' + tag + '[@class="' + className + '"]';
+            seek = document.evaluate(seek, document, null, 0, null);
+            while ((node = seek.iterateNext())) {
+                result.push(node);
+            }
+        } else {
+            var rightClass = new RegExp('(^| )' + className + '( |$)');
+            seek = document.getElementsByTagName(tag);
+            for (i = 0; i < seek.length; i++) {
+                if (rightClass.test((node = seek[i]).className)) {
+                    result.push(seek[i]);
+                }
+            }
+        }
+        return result;
+    };
 }
--- sauce/mocharun.js
+++ sauce/mocharun.js
@@ -1,25 +1,33 @@
-onload = function(){
-  var runner = mocha.run();
-  var failedTests = [];
-  runner.on('end', function(){
-    window.mochaResults = runner.stats;
-    window.mochaResults.reports = failedTests;
-    ramda.installTo(window);  // for manual testing in the console.
-  });
-       
-  runner.on('fail', logFailure);
-        
-  function logFailure(test, err){
-        
-    var flattenTitles = function(test){
-      var titles = [];
-      while (test.parent.title){
-        titles.push(test.parent.title);
-        test = test.parent;
-      }
-      return titles.reverse();
-    };
-   
-    failedTests.push({name: test.title, result: false, message: err.message, stack: err.stack, titles: flattenTitles(test) });
-  }
+/* jshint browser: true */
+
+window.onload = function onload() {
+    var runner = window.mocha.run();
+    var failedTests = [];
+    runner.on('end', function() {
+        window.mochaResults = runner.stats;
+        window.mochaResults.reports = failedTests;
+        window.ramda.installTo(window);  // for manual testing in the console.
+    });
+
+    runner.on('fail', logFailure);
+
+    function logFailure(test, err) {
+
+        var flattenTitles = function(test) {
+            var titles = [];
+            while (test.parent.title) {
+                titles.push(test.parent.title);
+                test = test.parent;
+            }
+            return titles.reverse();
+        };
+
+        failedTests.push({
+            name: test.title,
+            result: false,
+            message: err.message,
+            stack: err.stack,
+            titles: flattenTitles(test)
+        });
+    }
 };
