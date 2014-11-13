e6127d3b293cb06c03f04d099283eb037f09d5a4
arcseldon <arcseldon@gmail.com>
2014-11-14 05:15:12 +0900

    Update wrap to correctly return wrapped function arity

--- test/test.wrap.js
+++ test/test.wrap.js
@@ -10,4 +10,13 @@ describe('wrap', function() {
         assert.equal(greet('joe'), 'Hello joe');
         assert.equal(extendedGreet('joe'), 'Hello my dear joe, how are you?');
     });
+
+    it('should maintain the arity of the function that is being wrapped', function() {
+        var greet = function(name) {return 'Hello ' + name;};
+        var extendedGreet = R.wrap(greet, function(gr, name) {
+            return gr('my dear ' + name) + ', how are you?';
+        });
+        assert.equal(greet.length, extendedGreet.length);
+    });
+
 });
