326e65bfce109b360126ea95f7a5652347c60375
=frank tree <=frnktrb@googlemail.com>
2014-11-18 17:12:26 +0100

    fix converge doc to accept 2 functions

--- ramda.js
+++ ramda.js
@@ -1637,7 +1637,7 @@
 
 
     /**
-     * Accepts at least three functions and returns a new function. When invoked, this new
+     * Accepts at least two functions and returns a new function. When invoked, this new
      * function will invoke the first function, `after`, passing as its arguments the
      * results of invoking the subsequent functions with whatever arguments are passed to
      * the new function.
