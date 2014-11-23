418bea5896466028b623a07ff62cb1e77eb7d5ba
David Chambers <dc@davidchambers.me>
2014-11-23 09:53:07 -0800

    add R.call

--- test/test.functionBasics.js
+++ test/test.functionBasics.js
@@ -1,6 +1,30 @@
 var assert = require('assert');
 var R = require('..');
 
+describe('call', function() {
+    it('returns the result of calling its first argument with the remaining arguments', function() {
+        assert.strictEqual(R.call(Math.max, 1, 2, 3, -99, 42, 6, 7), 42);
+    });
+
+    it('throws on zero arguments', function() {
+        assert.throws(R.call, TypeError);
+    });
+
+    it('accepts one or more arguments', function() {
+        var fn = function() { return arguments.length; };
+        assert.strictEqual(R.call(fn), 0);
+        assert.strictEqual(R.call(fn, 'x'), 1);
+        assert.strictEqual(R.call(fn, 'x', 'y'), 2);
+        assert.strictEqual(R.call(fn, 'x', 'y', 'z'), 3);
+    });
+
+    it('provides no way to specify context', function() {
+        var obj = {method: function() { return this === obj; }};
+        assert.strictEqual(R.call(obj.method), false);
+        assert.strictEqual(R.call(R.bind(obj.method, obj)), true);
+    });
+});
+
 describe('apply', function() {
     it('applies function to argument list', function() {
         assert.strictEqual(R.apply(Math.max, [1, 2, 3, -99, 42, 6, 7]), 42);
