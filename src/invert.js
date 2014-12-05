b9181570503a635930e253da5a2f0cb3f27c82fd
stites <sam@stites.io>
2014-12-05 14:47:10 -0800

    rebase and split work for invert and invertObj

---
+++ src/invert.js
@@ -0,0 +1,41 @@
+var keys = require('./keys');
+
+/**
+ * Same as R.invertObj, however this accounts for objects
+ * with duplicate values by putting the values into an
+ * array.
+ *
+ * @func
+ * @memberOf R
+ * @category Object
+ * @sig {s: x} -> {x: [ s, ... ]}
+ * @param {s: x} obj The object or array to invert
+ * @return {x: [ s, ... ]} out A new object with keys
+ * in an array.
+ * @example
+ *
+ *      var raceResultsByFirstName = {
+ *        first: 'alice',
+ *        second: 'jake',
+ *        third: 'alice',
+ *      };
+ *      R.invert(raceResultsByFirstName);
+ *      //=> { 'alice': ['first', 'third'], 'jake':['second'] }
+ *
+ */
+module.exports = function invert(obj) {
+    var props = keys(obj),
+        len = props.length,
+        idx = -1,
+        out = {};
+
+    while (++idx < len) {
+        var key = props[idx],
+            val = obj[key];
+
+        out[val] = out[val] || [];
+        out[val].push(key);
+    }
+
+    return out;
+};
---
+++ test/invertObj.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var R = require('..');
+
+describe('invertObj', function() {
+
+    it('takes a list or object and returns an object', function() {
+        assert.equal(typeof R.invertObj([]), 'object');
+        assert.equal(typeof R.invertObj({}), 'object');
+        assert.throws(R.invertObj(1));
+        assert.throws(R.invertObj('1'));
+        assert.throws(R.invertObj(null));
+        assert.throws(R.invertObj(undefined));
+    });
+
+    it('returns the input\'s values as keys, and keys as values', function() {
+        assert.deepEqual(R.invertObj(['a', 'b', 'c']),       {a:'0', b:'1', c:'2'});
+        assert.deepEqual(R.invertObj({x:'a', y:'b', z:'c'}), {a:'x', b:'y', c:'z'});
+    });
+
+    it('prefers the last key found when handling keys with the same value', function() {
+        assert.deepEqual(R.invertObj(['a', 'b', 'a']), {a:'2', b:'1'});
+        assert.deepEqual(R.invertObj({x:'a', y:'b', z:'a', _id:'a'}), {a: '_id', b: 'y'});
+    });
+
+    // this one is more of a sanity check
+    it('is not destructive', function() {
+        var input = {x:'a', y:'b', z:'a', _id:'a'};
+        R.invertObj(input);
+        assert.deepEqual(input, {x:'a', y:'b', z:'a', _id:'a'});
+    });
+});
