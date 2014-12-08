f48b43106c3f7b9d65f5b11ab59d2926bf02c744
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2014-12-07 23:56:20 -0800

    Add mapAccumL and mapAccumR. Closes #632.
    Reviewed by @tolmasky.

---
+++ src/mapAccumL.js
@@ -0,0 +1,35 @@
+var _curry3 = require('./internal/_curry3');
+
+
+/**
+ * The mapAccumL function behaves like a combination of map and foldl; it applies a
+ * function to each element of a list, passing an accumulating parameter from left to
+ * right, and returning a final value of this accumulator together with the new list.
+ *
+ * The iterator function receives two values: *(acc, value)*
+ *
+ * @func
+ * @memberOf R
+ * @category List
+ * @sig (acc -> x -> (acc, y)) -> acc -> [x] -> (acc, [y])
+ * @param {Function} fn The function to be called on every element of the input `list`.
+ * @param {*} acc The accumulator value.
+ * @param {Array} list The list to iterate over.
+ * @return {*} The final, accumulated value.
+ * @example
+ *
+ *      var digits = ['1', '2', '3', '4'];
+ *      var append = function(a, b) {
+ *          return [a + b, a + b];
+ *      }
+ *
+ *      R.mapAccumL(append, 0, digits); //=> ['1234', ['1', '12', '123', '1234']]
+ */
+module.exports = _curry3(function mapAccumL(fn, acc, list) {
+    var idx = -1, len = list.length, result = new Array(len), tuple = [acc];
+    while (++idx < len) {
+        tuple = fn(tuple[0], list[idx]);
+        result[idx] = tuple[1];
+    }
+    return [tuple[0], result];
+});
---
+++ test/mapAccumL.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+
+var R = require('..');
+
+
+describe('mapAccumL', function() {
+    var add = function(a, b) {return [a + b, a + b];};
+    var mult = function(a, b) {return [a * b, a * b];};
+    var concat = function(a, b) {return [a.concat(b), a.concat(b)];};
+
+    it('map and accumulate simple functions over arrays with the supplied accumulator', function() {
+        assert.deepEqual(R.mapAccumL(add, 0, [1, 2, 3, 4]), [10, [1, 3, 6, 10]]);
+        assert.deepEqual(R.mapAccumL(mult, 1, [1, 2, 3, 4]), [24, [1, 2, 6, 24]]);
+    });
+
+    it('returns the list and accumulator for an empty array', function() {
+        assert.deepEqual(R.mapAccumL(add, 0, []), [0, []]);
+        assert.deepEqual(R.mapAccumL(mult, 1, []), [1, []]);
+        assert.deepEqual(R.mapAccumL(concat, [], []), [[], []]);
+    });
+
+    it('is automatically curried', function() {
+        var addOrConcat = R.mapAccumL(add);
+        var sum = addOrConcat(0);
+        var cat = addOrConcat('');
+        assert.deepEqual(sum([1, 2, 3, 4]), [10, [1, 3, 6, 10]]);
+        assert.deepEqual(cat(['1', '2', '3', '4']), ['1234', ['1', '12', '123', '1234']]);
+    });
+
+    it('correctly reports the arity of curried versions', function() {
+        var sum = R.mapAccumL(add, 0);
+        assert.strictEqual(sum.length, 1);
+    });
+
+    it('throws on zero arguments', function() {
+        assert.throws(R.mapAccumL, TypeError);
+        assert.throws(R.mapAccumL(add), TypeError);
+    });
+});
