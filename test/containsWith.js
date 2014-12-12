ba856ee02410ee991ebd07ace80cb7fe859d9791
buzzdecafe <m_hur@yahoo.com>
2014-12-12 18:57:51 -0500

    tests for arity and containsWith. closes #650, closes #651

--- test/containsWith.js
+++ test/containsWith.js
@@ -4,5 +4,21 @@ var R = require('..');
 
 
 describe('containsWith', function() {
-    assert.strictEqual(R, R);
+    var Ro = [{a: 1}, {a: 2}, {a: 3}, {a: 4}];
+    var So = [{a: 3}, {a: 4}, {a: 5}, {a: 6}];
+    var eqA = function(r, s) { return r.a === s.a; };
+
+    it('determines if an element is the list based on the predicate', function() {
+        assert(R.containsWith(eqA, {a: 3}, So));
+        assert.strictEqual(R.containsWith(eqA, {a: 3000}, So), false);
+    });
+    it('is curried', function() {
+        assert.strictEqual(typeof R.containsWith(eqA), 'function');
+        assert.strictEqual(typeof R.containsWith(eqA)({a: 3}), 'function');
+        assert(R.containsWith(eqA)({a: 3})(Ro));
+    });
+    it('throws on zero arguments', function() {
+        assert.throws(R.containsWith, TypeError);
+        assert.throws(R.containsWith(eqA), TypeError);
+    });
 });
