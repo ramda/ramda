7b78cf6b7efe6bcf6283bcbbf3a313b115246d63
buzzdecafe <m_hur@yahoo.com>
2014-12-12 16:38:24 -0500

    update propOr docs to indicate you must pass an object. closes #680

--- src/propOr.js
+++ src/propOr.js
@@ -3,9 +3,8 @@ var has = require('./has');
 
 
 /**
- * If the given object has an own property with the specified name,
- * returns the value of that property.
- * Otherwise returns the provided default value.
+ * If the given, non-null object has an own property with the specified name,
+ * returns the value of that property. Otherwise returns the provided default value.
  *
  * @func
  * @memberOf R
