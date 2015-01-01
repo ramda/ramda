2f7456d455069b69491fb9fa7d8b1f19dd66a370
Brian Mock <brian@mockbrian.com>
2015-01-01 15:11:39 -0800

    Improves scrolling to section behavior

--- docs/main.js
+++ docs/main.js
@@ -13,4 +13,27 @@ function strIn(a, b) {
     return b.indexOf(a) >= 0;
 }
 
+function scrollToTop() {
+    var main = document.querySelector('main');
+    main.scrollTop = 0;
+}
+
+function toggleCollapse(elem) {
+    var sel = elem.getAttribute('data-collapser')
+    document
+        .querySelector(sel)
+        .classList
+        .toggle('in');
+}
+
+function dispatchEvent(event) {
+    var target = event.target;
+    if (target.tagName === 'A' && target.getAttribute('href') === '#') {
+        scrollToTop(target);
+    } else if (target.getAttribute('data-collapser')) {
+        toggleCollapse(target);
+    }
+}
+
+document.body.addEventListener('click', dispatchEvent, false);
 ko.applyBindings(root);
