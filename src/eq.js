92ce75bba24cf4ad6b1b8f2a6563bc846c55fc1f
David Rusu <davidrusu.me@gmail.com>
2015-01-16 14:19:35 -0500

    made R.eq behave as Object.is from ES6

--- src/eq.js
+++ src/eq.js
@@ -19,5 +19,13 @@ var _curry2 = require('./internal/_curry2');
  *      R.eq(o, {}); //=> false
  *      R.eq(1, 1); //=> true
  *      R.eq(1, '1'); //=> false
+ *      R.eq(0, -0); //=> false
+ *      R.eq(NaN, NaN); //=> true
  */
-module.exports = _curry2(function eq(a, b) { return a === b; });
+module.exports = _curry2(function eq(a, b) {
+    if (a === 0) {
+        return 1 / a === 1 / b;
+    } else {
+        return a === b || (a !== a && b !== b);
+    }
+});
