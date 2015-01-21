919bbd6d8f0ac6c37b9b799df8f3da7b608f450e
Jethro Larson <jethrolarson@gmail.com>
2015-01-21 15:46:18 -0800

    Update ifElse.js
    added markdown formatting to condition

--- src/ifElse.js
+++ src/ifElse.js
@@ -10,8 +10,8 @@ var _curry3 = require('./internal/_curry3');
  * @category Logic
  * @sig (*... -> Boolean) -> (*... -> *) -> (*... -> *) -> (*... -> *)
  * @param {Function} condition A predicate function
- * @param {Function} onTrue A function to invoke when the condition evaluates to a truthy value.
- * @param {Function} onFalse A function to invoke when the condition evaluates to a falsy value.
+ * @param {Function} onTrue A function to invoke when the `condition` evaluates to a truthy value.
+ * @param {Function} onFalse A function to invoke when the `condition` evaluates to a falsy value.
  * @return {Function} A new unary function that will process either the `onTrue` or the `onFalse` function 
  * depending upon the result of the `condition` predicate.
  * @example
