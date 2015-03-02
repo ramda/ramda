6c262b2c4d2b768c3eaf8ca0aa22d3153e9549e9
Santeri Hiltunen <hilzu@reaktor.fi>
2015-03-03 01:43:39 +0200

    Implement dissocPath

--- dist/ramda.js
+++ dist/ramda.js
@@ -5765,6 +5765,38 @@
     });
 
     /**
+     * Makes a shallow clone of an object, omitting the property at the
+     * given path. Note that this copies and flattens prototype properties
+     * onto the new object as well.  All non-primitive properties are copied
+     * by reference.
+     *
+     * @func
+     * @memberOf R
+     * @category Object
+     * @sig String -> {k: v} -> {k: v}
+     * @param {String} path the dot-delimited path to set
+     * @param {Object} obj the object to clone
+     * @return {Object} a new object without the property at path
+     * @example
+     *
+     *      var obj1 = {a: 1, b: {c: 2, d: 3}, e: 4};
+     *      var obj2 = R.dissocPath('b.c', obj1);
+     *      //=> {a: 1, b: {d: 3}, e: 4}
+     */
+    var dissocPath = function () {
+        function dissocPath(parts, obj) {
+            if (parts.length === 1) {
+                return dissoc(parts[0], obj);
+            }
+            var current = obj[parts[0]];
+            return is(Object, current) ? assoc(parts[0], dissocPath(_slice(parts, 1), current), obj) : obj;
+        }
+        return _curry2(function (path, obj) {
+            return dissocPath(split('.', path), obj);
+        });
+    }();
+
+    /**
      * Performs a deep test on whether two items are equal.
      * Equality implies the two items are semmatically equivalent.
      * Cyclic structures are handled as expected
@@ -6203,6 +6235,7 @@
         difference: difference,
         differenceWith: differenceWith,
         dissoc: dissoc,
+        dissocPath: dissocPath,
         divide: divide,
         drop: drop,
         dropWhile: dropWhile,
