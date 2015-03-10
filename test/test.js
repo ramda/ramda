b220b07fe822c390e3ef1baaa7a4294f072da20f
David Chambers <dc@davidchambers.me>
2015-03-10 12:48:30 -0700

    add R.test

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+var R = require('..');
+
+
+describe('test', function() {
+    it('returns true if string matches pattern', function() {
+        assert.strictEqual(R.test(/^x/, 'xyz'), true);
+    });
+
+    it('returns false if string does not match pattern', function() {
+        assert.strictEqual(R.test(/^y/, 'xyz'), false);
+    });
+
+    it('is referentially transparent', function() {
+        var pattern = /x/g;
+        assert.strictEqual(pattern.lastIndex, 0);
+        assert.strictEqual(R.test(pattern, 'xyz'), true);
+        assert.strictEqual(pattern.lastIndex, 0);
+        assert.strictEqual(R.test(pattern, 'xyz'), true);
+    });
+});
