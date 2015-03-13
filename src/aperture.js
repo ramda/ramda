6dd786dd5ff31c9473dd209ccea308b15210e5c9
buzzdecafe <m_hur@yahoo.com>
2015-03-13 18:00:14 -0400

    moving window array splitting

---
+++ src/aperture.js
@@ -0,0 +1,29 @@
+var _curry2 = require('./internal/_curry2');
+var _slice = require('./internal/_slice');
+
+/**
+ * Returns a new list, composed of n-tuples of consecutive elements
+ * If `n` is greater than the length of the list, an empty list is returned.
+ *
+ * @func
+ * @memberOf R
+ * @category List
+ * @sig Number -> [a] -> [[a]]
+ * @param {Number} n The size of the tuples to create
+ * @param {Array} list The list to split into `n`-tuples
+ * @return {Array} The new list.
+ * @example
+ *
+ *      R.aperture(2, [1, 2, 3, 4, 5]); //=> [[1, 2], [2, 3], [3, 4], [4, 5]]
+ *      R.aperture(3, [1, 2, 3, 4, 5]); //=> [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+ *      R.aperture(7, [1, 2, 3, 4, 5]); //=> []
+ */
+module.exports = _curry2(function aperture(n, list) {
+    var idx = -1;
+    var limit = list.length - (n - 1);
+    var acc = new Array(limit >= 0 ? limit : 0);
+    while (++idx < limit) {
+        acc[idx] = _slice(list, idx, idx + n);
+    }
+    return acc;
+});
---
+++ test/aperture.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+var R = require('..');
+
+
+describe('aperture', function() {
+    var sevenLs = [1, 2, 3, 4, 5, 6, 7];
+    it('creates a list of n-tuples from a list', function() {
+        assert.deepEqual(R.aperture(1, sevenLs), [[1], [2], [3], [4], [5], [6], [7]]);
+        assert.deepEqual(R.aperture(2, sevenLs), [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]);
+        assert.deepEqual(R.aperture(3, sevenLs), [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7]]);
+        assert.deepEqual(R.aperture(4, [1, 2, 3, 4]), [[1, 2, 3, 4]]);
+    });
+
+    it('returns an empty list when `n` > `list.length`', function() {
+        assert.deepEqual(R.aperture(6, [1, 2, 3]), []);
+        assert.deepEqual(R.aperture(1, []), []);
+    });
+
+    it('is automatically curried', function() {
+        var pairwise = R.aperture(2);
+        assert.deepEqual(pairwise(sevenLs), [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]);
+    });
+});
