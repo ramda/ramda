afc92522c261d9603538cce23cdb873375f7cc3d
buzzdecafe <m_hur@yahoo.com>
2015-03-19 18:14:23 -0400

    properly curry ifElse; fixes #939

--- src/ifElse.js
+++ src/ifElse.js
@@ -1,4 +1,5 @@
 var _curry3 = require('./internal/_curry3');
+var curryN = require('./curryN');
 
 
 /**
@@ -23,7 +24,9 @@ var _curry3 = require('./internal/_curry3');
  *      flattenArrays([[[10], 123], [8, [10]], "hello"]); //=> [[10, 123], [8, 10], "hello"]
  */
 module.exports = _curry3(function ifElse(condition, onTrue, onFalse) {
-    return function _ifElse() {
-        return condition.apply(this, arguments) ? onTrue.apply(this, arguments) : onFalse.apply(this, arguments);
-    };
+    return curryN(Math.max(condition.length, onTrue.length, onFalse.length),
+        function _ifElse() {
+            return condition.apply(this, arguments) ? onTrue.apply(this, arguments) : onFalse.apply(this, arguments);
+        }
+    );
 });
