74a1231d67fb3e8294427c52c8f7cca3e4d293d0
buzzdecafe <m_hur@yahoo.com>
2015-03-30 18:46:05 -0400

    add test for sparse array; remove redundant ramda ref in test index

--- test/differenceWith.js
+++ test/differenceWith.js
@@ -9,10 +9,18 @@ describe('differenceWith', function() {
   var So = [{a: 3}, {a: 4}, {a: 5}, {a: 6}];
   var So2 = [{a: 3}, {a: 4}, {a: 5}, {a: 6}, {a: 3}, {a: 4}, {a: 5}, {a: 6}];
   var eqA = function(r, s) { return r.a === s.a; };
+  var eq = function(a, b) { return a === b; };
+
   it('combines two lists into the set of all their elements based on the passed-in equality predicate', function() {
     assert.deepEqual(R.differenceWith(eqA, Ro, So), [{a: 1}, {a: 2}]);
   });
+
   it('does not allow duplicates in the output even if the input lists had duplicates', function() {
     assert.deepEqual(R.differenceWith(eqA, Ro2, So2), [{a: 1}, {a: 2}]);
   });
+
+  it('does not return a "sparse" array', function() {
+    assert.strictEqual(R.differenceWith(eq, [1, 3, 2, 1, 3, 1, 2, 3], [3]).length, 2);
+  });
+
 });
