47e82b2541d48934a2cd6f2fc3d881b2b408082c
David Chambers <dc@davidchambers.me>
2015-03-30 10:41:10 -0700

    have R.converge return a function of appropriate length

--- src/converge.js
+++ src/converge.js
@@ -1,6 +1,8 @@
 var _map = require('./internal/_map');
 var _slice = require('./internal/_slice');
 var curryN = require('./curryN');
+var max = require('./max');
+var pluck = require('./pluck');
 
 
 /**
@@ -31,10 +33,10 @@ var curryN = require('./curryN');
  */
 module.exports = curryN(3, function(after) {
   var fns = _slice(arguments, 1);
-  return function() {
+  return curryN(max(pluck('length', fns)), function() {
     var args = arguments;
     return after.apply(this, _map(function(fn) {
       return fn.apply(this, args);
     }, fns));
-  };
+  });
 });
