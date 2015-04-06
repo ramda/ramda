13097b9e63d29ef6fa949558b5e2512eb29a4a06
buzzdecafe <m_hur@yahoo.com>
2015-04-06 18:50:07 -0400

    make pluck behave with transducers, fixes #1012

--- test/pluck.js
+++ test/pluck.js
@@ -15,4 +15,11 @@ describe('pluck', function() {
     assert.strictEqual(typeof nm, 'function');
     assert.deepEqual(nm(people), ['Fred', 'Wilma', 'Pebbles']);
   });
+
+  it('behaves as a transducer when given a transducer in list position', function() {
+    var numbers = [{a: 1}, {a: 2}, {a: 3}, {a: 4}];
+    var transducer = R.compose(R.pluck('a'), R.map(R.add(1)), R.take(2));
+    assert.deepEqual(R.transduce(transducer, R.flip(R.append), [], numbers), [2, 3]);
+  });
+
 });
