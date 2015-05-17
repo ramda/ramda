20b7c3a4ee51951ffd4a8a7cb17bffb4a35e3607
David Chambers <dc@davidchambers.me>
2015-05-17 11:34:55 -0700

    derive R.contains from R.indexOf

--- src/contains.js
+++ src/contains.js
@@ -1,23 +1,34 @@
-var _contains = require('./internal/_contains');
 var _curry2 = require('./internal/_curry2');
+var _hasMethod = require('./internal/_hasMethod');
+var indexOf = require('./indexOf');
 
 
 /**
  * Returns `true` if the specified value is equal, in `R.equals` terms,
  * to at least one element of the given list; `false` otherwise.
  *
+ * Dispatches to the collection's `contains` method if applicable. If the
+ * collection does not have a `contains` method but does have an `indexOf`
+ * method, this function dispatches to `indexOf` and returns `true` if the
+ * return value is greater than or equal to zero; `false` otherwise. As a
+ * result, this function can be used to determine whether a given string
+ * contains a particular substring.
+ *
  * @func
  * @memberOf R
  * @category List
  * @sig a -> [a] -> Boolean
  * @param {Object} a The item to compare against.
- * @param {Array} list The array to consider.
- * @return {Boolean} `true` if the item is in the list, `false` otherwise.
+ * @param {Array} xs The collection to consider.
+ * @return {Boolean} `true` if the item is in the collection, `false` otherwise.
  *
  * @example
  *
  *      R.contains(3, [1, 2, 3]); //=> true
  *      R.contains(4, [1, 2, 3]); //=> false
  *      R.contains([42], [[42]]); //=> true
+ *      R.contains('am', 'Ramda'); //=> true
  */
-module.exports = _curry2(_contains);
+module.exports = _curry2(function contains(a, xs) {
+  return _hasMethod('contains', xs) ? xs.contains(a) : indexOf(a, xs) >= 0;
+});
--- test/contains.js
+++ test/contains.js
@@ -28,6 +28,64 @@ describe('contains', function() {
     assert.strictEqual(R.contains(new Just([42]), [new Just([42])]), true);
   });
 
+  it('dispatches to `contains` method', function() {
+    function Empty() {}
+    Empty.prototype.contains = R.always(false);
+
+    function List(head, tail) {
+      this.head = head;
+      this.tail = tail;
+    }
+    List.prototype.contains = function(x) {
+      return this.head === x || this.tail.contains(x);
+    };
+
+    var list = new List('foo', new List('bar', new List('baz', new Empty())));
+
+    assert.strictEqual(R.contains('foo', list), true);
+    assert.strictEqual(R.contains('bar', list), true);
+    assert.strictEqual(R.contains('baz', list), true);
+    assert.strictEqual(R.contains('quux', list), false);
+  });
+
+  it('dispatches to `indexOf` method', function() {
+    function Empty() {}
+    Empty.prototype.indexOf = R.always(-1);
+
+    function List(head, tail) {
+      this.head = head;
+      this.tail = tail;
+    }
+    List.prototype.indexOf = function(x) {
+      var idx = this.tail.indexOf(x);
+      return this.head === x ? 0 : idx >= 0 ? 1 + idx : -1;
+    };
+
+    var list = new List('b',
+               new List('a',
+               new List('n',
+               new List('a',
+               new List('n',
+               new List('a',
+               new Empty()))))));
+
+    assert.strictEqual(R.contains('a', list), true);
+    assert.strictEqual(R.contains('b', list), true);
+    assert.strictEqual(R.contains('c', list), false);
+    assert.strictEqual(R.contains('a', 'banana'), true);
+    assert.strictEqual(R.contains('b', 'banana'), true);
+    assert.strictEqual(R.contains('c', 'banana'), false);
+    assert.strictEqual(R.contains('nana', 'banana'), true);
+  });
+
+  it('does not dispatch to `indexOf` if `contains` is defined', function() {
+    function Empty() {}
+    Empty.prototype.contains = R.always(false);
+    Empty.prototype.indexOf = R.always(42);
+
+    assert.strictEqual(R.contains('foo', new Empty()), false);
+  });
+
   it('is curried', function() {
     assert.strictEqual(typeof R.contains(7), 'function');
     assert.strictEqual(R.contains(7)([1, 2, 3]), false);
