46ab6720f6158b6ee2dcfd74166ba379640213f8
David Chambers <dc@davidchambers.me>
2015-05-21 11:15:12 -0700

    fix handling of `new Number(0)` in R.eq

--- src/internal/_eq.js
+++ src/internal/_eq.js
@@ -1,7 +1,11 @@
-module.exports = function _eq(a, b) {
-  if (a === 0) {
-    return 1 / a === 1 / b;
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is#Polyfill
+module.exports = function _eq(x, y) {
+  // SameValue algorithm
+  if (x === y) { // Steps 1-5, 7-10
+    // Steps 6.b-6.e: +0 != -0
+    return x !== 0 || 1 / x === 1 / y;
   } else {
-    return a === b || (a !== a && b !== b);
+    // Step 6.a: NaN == NaN
+    return x !== x;
   }
 };
