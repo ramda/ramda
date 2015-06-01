6c854463d5873ad58ae3aad4914436aafef5b906
paldepind <simonfv@gmail.com>
2015-06-01 09:38:41 +0200

    Make `R.evolve` recursive

--- src/evolve.js
+++ src/evolve.js
@@ -1,11 +1,12 @@
 var _curry2 = require('./internal/_curry2');
-var _extend = require('./internal/_extend');
-var mapObjIndexed = require('./mapObjIndexed');
 
 
 /**
- * Creates a new object by evolving a shallow copy of `object`, according to the
- * `transformation` functions.  All non-primitive properties are copied by reference.
+ * Creates a new object by recursively evolving a shallow copy of `object`, according to the
+ * `transformation` functions. All non-primitive properties are copied by reference.
+ *
+ * A `tranformation` function will not be invoked if its corresponding key does not exist in
+ * the evolved object.
  *
  * @func
  * @memberOf R
@@ -17,10 +18,22 @@ var mapObjIndexed = require('./mapObjIndexed');
  * @return {Object} The transformed object.
  * @example
  *
- *      R.evolve({ elapsed: R.add(1), remaining: R.add(-1) }, { name: 'Tomato', elapsed: 100, remaining: 1400 }); //=> { name: 'Tomato', elapsed: 101, remaining: 1399 }
+ *      var tomato  = {firstName: '  Tomato ', elapsed: 100, remaining: 1400};
+ *      var transformations = {
+ *        firstName: R.trim,
+ *        lastName: R.trim, // Will not get invoked.
+ *        data: {elapsed: R.add(1), remaining: R.add(-1)}
+ *      };
+ *      R.evolve(transformations, tomato); //=> {firstName: 'Tomato', data: {elapsed: 101, remaining: 1399}}
  */
 module.exports = _curry2(function evolve(transformations, object) {
-  return _extend(_extend({}, object), mapObjIndexed(function(fn, key) {
-    return fn(object[key]);
-  }, transformations));
+  var transformation, key, type, result = {};
+  for (key in object) {
+    transformation = transformations[key];
+    type = typeof transformation;
+    result[key] = type === 'function' ? transformation(object[key])
+                : type === 'object'   ? evolve(transformations[key], object[key])
+                                      : object[key];
+  }
+  return result;
 });
