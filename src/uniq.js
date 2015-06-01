7f36f30dacfcf42b89b36f051f1f43bb91daf025
Scott Christopher <scott.christopher@hiveempire.com>
2015-06-01 17:07:02 +1000

    R.uniq implementation to use Set if available

--- src/uniq.js
+++ src/uniq.js
@@ -1,3 +1,5 @@
+var _contains = require('./internal/_contains');
+var _curry1 = require('./internal/_curry1');
 var equals = require('./equals');
 var uniqWith = require('./uniqWith');
 
@@ -18,4 +20,39 @@ var uniqWith = require('./uniqWith');
  *      R.uniq([1, '1']);     //=> [1, '1']
  *      R.uniq([[42], [42]]); //=> [[42]]
  */
-module.exports = uniqWith(equals);
+module.exports = (function() {
+  /**
+   * Uses a native `Set` instance where possible for
+   * removing duplicate items. Items that implement
+   * the fantasy-land Setoid spec fallback to using
+   * `_contains` to support custom equality behaviour.
+   */
+  function uniq(list) {
+    /* global Set */
+    var item,
+        set = new Set(),
+        idx = -1,
+        len = list.length,
+        items = [],
+        uniqs = [];
+
+    while (++idx < len) {
+      item = list[idx];
+      // `_contains` is also used to differentiate between
+      // +0 and -0, as the native Set does not.
+      if (item === 0 || (item != null && typeof item.equals === 'function')) {
+        if (!_contains(item, items)) {
+          items.push(item);
+          uniqs.push(item);
+        }
+      } else {
+        if (set.size !== set.add(item).size) {
+          uniqs.push(item);
+        }
+      }
+    }
+    return uniqs;
+  }
+
+  return typeof Set !== 'function' ? uniqWith(equals) : _curry1(uniq);
+})();
