dacf17f63aeb06efc9749f19a7f179ab3b282ba4
David Chambers <dc@davidchambers.me>
2015-06-19 08:04:54 -0700

    demonstrate that R.isEmpty({}) evaluates false

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -12,10 +12,13 @@ var _curry1 = require('./internal/_curry1');
  * @return {Boolean}
  * @example
  *
- *      R.isEmpty([1, 2, 3]); //=> false
- *      R.isEmpty([]); //=> true
- *      R.isEmpty(''); //=> true
- *      R.isEmpty(null); //=> false
+ *      R.isEmpty([1, 2, 3]);   //=> false
+ *      R.isEmpty([]);          //=> true
+ *      R.isEmpty('');          //=> true
+ *      R.isEmpty(null);        //=> false
+ *      R.isEmpty(R.keys({}));  //=> true
+ *      R.isEmpty({});          //=> false ({} does not have a length property)
+ *      R.isEmpty({length: 0}); //=> true
  */
 module.exports = _curry1(function isEmpty(list) {
   return Object(list).length === 0;
