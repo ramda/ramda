6f68e4df558511d53291466015165cd123d10a61
paldepind <simonfv@gmail.com>
2015-06-22 10:50:39 +0200

    Make `and` and `or` sensible

--- src/or.js
+++ src/or.js
@@ -1,27 +1,24 @@
 var _curry2 = require('./internal/_curry2');
-var _hasMethod = require('./internal/_hasMethod');
 
 
 /**
- * A function that returns the first truthy of two arguments otherwise the
- * last argument. Note that this is NOT short-circuited, meaning that if
- * expressions are passed they are both evaluated.
- *
- * Dispatches to the `or` method of the first argument if applicable.
+ * A function that returns `true` if one or both of its arguments are strictly
+ * equal to `true` argument.
  *
  * @func
  * @memberOf R
  * @category Logic
- * @sig * -> * -> *
- * @param {*} a any value
- * @param {*} b any other value
- * @return {*} the first truthy argument, otherwise the last argument.
+ * @sig Boolean -> Boolean -> Boolean
+ * @param {Boolean} a A boolean value
+ * @param {Boolean} b A boolean value
+ * @return {Boolean} `true` if one or both arguments are `true`, `false` otherwise
  * @example
  *
+ *      R.or(true, true); //=> true
+ *      R.or(true, false); //=> true
  *      R.or(false, true); //=> true
- *      R.or(0, []); //=> []
- *      R.or(null, ''); => ''
+ *      R.or(false, false); //=> false
  */
 module.exports = _curry2(function or(a, b) {
-  return _hasMethod('or', a) ? a.or(b) : a || b;
+  return a === true || b === true;
 });
