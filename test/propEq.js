c7adcf770fd6a9df436f35da148b8b7124494e48
David Chambers <dc@davidchambers.me>
2015-07-05 17:24:12 -0700

    add R.propIs and R.propSatisfies

--- test/propEq.js
+++ test/propEq.js
@@ -4,15 +4,14 @@ var R = require('..');
 
 
 describe('propEq', function() {
-  var obj1 = {name: 'Abby', age: 7, hair: 'blond'};
-  var obj2 = {name: 'Fred', age: 12, hair: 'brown'};
-  var obj3 = {name: 'Rusty', age: 10, hair: 'brown'};
-  var obj4 = {name: 'Alois', age: 15, disposition: 'surly'};
+
+  var abby = {name: 'Abby', age: 7, hair: 'blond'};
+  var fred = {name: 'Fred', age: 12, hair: 'brown'};
 
   it('determines whether a particular property matches a given value for a specific object', function() {
-    assert.strictEqual(R.propEq('name', 'Abby', obj1), true);
-    assert.strictEqual(R.propEq('hair', 'brown', obj2), true);
-    assert.strictEqual(R.propEq('hair', 'blond', obj2), false);
+    assert.strictEqual(R.propEq('Abby', 'name', abby), true);
+    assert.strictEqual(R.propEq('brown', 'hair', fred), true);
+    assert.strictEqual(R.propEq('blond', 'hair', fred), false);
   });
 
   it('has R.equals semantics', function() {
@@ -21,20 +20,14 @@ describe('propEq', function() {
       return x instanceof Just && R.equals(x.value, this.value);
     };
 
-    assert.strictEqual(R.propEq('value', 0, {value: -0}), false);
-    assert.strictEqual(R.propEq('value', -0, {value: 0}), false);
-    assert.strictEqual(R.propEq('value', NaN, {value: NaN}), true);
-    assert.strictEqual(R.propEq('value', new Just([42]), {value: new Just([42])}), true);
+    assert.strictEqual(R.propEq(0, 'value', {value: -0}), false);
+    assert.strictEqual(R.propEq(-0, 'value', {value: 0}), false);
+    assert.strictEqual(R.propEq(NaN, 'value', {value: NaN}), true);
+    assert.strictEqual(R.propEq(new Just([42]), 'value', {value: new Just([42])}), true);
   });
 
   it('is curried', function() {
-    var kids = [obj1, obj2, obj3, obj4];
-    var hairMatch = R.propEq('hair');
-    assert.strictEqual(typeof hairMatch, 'function');
-    var brunette = hairMatch('brown');
-    assert.deepEqual(R.filter(brunette, kids), [obj2, obj3]);
-    // more likely usage:
-    assert.deepEqual(R.filter(R.propEq('hair', 'brown'), kids), [obj2, obj3]);
+    assert.strictEqual(R.propEq('Abby')('name')(abby), true);
   });
 
 });
