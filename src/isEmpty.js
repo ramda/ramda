6accdb0ac1de841fe2b8e0f50146e7cd03b97e35
Ludwig Magnusson <ludwig@mediatool.com>
2015-07-08 11:03:15 +0200

    Simplify the defenition of isEmpty
    The code examples now only show cases that match up with the description
    of the function. This has been reflected in the tests as well.
    Implementation is left unchanged.

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -15,10 +15,7 @@ var _curry1 = require('./internal/_curry1');
  *      R.isEmpty([1, 2, 3]);   //=> false
  *      R.isEmpty([]);          //=> true
  *      R.isEmpty('');          //=> true
- *      R.isEmpty(null);        //=> false
- *      R.isEmpty(R.keys({}));  //=> true
- *      R.isEmpty({});          //=> false ({} does not have a length property)
- *      R.isEmpty({length: 0}); //=> true
+ *      R.isEmpty('foo');        //=> false
  */
 module.exports = _curry1(function isEmpty(list) {
   return Object(list).length === 0;
