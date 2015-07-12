f688d8ac7931377ca40037c89f0f10a83646ee14
David Chambers <dc@davidchambers.me>
2015-07-12 13:19:49 -0700

    remove all type checking from R.concat

--- src/concat.js
+++ src/concat.js
@@ -1,23 +1,18 @@
-var _concat = require('./internal/_concat');
-var _curry2 = require('./internal/_curry2');
-var _hasMethod = require('./internal/_hasMethod');
-var _isArray = require('./internal/_isArray');
+var flip = require('./flip');
+var invoker = require('./invoker');
 
 
 /**
- * Returns a new list consisting of the elements of the first list followed by the elements
- * of the second.
+ * Returns the result of concatenating the given lists or strings.
  *
  * @func
  * @memberOf R
  * @category List
  * @sig [a] -> [a] -> [a]
- * @param {Array} list1 The first list to merge.
- * @param {Array} list2 The second set to merge.
- * @return {Array} A new array consisting of the contents of `list1` followed by the
- *         contents of `list2`. If, instead of an Array for `list1`, you pass an
- *         object with a `concat` method on it, `concat` will call `list1.concat`
- *         and pass it the value of `list2`.
+ * @sig String -> String -> String
+ * @param {Array|String} a
+ * @param {Array|String} b
+ * @return {Array|String}
  *
  * @example
  *
@@ -25,12 +20,4 @@ var _isArray = require('./internal/_isArray');
  *      R.concat([4, 5, 6], [1, 2, 3]); //=> [4, 5, 6, 1, 2, 3]
  *      R.concat('ABC', 'DEF'); // 'ABCDEF'
  */
-module.exports = _curry2(function concat(set1, set2) {
-  if (_isArray(set2)) {
-    return _concat(set1, set2);
-  } else if (_hasMethod('concat', set1)) {
-    return set1.concat(set2);
-  } else {
-    throw new TypeError("can't concat " + typeof set1);
-  }
-});
+module.exports = flip(invoker(1, 'concat'));
