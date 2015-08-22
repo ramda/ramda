f28adc24fee130ee30943ea7aac722cb7c883e1d
Asaf Romano <asaf@sent.com>
2015-08-22 20:39:30 +0300

    Rename isSet to allUniq
    Closes #1278

--- test/allUniq.js
+++ test/allUniq.js
@@ -3,15 +3,15 @@ var assert = require('assert');
 var R = require('..');
 
 
-describe('isSet', function() {
+describe('allUniq', function() {
   it('returns true if a list is composed of unique elements', function() {
     var list = [1, 2, 3, 1, 2, 3, 1, 2, 3];
-    assert.strictEqual(R.isSet(list), false);
-    assert.strictEqual(R.isSet([3, 1, 4, 2, 5, 7, 9]), true);
+    assert.strictEqual(R.allUniq(list), false);
+    assert.strictEqual(R.allUniq([3, 1, 4, 2, 5, 7, 9]), true);
   });
 
   it('returns true for an empty array', function() {
-    assert.strictEqual(R.isSet([]), true);
+    assert.strictEqual(R.allUniq([]), true);
   });
 
   it('has R.equals semantics', function() {
@@ -20,10 +20,10 @@ describe('isSet', function() {
       return x instanceof Just && R.equals(x.value, this.value);
     };
 
-    assert.strictEqual(R.isSet([0, -0]), true);
-    assert.strictEqual(R.isSet([-0, 0]), true);
-    assert.strictEqual(R.isSet([NaN, NaN]), false);
-    assert.strictEqual(R.isSet([new Just([42]), new Just([42])]), false);
+    assert.strictEqual(R.allUniq([0, -0]), true);
+    assert.strictEqual(R.allUniq([-0, 0]), true);
+    assert.strictEqual(R.allUniq([NaN, NaN]), false);
+    assert.strictEqual(R.allUniq([new Just([42]), new Just([42])]), false);
   });
 
 });
