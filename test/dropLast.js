c2a0754d19f3e43cf78e2c1bb215ebecd1c7963b
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2015-09-25 23:35:43 +0200

    dropLast can act as transducer

--- test/dropLast.js
+++ test/dropLast.js
@@ -35,4 +35,10 @@ describe('dropLast', function() {
     eq(dropLast2(['a', 'b', 'c', 'd', 'e']), ['a', 'b', 'c']);
     eq(dropLast2(['x', 'y', 'z']), ['x']);
   });
+
+  it('can act as a transducer', function() {
+    var dropLast2 = R.dropLast(2);
+    assert.deepEqual(R.into([], dropLast2, [1, 3, 5, 7, 9, 1, 2]), [1, 3, 5, 7, 9]);
+    assert.deepEqual(R.into([], dropLast2, [1]), []);
+  });
 });
