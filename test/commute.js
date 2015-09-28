1502c6d5a683725b4536c61e778108d5d7950343
David Chambers <dc@davidchambers.me>
2015-09-27 18:57:01 -0700

    fix R.commute and R.commuteMap

--- test/commute.js
+++ test/commute.js
@@ -1,5 +1,6 @@
-var Maybe = require('./shared/Maybe');
+var S = require('sanctuary');
 
+var Maybe = require('./shared/Maybe');
 var R = require('..');
 var eq = require('./shared/eq');
 
@@ -13,11 +14,18 @@ describe('commute', function() {
   it('"pivots" a list (list of functors => functor of a list)', function() {
     eq(R.commute(R.of, as), [[1, 3], [1, 4]]);
     eq(R.commute(R.of, bs), [[1, 3], [2, 3]]);
-    eq(R.commute(R.of, cs), [[1, 3], [2, 3], [1, 4], [2, 4]]);
+    eq(R.commute(R.of, cs), [[1, 3], [1, 4], [2, 3], [2, 4]]);
   });
 
   it('works on Algebraic Data Types such as "Maybe"', function() {
     eq(R.commute(Maybe.of, [Maybe(3), Maybe(4), Maybe(5)]), Maybe([3, 4, 5]));
+  });
+
+  it('traverses left to right', function() {
+    eq(R.commute(S.Either.of, [S.Right(1), S.Right(2)]), S.Right([1, 2]));
+    eq(R.commute(S.Either.of, [S.Right(1), S.Left('XXX')]), S.Left('XXX'));
+    eq(R.commute(S.Either.of, [S.Left('XXX'), S.Right(1)]), S.Left('XXX'));
+    eq(R.commute(S.Either.of, [S.Left('XXX'), S.Left('YYY')]), S.Left('XXX'));
   });
 
   it('is curried', function() {
@@ -25,7 +33,7 @@ describe('commute', function() {
     eq(typeof cmtArr, 'function');
     eq(cmtArr(as), [[1, 3], [1, 4]]);
     eq(cmtArr(bs), [[1, 3], [2, 3]]);
-    eq(cmtArr(cs), [[1, 3], [2, 3], [1, 4], [2, 4]]);
+    eq(cmtArr(cs), [[1, 3], [1, 4], [2, 3], [2, 4]]);
 
   });
 });
