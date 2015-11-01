9d207cd4e51ab83b9371c9967e6a7868db284dee
Raine Virta <raine.virta@gmail.com>
2015-11-01 14:54:35 +0200

    clarify R.length docs

--- src/length.js
+++ src/length.js
@@ -3,19 +3,20 @@ var is = require('./is');
 
 
 /**
- * Returns the number of elements in the array by returning `list.length`.
+ * Returns the length of an `Array`, `String`, or arity in case of a `Function`.
  *
  * @func
  * @memberOf R
  * @since v0.3.0
  * @category List
  * @sig [a] -> Number
- * @param {Array} list The array to inspect.
- * @return {Number} The length of the array.
+ * @param {*} list The value to inspect.
+ * @return {Number} The length of the value.
  * @example
  *
  *      R.length([]); //=> 0
  *      R.length([1, 2, 3]); //=> 3
+ *      R.length(1); //=> NaN
  */
 module.exports = _curry1(function length(list) {
   return list != null && is(Number, list.length) ? list.length : NaN;
