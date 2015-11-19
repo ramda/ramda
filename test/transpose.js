d3c5f35cf96db9a83f987d8b2a80cbcfc0744a54
kwijibo <github@keithalexander.co.uk>
2015-11-19 22:12:41 +0000

    Adds a `transpose` function to R for converting `rows' to `columns` in a 2D array structure
    The behaviour follows that of http://hackage.haskell.org/package/base/docs/Data-List.html#v:transpose
    with regard to how rows with different lengths are treated.
    
    background to this function discussed in #81 #1068 #1069

---
+++ test/transpose.js
@@ -0,0 +1,22 @@
+var R = require('..');
+var eq = require('./shared/eq');
+
+describe('transpose', function() {
+  it('returns an array of two arrays', function() {
+    var input = [['a', 1], ['b', 2], ['c', 3]];
+    eq(R.transpose(input), [['a', 'b', 'c'], [1, 2, 3]]);
+  });
+  it('skips elements when rows are shorter', function() {
+    var actual = R.transpose([[10, 11], [20], [], [30, 31, 32]]);
+    var expected = [[10, 20, 30], [11, 31], [32]];
+    eq(actual, expected);
+  });
+  it('copes with empty arrays', function() {
+    eq(R.transpose([]), []);
+  });
+  it('copes with true, false, null & undefined elements of arrays', function() {
+    var actual = R.transpose([[true, false, undefined, null], [null, undefined, false, true]]);
+    var expected = [[true, null], [false, undefined], [undefined, false], [null, true]];
+    eq(actual, expected);
+  });
+});
