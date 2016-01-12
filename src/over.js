231363772f69c837b376ebea27e0def4ddd99b1b
Scott Christopher <scott.christopher@hiveempire.com>
2016-01-12 20:40:05 +1100

    Improve comments around lens functions

--- src/over.js
+++ src/over.js
@@ -24,11 +24,16 @@ var _curry3 = require('./internal/_curry3');
  *      R.over(headLens, R.toUpper, ['foo', 'bar', 'baz']); //=> ['FOO', 'bar', 'baz']
  */
 module.exports = (function() {
+  // `Identity` is a functor that holds a single value, where `map` simply
+  // transforms the held value with the provided function.
   var Identity = function(x) {
     return {value: x, map: function(f) { return Identity(f(x)); }};
   };
 
   return _curry3(function over(lens, f, x) {
+    // The value returned by the getter function is first transformed with `f`,
+    // then set as the value of an `Identity`. This is then mapped over with the
+    // setter function of the lens.
     return lens(function(y) { return Identity(f(y)); })(x).value;
   });
 }());
