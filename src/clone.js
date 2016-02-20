746be1175bc6176ffc896ba4fdd36d59ab48164a
Asaf Romano <asaf@sent.com>
2016-02-20 19:02:26 +0200

    Optimize into to perform mutable reductions
    Closes #1656

--- src/clone.js
+++ src/clone.js
@@ -25,5 +25,5 @@ var _curry1 = require('./internal/_curry1');
 module.exports = _curry1(function clone(value) {
   return value != null && typeof value.clone === 'function' ?
     value.clone() :
-    _clone(value, [], []);
+    _clone(value, [], [], true);
 });
