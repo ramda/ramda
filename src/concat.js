b667d202fd9040ed932195b236c7c6bb10e115a9
Brad Compton <notpmoc84@hotmail.com>
2016-05-04 12:06:38 -0700

    Fixed trailing spaces

--- src/concat.js
+++ src/concat.js
@@ -6,9 +6,9 @@ var toString = require('./toString');
 
 /**
  * Returns the result of concatenating the given lists or strings.
- * 
- * Note: `R.concat` expects both arguments to be of the same type, 
- * unlike the native `Array.prototype.concat` method. It will throw 
+ *
+ * Note: `R.concat` expects both arguments to be of the same type,
+ * unlike the native `Array.prototype.concat` method. It will throw
  * an error if you `concat` an Array with a non-Array value.
  *
  * Dispatches to the `concat` method of the first argument, if present.
