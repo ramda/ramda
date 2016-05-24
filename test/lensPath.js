a0e834cd3700e425107936c5bc36d9f66b07292d
David Chambers <dc@davidchambers.me>
2016-05-24 03:27:54 -0700

    fix type errors in tests for R.lensPath (#1779)

--- test/lensPath.js
+++ test/lensPath.js
@@ -24,13 +24,13 @@ describe('lensPath', function() {
       eq(R.set(R.lensPath([]), 0, testObj), 0);
     });
     it('adds the property to the object if it doesn\'t exist', function() {
-      eq(R.set(R.lensPath('X'), 0, testObj), {a: {b: 2}, d: 3, X: 0});
+      eq(R.set(R.lensPath(['X']), 0, testObj), {a: {b: 2}, d: 3, X: 0});
       eq(R.set(R.lensPath(['a', 'X']), 0, testObj), {a: {b: 2, X: 0}, d: 3});
     });
   });
   describe('over', function() {
     it('applies function to the value of the specified object property', function() {
-      eq(R.over(R.lensPath('d'), R.inc, testObj), {a: {b: 2}, d: 4});
+      eq(R.over(R.lensPath(['d']), R.inc, testObj), {a: {b: 2}, d: 4});
       eq(R.over(R.lensPath(['a', 'b']), R.inc, testObj), {a: {b: 3}, d: 3});
       eq(R.over(R.lensPath([]), R.toPairs, testObj), [['a', {b: 2}], ['d', 3]]);
     });
@@ -41,21 +41,21 @@ describe('lensPath', function() {
   });
   describe('composability', function() {
     it('can be composed', function() {
-      var composedLens = R.compose(R.lensPath('a'), R.lensPath('b'));
+      var composedLens = R.compose(R.lensPath(['a']), R.lensPath(['b']));
       eq(R.view(composedLens, testObj), 2);
     });
   });
   describe('well behaved lens', function() {
     it('set s (get s) === s', function() {
-      eq(R.set(R.lensPath('d'), R.view(R.lensPath('d'), testObj), testObj), testObj);
+      eq(R.set(R.lensPath(['d']), R.view(R.lensPath(['d']), testObj), testObj), testObj);
       eq(R.set(R.lensPath(['a', 'b']), R.view(R.lensPath(['a', 'b']), testObj), testObj), testObj);
     });
     it('get (set s v) === v', function() {
-      eq(R.view(R.lensPath('d'), R.set(R.lensPath('d'), 0, testObj)), 0);
+      eq(R.view(R.lensPath(['d']), R.set(R.lensPath(['d']), 0, testObj)), 0);
       eq(R.view(R.lensPath(['a', 'b']), R.set(R.lensPath(['a', 'b']), 0, testObj)), 0);
     });
     it('get (set(set s v1) v2) === v2', function() {
-      var p = 'd';
+      var p = ['d'];
       var q = ['a', 'b'];
       eq(R.view(R.lensPath(p), R.set(R.lensPath(p), 11, R.set(R.lensPath(p), 10, testObj))), 11);
       eq(R.view(R.lensPath(q), R.set(R.lensPath(q), 11, R.set(R.lensPath(q), 10, testObj))), 11);
