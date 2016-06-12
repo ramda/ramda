2028b1434c4d506c110bbd99016c0b81fb25f13c
Cristiano D'Avila <cristianocd@gmail.com>
2016-06-12 15:31:40 -0300

    fix R.take extra step (#1793)
    * add sinon to devDependencies
    
    * fix R.take extra step

--- src/internal/_xtake.js
+++ src/internal/_xtake.js
@@ -2,21 +2,18 @@ var _curry2 = require('./_curry2');
 var _reduced = require('./_reduced');
 var _xfBase = require('./_xfBase');
 
-
 module.exports = (function() {
   function XTake(n, xf) {
     this.xf = xf;
     this.n = n;
+    this.i = 0;
   }
   XTake.prototype['@@transducer/init'] = _xfBase.init;
   XTake.prototype['@@transducer/result'] = _xfBase.result;
   XTake.prototype['@@transducer/step'] = function(result, input) {
-    if (this.n === 0) {
-      return _reduced(result);
-    } else {
-      this.n -= 1;
-      return this.xf['@@transducer/step'](result, input);
-    }
+    this.i += 1;
+    var ret = this.n === 0 ? result : this.xf['@@transducer/step'](result, input);
+    return this.i >= this.n ? _reduced(ret) : ret;
   };
 
   return _curry2(function _xtake(n, xf) { return new XTake(n, xf); });
--- test/take.js
+++ test/take.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var sinon = require('sinon');
 
 var R = require('..');
 var eq = require('./shared/eq');
@@ -37,6 +38,12 @@ describe('take', function() {
 
   it('handles zero correctly (#1224)', function() {
     eq(R.into([], R.take(0), [1, 2, 3]), []);
+  });
+
+  it('steps correct number of times', function() {
+    var spy = sinon.spy();
+    R.into([], R.compose(R.map(spy), R.take(2)), [1, 2, 3]);
+    sinon.assert.calledTwice(spy);
   });
 
 });
