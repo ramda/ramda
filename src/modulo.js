8769a9b0ad7f2cb75d90de5f64568b1dcfcb1dcf
Pablo Navarro Castillo <pnavarrc@gmail.com>
2016-06-15 13:30:31 -0700

    Fix error in R.modulo docstring (#1803)

--- src/modulo.js
+++ src/modulo.js
@@ -2,7 +2,7 @@ var _curry2 = require('./internal/_curry2');
 
 
 /**
- * Divides the second parameter by the first and returns the remainder. Note
+ * Divides the first parameter by the second and returns the remainder. Note
  * that this function preserves the JavaScript-style behavior for modulo. For
  * mathematical modulo see `mathMod`.
  *
