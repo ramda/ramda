382b1fa90e45789314d8f0b7de3c901af77c5154
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-09-03 21:45:11 +0200

    doc : new better simple example for R.trush

--- src/thrush.js
+++ src/thrush.js
@@ -19,21 +19,16 @@ var _curry2 = require('./internal/_curry2');
  * @return {*} Tun result of applying f on x.
  * @example
  *
+ *      // First example
  *      var t42 = R.thrush(42);
  *      t42(R.identity); //=> 42
  *      t42(R.add(1)); //=> 43
  *
- *      // with lenses
- *      var state = {
- *        firstName: "Bruce",
- *        lastName: "Wayne",
- *      };
- *      var tState = R.thrush(state);
- *      var lastName = lensProp('lastName');
+ *      // Second example
+ *      var url = { pathname: 'http://localhost/index.html', query: '?firstName=Bruce&lastName=Wayne' };
  *
- *      tState(R.view(lastName)); //=> "Wayne"
- *
- *      var overState = R.compose(tState, over);
- *      overState(lastName, R.toUpper); //=> {"firstName": "Hello", "lastName": "WORLD"}
+ *      R.thrush(url)(
+ *        R.compose(R.join(''), R.values)
+ *      ); //=> http://localhost/index.html?firstName=Bruce&lastName=Wayne
  */
 module.exports = _curry2(function thrush(x, f) { return f(x); });
