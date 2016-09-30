84db877ae9bf1b68743df06b02e7bc436b91ec92
Scott Christopher <scott.christopher@finder.com>
2016-09-30 13:41:48 +1000

    Add support for integer path segments in lensPath

--- src/lensPath.js
+++ src/lensPath.js
@@ -1,7 +1,12 @@
 var _curry1 = require('./internal/_curry1');
-var assocPath = require('./assocPath');
-var lens = require('./lens');
-var path = require('./path');
+var _isInteger = require('./internal/_isInteger');
+var _map = require('./internal/_map');
+var apply = require('./apply');
+var compose = require('./compose');
+var identity = require('./identity');
+var ifElse = require('./ifElse');
+var lensIndex = require('./lensIndex');
+var lensProp = require('./lensProp');
 
 
 /**
@@ -11,19 +16,27 @@ var path = require('./path');
  * @memberOf R
  * @since v0.19.0
  * @category Object
+ * @typedefn Idx = String | Int
  * @typedefn Lens s a = Functor f => (a -> f a) -> s -> f s
- * @sig [String] -> Lens s a
+ * @sig [Idx] -> Lens s a
  * @param {Array} path The path to use.
  * @return {Lens}
  * @see R.view, R.set, R.over
  * @example
  *
- *      var xyLens = R.lensPath(['x', 'y']);
+ *      var xHeadYLens = R.lensPath(['x', 0, 'y']);
  *
- *      R.view(xyLens, {x: {y: 2, z: 3}});            //=> 2
- *      R.set(xyLens, 4, {x: {y: 2, z: 3}});          //=> {x: {y: 4, z: 3}}
- *      R.over(xyLens, R.negate, {x: {y: 2, z: 3}});  //=> {x: {y: -2, z: 3}}
+ *      R.view(xHeadYLens, {x: [{y: 2, z: 3}, {y: 4, z: 5}]});
+ *      //=> 2
+ *      R.set(xHeadYLens, 1, {x: [{y: 2, z: 3}, {y: 4, z: 5}]});
+ *      //=> {x: [{y: 1, z: 3}, {y: 4, z: 5}]}
+ *      R.over(xHeadYLens, R.negate, {x: [{y: 2, z: 3}, {y: 4, z: 5}]});
+ *      //=> {x: [{y: -2, z: 3}, {y: 4, z: 5}]}
  */
-module.exports = _curry1(function lensPath(p) {
-  return lens(path(p), assocPath(p));
+module.exports = _curry1(function lensPath(path) {
+  if (path.length === 0) {
+    return identity;
+  } else {
+    return apply(compose, _map(ifElse(_isInteger, lensIndex, lensProp), path));
+  }
 });
