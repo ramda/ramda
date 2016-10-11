72c935d96323690d40dedc4c066a22db99d223ef
Vladimir Starkov <iamstarkov@gmail.com>
2016-10-12 00:09:18 +0200

    test: sort both api arrays before asserting them

--- test/index.js
+++ test/index.js
@@ -4,8 +4,8 @@ var eq = require('./shared/eq');
 
 
 describe('api surface', function() {
-  var exportedApi = Object.keys(R);
-  var actualApi = Object.keys(dist);
+  var exportedApi = Object.keys(R).sort();
+  var actualApi = Object.keys(dist).sort();
 
   it('exported api is in sync with an actual api', function() {
     eq(exportedApi, actualApi);
