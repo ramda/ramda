bfdf198708e2c48d51d6f096b70c8c11474d9fad
buzzdecafe <m_hur@yahoo.com>
2016-10-16 10:21:44 -0400

    document behavior of functions in chain

--- src/chain.js
+++ src/chain.js
@@ -11,12 +11,15 @@ var map = require('./map');
  *
  * Dispatches to the `chain` method of the second argument, if present,
  * according to the [FantasyLand Chain spec](https://github.com/fantasyland/fantasy-land#chain).
+ * Treats the second argument as a FantasyLand Chain, when the the second argument is a function
+ * of the form `a -> x -> b`.
  *
  * @func
  * @memberOf R
  * @since v0.3.0
  * @category List
  * @sig Chain m => (a -> m b) -> m a -> m b
+ * @sig (x -> a) -> (a -> x -> b) -> (x -> b)
  * @param {Function} fn The function to map with
  * @param {Array} list The list to map over
  * @return {Array} The result of flat-mapping `list` with `fn`
@@ -24,6 +27,9 @@ var map = require('./map');
  *
  *      var duplicate = n => [n, n];
  *      R.chain(duplicate, [1, 2, 3]); //=> [1, 1, 2, 2, 3, 3]
+ *
+ *      var appendHead = R.chain(R.head, R.append);
+ *      appendHead([1, 2, 3]); //=> [1, 2, 3, 1]
  */
 module.exports = _curry2(_dispatchable('chain', _xchain, function chain(fn, monad) {
   if (typeof monad === 'function') {
