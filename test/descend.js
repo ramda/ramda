02a4e613f098a5c85bee2f36881c03fdf41166ef
Dave Tonge <dave.tonge@momentumft.co.uk>
2016-10-19 15:22:27 +0100

    Add `ascend`, `descend` and `sortWith` methods
    See discussion in issue #994.
    `sortWith` sorts a list by an arbitrary number of comparators
    `ascend` and `descend` are helpful utility methods to create
    such comparators.

---
+++ test/descend.js
@@ -0,0 +1,8 @@
+var R = require('..');
+var eq = require('./shared/eq');
+
+describe('descend', function() {
+  it('builds a descending comparator function out of the identity function', function() {
+    eq([3, 1, 8, 1, 2, 5].sort(R.descend(R.identity)), [8, 5, 3, 2, 1, 1]);
+  });
+});
