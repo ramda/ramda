e6ed8a780876db26c6f654f8b34bb781bcc0a5ba
wangzengdi <sunnyadi@163.com>
2016-11-15 22:02:03 +0800

    flip reduceRight reducer arguments order

--- src/reduceRight.js
+++ src/reduceRight.js
@@ -37,7 +37,7 @@ var _curry3 = require('./internal/_curry3');
 module.exports = _curry3(function reduceRight(fn, acc, list) {
   var idx = list.length - 1;
   while (idx >= 0) {
-    acc = fn(acc, list[idx]);
+    acc = fn(list[idx], acc);
     idx -= 1;
   }
   return acc;
