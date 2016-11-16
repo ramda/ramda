f05d90335cdf9487c881d0562a18e0654e332673
wangzengdi <sunnyadi@163.com>
2016-11-16 20:31:46 +0800

    modify comment of reduceRight and add an unit test

--- test/reduceRight.js
+++ test/reduceRight.js
@@ -9,6 +9,10 @@ describe('reduceRight', function() {
     eq(R.reduceRight(function(a, b) {return a + b;}, '', ['a', 'b', 'c', 'd']), 'abcd');
   });
 
+  it('folds subtract over arrays in the right order', function() {
+    eq(R.reduceRight(function(a, b) {return a - b;}, 0, [1, 2, 3, 4]), -2);
+  });
+
   it('folds simple functions over arrays with the supplied accumulator', function() {
     eq(R.reduceRight(avg, 54, [12, 4, 10, 6]), 12);
   });
