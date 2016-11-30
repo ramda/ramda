fcbb62b1a93b021414127787acb5c7d65bf5275d
Scott Christopher <scott.christopher@finder.com>
2016-11-30 17:08:03 +1100

    pipeK/composeK generated fns now expect unlifted values

--- src/composeK.js
+++ src/composeK.js
@@ -1,8 +1,6 @@
 var chain = require('./chain');
 var compose = require('./compose');
-var identity = require('./identity');
 var map = require('./map');
-var prepend = require('./prepend');
 
 
 /**
@@ -15,7 +13,7 @@ var prepend = require('./prepend');
  * @memberOf R
  * @since v0.16.0
  * @category Function
- * @sig Chain m => ((y -> m z), (x -> m y), ..., (a -> m b)) -> (m a -> m z)
+ * @sig Chain m => ((y -> m z), (x -> m y), ..., (a -> m b)) -> (a -> m z)
  * @param {...Function} ...functions The functions to compose
  * @return {Function}
  * @see R.pipeK
@@ -31,10 +29,15 @@ var prepend = require('./prepend');
  *         get('address'),
  *         get('user'),
  *       );
- *       getStateCode(Maybe.of({"user":{"address":{"state":"ny"}}})); //=> Maybe.Just("NY")
- *       getStateCode(Maybe.of({})); //=> Maybe.Nothing()
- * @symb R.composeK(f, g, h)(a, b) = R.chain(f, R.chain(g, h(a, b)))
+ *       getStateCode({"user":{"address":{"state":"ny"}}}); //=> Maybe.Just("NY")
+ *       getStateCode({}); //=> Maybe.Nothing()
+ * @symb R.composeK(f, g, h)(a) = R.chain(f, R.chain(g, h(a)))
  */
 module.exports = function composeK() {
-  return compose.apply(this, prepend(identity, map(chain, arguments)));
+  if (arguments.length === 0) {
+    throw new Error('composeK requires at least one argument');
+  }
+  var init = Array.prototype.slice.call(arguments);
+  var last = init.pop();
+  return compose(compose.apply(this, map(chain, init)), last);
 };
