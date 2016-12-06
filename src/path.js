d723551aa3169e49c71d6e33b0fcd71ba226247a
FrancoisPala <FrancoisPala@users.noreply.github.com>
2016-12-06 13:53:05 +0100

    Update path.js
    Hi, I've tried to do this function recursively and found out that the performances were better (unless for very deep cases, like 5 or 6-deep keys in a object) using benchmark.js (https://github.com/bestiejs/benchmark.js/), is there any reason you guys did it iteratively instead?
    Thanks!

--- src/path.js
+++ src/path.js
@@ -19,15 +19,9 @@ var _curry2 = require('./internal/_curry2');
  *      R.path(['a', 'b'], {a: {b: 2}}); //=> 2
  *      R.path(['a', 'b'], {c: {b: 2}}); //=> undefined
  */
-module.exports = _curry2(function path(paths, obj) {
-  var val = obj;
-  var idx = 0;
-  while (idx < paths.length) {
-    if (val == null) {
-      return;
+module.exports = _curry2(function path(paths, obj, i) {
+    if (paths[i]) {
+        return path(paths, obj[paths[i]], i + 1);
     }
-    val = val[paths[idx]];
-    idx += 1;
-  }
-  return val;
+    return obj;
 });
