5996fa67383ebea769429ad97b30285972efce35
David Chambers <dc@davidchambers.me>
2017-02-03 14:00:25 +0100

    add R.innerJoin and deprecate R.intersectionWith (#1868)
    * add R.innerJoin and deprecate R.intersectionWith
    
    * removing assertion about ordering

--- src/intersection.js
+++ src/intersection.js
@@ -17,7 +17,7 @@ var uniq = require('./uniq');
  * @param {Array} list1 The first list.
  * @param {Array} list2 The second list.
  * @return {Array} The list of elements found in both `list1` and `list2`.
- * @see R.intersectionWith
+ * @see R.innerJoin
  * @example
  *
  *      R.intersection([1,2,3,4], [7,6,5,4,3]); //=> [4, 3]
---
+++ test/innerJoin.js
@@ -0,0 +1,26 @@
+var R = require('..');
+var eq = require('./shared/eq');
+
+
+var a = {id: 1, name: 'a'};
+var b = {id: 2, name: 'b'};
+var c = {id: 3, name: 'c'};
+var f = R.innerJoin(function(r, id) { return r.id === id; });
+
+
+describe('innerJoin', function() {
+
+  it('only returns elements from the first list', function() {
+    eq(f([a, b, c], []), []);
+    eq(f([a, b, c], [1]), [a]);
+    eq(f([a, b, c], [1, 2]), [a, b]);
+    eq(f([a, b, c], [1, 2, 3]), [a, b, c]);
+    eq(f([a, b, c], [1, 2, 3, 4]), [a, b, c]);
+  });
+
+  it('does not remove duplicates', function() {
+    eq(f([a, a, a], [1, 2, 3]), [a, a, a]);
+    eq(f([a, b, c], [1, 1, 1]), [a]);
+  });
+
+});
