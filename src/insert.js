61a2d00284547179770f8c6c50ba4237119d44bd
wangzengdi <sunnyadi@163.com>
2017-02-27 10:52:41 +0800

    fix insert.js typo (#2100)
    * fix insert.js typo
    
    * optimize insert.js comment

--- src/insert.js
+++ src/insert.js
@@ -2,7 +2,8 @@ var _curry3 = require('./internal/_curry3');
 
 
 /**
- * Inserts the supplied element into the list, at index `index`. _Note that
+ * Inserts the supplied element into the list, at the specified `index`. _Note that
+
  * this is not destructive_: it returns a copy of the list with the changes.
  * <small>No lists have been harmed in the application of this function.</small>
  *
