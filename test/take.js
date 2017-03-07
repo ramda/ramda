6e27ce5e29a579bf4334aa9c1c19542283b20794
David Normington <david.normington@signal-noise.com>
2017-03-07 17:42:00 +0000

    Fixed `xtake` where n < 0

--- test/take.js
+++ test/take.js
@@ -46,4 +46,10 @@ describe('take', function() {
     sinon.assert.calledTwice(spy);
   });
 
+  it('transducer called for every member of list if `n` is < 0', function() {
+    var spy = sinon.spy();
+    R.into([], R.compose(R.map(spy), R.take(-1)), [1, 2, 3]);
+    sinon.assert.calledThrice(spy);
+  });
+
 });
