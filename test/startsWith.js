6882f20fae29a37f03bcd9762b686a8fe1cc1b3a
Matt Grande <matt@weeverapps.com>
2017-03-13 19:41:33 -0400

    Add more tests

--- test/startsWith.js
+++ test/startsWith.js
@@ -6,15 +6,31 @@ describe('startsWith', function() {
     eq(R.startsWith('a', 'abc'), true);
   });
 
+  it('should return true when a long string starts with the provided value', function() {
+    eq(R.startsWith('astro', 'astrology'), true);
+  });
+
   it('should return false when a string does not start with the provided value', function() {
     eq(R.startsWith('b', 'abc'), false);
+  });
+
+  it('should return false when a long string does not start with the provided value', function() {
+    eq(R.startsWith('stro', 'astrology'), false);
   });
 
   it('should return true when an array starts with the provided value', function() {
     eq(R.startsWith(['a'], ['a', 'b', 'c']), true);
   });
 
+  it('should return true when an array starts with the provided values', function() {
+    eq(R.startsWith(['a', 'b'], ['a', 'b', 'c']), true);
+  });
+
   it('should return false when an array does not start with the provided value', function() {
     eq(R.startsWith(['b'], ['a', 'b', 'c']), false);
+  });
+
+  it('should return false when an array does not start with the provided values', function() {
+    eq(R.startsWith(['b', 'c'], ['a', 'b', 'c']), false);
   });
 });
