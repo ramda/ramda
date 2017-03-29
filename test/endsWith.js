15158a94e5a35f87bda37280d7d8ca2d2f2a7b02
Matt Grande <matt@weeverapps.com>
2017-03-29 15:56:46 -0400

    Add startsWith and endsWith functions (#2120)

---
+++ test/endsWith.js
@@ -0,0 +1,36 @@
+var R = require('..');
+var eq = require('./shared/eq');
+
+describe('startsWith', function() {
+  it('should return true when a string ends with the provided value', function() {
+    eq(R.endsWith('c', 'abc'), true);
+  });
+
+  it('should return true when a long string ends with the provided value', function() {
+    eq(R.endsWith('ology', 'astrology'), true);
+  });
+
+  it('should return false when a string does not end with the provided value', function() {
+    eq(R.endsWith('b', 'abc'), false);
+  });
+
+  it('should return false when a long string does not end with the provided value', function() {
+    eq(R.endsWith('olog', 'astrology'), false);
+  });
+
+  it('should return true when an array ends with the provided value', function() {
+    eq(R.endsWith(['c'], ['a', 'b', 'c']), true);
+  });
+
+  it('should return true when an array ends with the provided values', function() {
+    eq(R.endsWith(['b', 'c'], ['a', 'b', 'c']), true);
+  });
+
+  it('should return false when an array does not end with the provided value', function() {
+    eq(R.endsWith(['b'], ['a', 'b', 'c']), false);
+  });
+
+  it('should return false when an array does not end with the provided values', function() {
+    eq(R.endsWith(['a', 'b'], ['a', 'b', 'c']), false);
+  });
+});
---
+++ test/startsWith.js
@@ -0,0 +1,36 @@
+var R = require('..');
+var eq = require('./shared/eq');
+
+describe('startsWith', function() {
+  it('should return true when a string starts with the provided value', function() {
+    eq(R.startsWith('a', 'abc'), true);
+  });
+
+  it('should return true when a long string starts with the provided value', function() {
+    eq(R.startsWith('astro', 'astrology'), true);
+  });
+
+  it('should return false when a string does not start with the provided value', function() {
+    eq(R.startsWith('b', 'abc'), false);
+  });
+
+  it('should return false when a long string does not start with the provided value', function() {
+    eq(R.startsWith('stro', 'astrology'), false);
+  });
+
+  it('should return true when an array starts with the provided value', function() {
+    eq(R.startsWith(['a'], ['a', 'b', 'c']), true);
+  });
+
+  it('should return true when an array starts with the provided values', function() {
+    eq(R.startsWith(['a', 'b'], ['a', 'b', 'c']), true);
+  });
+
+  it('should return false when an array does not start with the provided value', function() {
+    eq(R.startsWith(['b'], ['a', 'b', 'c']), false);
+  });
+
+  it('should return false when an array does not start with the provided values', function() {
+    eq(R.startsWith(['b', 'c'], ['a', 'b', 'c']), false);
+  });
+});
