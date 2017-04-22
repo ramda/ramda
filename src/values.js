cb73507fdd5b0ec5a23467035de08266e136d082
Martin hernan Algañaraz <idcmardelplata@gmail.com>
2017-04-22 08:02:50 -0300

    Refactored function values

--- src/values.js
+++ src/values.js
@@ -18,14 +18,6 @@ var keys = require('./keys');
  *
  *      R.values({a: 1, b: 2, c: 3}); //=> [1, 2, 3]
  */
-module.exports = _curry1(function values(obj) {
-  var props = keys(obj);
-  var len = props.length;
-  var vals = [];
-  var idx = 0;
-  while (idx < len) {
-    vals[idx] = obj[props[idx]];
-    idx += 1;
-  }
-  return vals;
+module.exports = _curry1(function values(obj){
+  return keys(obj).map(function(key) { return obj [key]; });
 });
