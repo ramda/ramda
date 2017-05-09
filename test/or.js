1a5d762fd94bff313b3db3485b9fd074af27bc90
pepewritescode <pepewritescode@gmail.com>
2017-05-09 21:50:05 +0545

    Change && to || in test case for or

--- test/or.js
+++ test/or.js
@@ -3,7 +3,7 @@ var eq = require('./shared/eq');
 
 
 describe('or', function() {
-  it('compares two values with js &&', function() {
+  it('compares two values with js ||', function() {
     eq(R.or(true, true), true);
     eq(R.or(true, false), true);
     eq(R.or(false, true), true);
