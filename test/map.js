ba9ba9219179650e080158d21c30627cfb11224d
Otto Nascarella <ottoln@gmail.com>
2017-05-10 15:13:30 -0300

    fixed fantasy-land test

--- test/map.js
+++ test/map.js
@@ -67,16 +67,17 @@ describe('map', function() {
   });
 
   it('correctly uses fantasy-land implementations', function() {
-    var Stuff = function(v) {
+
+    function Stuff(v) {
       this._val = v;
-    };
+    }
 
     Stuff.prototype['fantasy-land/map'] = function(funk) {
       return new Stuff(funk(this._val));
     };
 
     var m1 = new Stuff(1);
-    var m2 = R.map(R.sum(1), m1);
+    var m2 = R.map(R.add(1), m1);
 
     eq(m1._val + 1, m2._val);
   });
