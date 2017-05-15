c3beef1d80998ecb6e17b264c71d97cdf4586e77
scriptype <scriptyper@gmail.com>
2017-05-15 22:52:51 +0300

    Fix confusing usage of R.__ in the docs of .where

--- src/where.js
+++ src/where.js
@@ -26,8 +26,8 @@ var _has = require('./internal/_has');
  *      var pred = R.where({
  *        a: R.equals('foo'),
  *        b: R.complement(R.equals('bar')),
- *        x: R.gt(__, 10),
- *        y: R.lt(__, 20)
+ *        x: R.gt(R.__, 10),
+ *        y: R.lt(R.__, 20)
  *      });
  *
  *      pred({a: 'foo', b: 'xxx', x: 11, y: 19}); //=> true
