62bddeb6a4afb81eb419a77742f4a30c2eddb529
Angelo Ocana <angeloocana@gmail.com>
2017-05-30 17:01:15 -0300

    Fix eslint space-before-function-paren
    To fix it I had to install esLint pluging to vscode.

--- test/memoize.js
+++ test/memoize.js
@@ -2,10 +2,10 @@ var R = require('..');
 var eq = require('./shared/eq');
 
 
-describe('memoize', function () {
-  it('memoizes "false" return values', function () {
+describe('memoize', function() {
+  it('memoizes "false" return values', function() {
     var count = 0;
-    var inc = R.memoize(function (n) {
+    var inc = R.memoize(function(n) {
       count += 1;
       return n + 1;
     });
@@ -15,7 +15,7 @@ describe('memoize', function () {
     eq(count, 1);
   });
 
-  it('differentiates values with same string representation', function () {
+  it('differentiates values with same string representation', function() {
     var f = R.memoize(R.toString);
     eq(f(42), '42');
     eq(f('42'), '"42"');
@@ -23,9 +23,9 @@ describe('memoize', function () {
     eq(f([['42']]), '[["42"]]');
   });
 
-  it('respects object equivalence', function () {
+  it('respects object equivalence', function() {
     var count = 0;
-    var f = R.memoize(function (x) {
+    var f = R.memoize(function(x) {
       count += 1;
       return R.toString(x);
     });
@@ -35,15 +35,15 @@ describe('memoize', function () {
     eq(count, 1);
   });
 
-  it('accepts 2 number arguments', function () {
-    const add = R.memoize(function (a, b) { return a + b; });
+  it('accepts 2 number arguments', function() {
+    const add = R.memoize(function(a, b) { return a + b; });
 
     eq(add(1, 2), 3);
     eq(add(1, 3), 4);
   });
 
-  it('accepts 2 string arguments', function () {
-    var f = R.memoize(function (concatWith, a, b) {
+  it('accepts 2 string arguments', function() {
+    var f = R.memoize(function(concatWith, a, b) {
       return a + concatWith + b;
     });
 
