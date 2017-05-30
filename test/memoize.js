5648d2585e6ca3659e7dfd130f623d6a954f3a20
Angelo Ocana <angeloocana@gmail.com>
2017-05-30 16:42:57 -0300

    Fix memoize with more than 1 arguments
    After 0.24.0 calling memoize with 2 or more arguments stoped working.
    
    issue: https://github.com/ramda/ramda/issues/2183
    
    To fix it, we call **toString** with **arguments**.

--- test/memoize.js
+++ test/memoize.js
@@ -2,10 +2,10 @@ var R = require('..');
 var eq = require('./shared/eq');
 
 
-describe('memoize', function() {
-  it('memoizes "false" return values', function() {
+describe('memoize', function () {
+  it('memoizes "false" return values', function () {
     var count = 0;
-    var inc = R.memoize(function(n) {
+    var inc = R.memoize(function (n) {
       count += 1;
       return n + 1;
     });
@@ -15,7 +15,7 @@ describe('memoize', function() {
     eq(count, 1);
   });
 
-  it('differentiates values with same string representation', function() {
+  it('differentiates values with same string representation', function () {
     var f = R.memoize(R.toString);
     eq(f(42), '42');
     eq(f('42'), '"42"');
@@ -23,16 +23,34 @@ describe('memoize', function() {
     eq(f([['42']]), '[["42"]]');
   });
 
-  it('respects object equivalence', function() {
+  it('respects object equivalence', function () {
     var count = 0;
-    var f = R.memoize(function(x) {
+    var f = R.memoize(function (x) {
       count += 1;
       return R.toString(x);
     });
-    eq(f({x: 1, y: 2}), '{"x": 1, "y": 2}');
-    eq(f({x: 1, y: 2}), '{"x": 1, "y": 2}');
-    eq(f({y: 2, x: 1}), '{"x": 1, "y": 2}');
+    eq(f({ x: 1, y: 2 }), '{"x": 1, "y": 2}');
+    eq(f({ x: 1, y: 2 }), '{"x": 1, "y": 2}');
+    eq(f({ y: 2, x: 1 }), '{"x": 1, "y": 2}');
     eq(count, 1);
   });
 
+  it('accepts 2 number arguments', function () {
+    const add = R.memoize(function (a, b) { return a + b; });
+
+    eq(add(1, 2), 3);
+    eq(add(1, 3), 4);
+  });
+
+  it('accepts 2 string arguments', function () {
+    var f = R.memoize(function (concatWith, a, b) {
+      return a + concatWith + b;
+    });
+
+    const a_bc = f('_', 'a', 'bc');
+    const ab_c = f('_', 'ab', 'c');
+
+    eq(a_bc, 'a_bc');
+    eq(ab_c, 'ab_c');
+  });
 });
