dde12104f09228c4e4e0c7012f9822441f1bacc0
Angelo Ocana <angeloocana@gmail.com>
2017-06-01 11:14:58 -0300

    Simplifies memoize test

--- test/memoize.js
+++ test/memoize.js
@@ -36,89 +36,20 @@ describe('memoize', function() {
   });
 
   it('works with 2 arguments', function() {
-    // Example from https://github.com/angeloocana/joj-core
+    var called = 0;
 
-    /**
-     * Get all valid and invalid board near positions.
-     */
-    function getAllNearPositions(position) {
-
-      function addPosition(toAdd) {
-        return {
-          x: position.x + toAdd[0],
-          y: position.y + toAdd[1]
-        };
-      }
-
-      return R.map(addPosition, [
-        [-1, -1],
-        [0, -1],
-        [1, -1],
-
-        [-1, 0],
-        [1, 0],
-
-        [-1, 1],
-        [0, 1],
-        [1, 1]
-      ]);
-    }
-
-    /**
-     * Checks if position exists in this board size.
-     */
-    var hasPositionByBoardSize = R.curry(function(boardSize, position) {
-      return position
-        && position.x >= 0 && position.y >= 0
-        && boardSize.y > position.y && boardSize.x > position.x;
+    var add = R.memoize(function(x, y) {
+      called += 1;
+      return x + y;
     });
 
-    var getNearPositions = R.memoize(function(boardSize, xy) {
-      var hasPosition = hasPositionByBoardSize(boardSize);
-      return R.filter(hasPosition, getAllNearPositions(xy));
-    });
+    eq(add(1, 2), 3);
+    eq(called, 1);
 
-    var nearPositions88_11 = getNearPositions({x: 8, y: 8}, {x: 1, y: 1});
-    var nearPositions88_11_2 = getNearPositions({x: 8, y: 8}, {x: 1, y: 1});
+    eq(add(1, 2), 3);
+    eq(called, 1);
 
-    var nearPositions88_22 = getNearPositions({x: 8, y: 8}, {x: 2, y: 2});
-
-    var nearPositions33_22 = getNearPositions({x: 3, y: 3}, {x: 2, y: 2});
-
-    var expectedNearPositions88_11 = [
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 2, y: 0},
-      {x: 0, y: 1},
-      {x: 2, y: 1},
-      {x: 0, y: 2},
-      {x: 1, y: 2},
-      {x: 2, y: 2}
-    ];
-
-    var expectedNearPositions88_22 = [
-      {x: 1, y: 1},
-      {x: 2, y: 1},
-      {x: 3, y: 1},
-      {x: 1, y: 2},
-      {x: 3, y: 2},
-      {x: 1, y: 3},
-      {x: 2, y: 3},
-      {x: 3, y: 3}
-    ];
-
-    var expectedNearPositions33_22 = [
-      {x: 1, y: 1},
-      {x: 2, y: 1},
-      {x: 1, y: 2}
-    ];
-
-    if (nearPositions88_11 !== nearPositions88_11_2) {
-      throw Error('memoize not working');
-    }
-
-    eq(nearPositions88_11, expectedNearPositions88_11);
-    eq(nearPositions88_22, expectedNearPositions88_22);
-    eq(nearPositions33_22, expectedNearPositions33_22);
+    eq(add(1, 3), 4);
+    eq(called, 2);
   });
 });
