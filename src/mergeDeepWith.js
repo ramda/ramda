e2ab0e9da604290c1594409676b4064c45b25268
wangzengdi <sunnyadi@163.com>
2017-06-06 06:49:05 +0800

    optimize mergeDeepWith comment

--- src/mergeDeepWith.js
+++ src/mergeDeepWith.js
@@ -7,8 +7,8 @@ var mergeDeepWithKey = require('./mergeDeepWithKey');
  * If a key exists in both objects:
  * - and both associated values are also objects then the values will be
  *   recursively merged.
- * - otherwise the provided function is applied to associated values using the
- *   resulting value as the new value associated with the key.
+ * - otherwise the provided function is applied to associated values, then the
+ *   resulting value will be the new value associated with the key.
  * If a key only exists in one object, the value will be associated with the key
  * of the resulting object.
  *
