6609509e43b60800bfcdc30a6ff7bb89f7ae2ea0
Anton Ovechkin <owanturist@gmail.com>
2017-06-06 14:44:05 +0700

    Scan doc was fixed

--- src/scan.js
+++ src/scan.js
@@ -9,7 +9,7 @@ var _curry3 = require('./internal/_curry3');
  * @memberOf R
  * @since v0.10.0
  * @category List
- * @sig (a,b -> a) -> a -> [b] -> [a]
+ * @sig ((a, b) -> a) -> a -> [b] -> [a]
  * @param {Function} fn The iterator function. Receives two values, the accumulator and the
  *        current element from the array
  * @param {*} acc The accumulator value.
