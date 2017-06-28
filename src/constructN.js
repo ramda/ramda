07403a51a1df15a4ae7d65a5dc0716fd9c34f1eb
alnero <darestream@gmail.com>
2017-06-28 21:34:16 +0300

    Add semicolons and some spacing between the lines

--- src/constructN.js
+++ src/constructN.js
@@ -21,16 +21,18 @@ var nAry = require('./nAry');
  *      // Variadic Constructor function
  *      function Salad() {
  *        this.ingredients = arguments;
- *      };
- *      Salad.prototype.recipe = function() {
- *        var instructions = R.map((ingredient) => 'Add a dollop of ' + ingredient, this.ingredients)
- *        return R.join('\n', instructions)
  *      }
  *
- *      var ThreeLayerSalad = R.constructN(3, Salad)
+ *      Salad.prototype.recipe = function() {
+ *        var instructions = R.map((ingredient) => 'Add a dollop of ' + ingredient, this.ingredients);
+ *        return R.join('\n', instructions);
+ *      };
+ *
+ *      var ThreeLayerSalad = R.constructN(3, Salad);
  *
  *      // Notice we no longer need the 'new' keyword, and the constructor is curried for 3 arguments.
- *      var salad = ThreeLayerSalad('Mayonnaise')('Potato Chips')('Ketchup')
+ *      var salad = ThreeLayerSalad('Mayonnaise')('Potato Chips')('Ketchup');
+ *
  *      console.log(salad.recipe());
  *      // Add a dollop of Mayonnaise
  *      // Add a dollop of Potato Chips
