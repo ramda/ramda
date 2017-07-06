a3fa07c167404eefd00be4c3d46c0f405cb5653b
Tobias Wennergren <wennergr@gmail.com>
2017-07-06 06:24:28 +0300

    Add transduce support for tap
    Tap will act as tranducer if given a transformer in
    the second parameter

--- test/tap.js
+++ test/tap.js
@@ -1,8 +1,12 @@
 var R = require('..');
 var eq = require('./shared/eq');
+var listXf = require('./helpers/listXf');
+var _curry2 = require('../src/internal/_curry2');
 
 
 describe('tap', function() {
+  var pushToList = _curry2(function(lst, x) { lst.push(x); });
+
   it('returns a function that always returns its argument', function() {
     var f = R.tap(R.identity);
     eq(typeof f, 'function');
@@ -17,4 +21,23 @@ describe('tap', function() {
     eq(sideEffect, 'string 200');
   });
 
+  it('can act as a transducer', function() {
+    var sideEffect = [];
+    var numbers = [1,2,3,4,5];
+
+    var xf = R.compose(R.map(R.identity), R.tap(pushToList(sideEffect)));
+
+    eq(R.into([], xf, numbers), numbers);
+    eq(sideEffect, numbers);
+  });
+
+  it('dispatches to transformer objects', function() {
+    var sideEffect = [];
+    var pushToSideEffect = pushToList(sideEffect);
+
+    eq(R.tap(pushToSideEffect, listXf), {
+      f: pushToSideEffect,
+      xf: listXf
+    });
+  });
 });
