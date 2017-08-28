7705b647a6c0bafd159c88aa8140070c53bdbaec
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-08-28 15:02:24 +0200

    Fixed linting problems

--- scripts/annotate-pure-calls.js
+++ scripts/annotate-pure-calls.js
@@ -1,31 +1,5 @@
 'use strict';
-var t = require('babel-types');
-
 var PURE_ANNOTATION = '#__PURE__';
-
-var variableDeclaratorVisitor = function(path) {
-  var parentPath = path.findParent(function(p) {
-    return p.isVariableDeclaration();
-  });
-
-  if (!parentPath.parentPath.isProgram()) {
-    return;
-  }
-
-  path.addComment('leading', PURE_ANNOTATION);
-};
-
-var assignmentExpressionVisitor = function(path) {
-  var parentPath = path.findParent(function(p) {
-    return p.isStatement();
-  });
-
-  if (!parentPath.parentPath.isProgram()) {
-    return;
-  }
-
-  path.addComment('leading', PURE_ANNOTATION);
-};
 
 var callExpressionVisitor = function(path) {
   var parentPath = path.findParent(function(p) {
@@ -37,18 +11,6 @@ var callExpressionVisitor = function(path) {
   }
 
   path.addComment('leading', PURE_ANNOTATION);
-
-  // var parentPath = path.parentPath;
-
-  // if (parentPath.isVariableDeclarator()) {
-  //   variableDeclaratorVisitor(path);
-  //   return;
-  // }
-
-  // if (parentPath.isAssignmentExpression()) {
-  //   assignmentExpressionVisitor(path);
-  //   return;
-  // }
 };
 
 module.exports = function() {
