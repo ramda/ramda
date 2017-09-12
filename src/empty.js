9d716b8f7f85b512275e414817be1dc20f8b1195
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-09-12 07:05:50 +0200

    style(empty): remove extra * char

--- src/empty.js
+++ src/empty.js
@@ -9,7 +9,7 @@ var _isString = require('./internal/_isString');
  * Returns the empty value of its argument's type. Ramda defines the empty
  * value of Array (`[]`), Object (`{}`), String (`''`), and Arguments. Other
  * types are supported if they define `<Type>.empty`,
- * * `<Type>.prototype.empty` or implement the
+ * `<Type>.prototype.empty` or implement the
  * [FantasyLand Monoid spec](https://github.com/fantasyland/fantasy-land#monoid).
  *
  * Dispatches to the `empty` method of the first argument, if present.
