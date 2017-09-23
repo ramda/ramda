6389c8aa780d21e01ddab543cb5ac07100692853
Otto Nascarella <otto@udacity.com>
2017-09-22 23:22:44 -0300

    refactor(path): removed unnecessary if statement

--- src/path.js
+++ src/path.js
@@ -22,10 +22,7 @@ var _curry2 = require('./internal/_curry2');
 module.exports = _curry2(function path(paths, obj) {
   var val = obj;
   var idx = 0;
-  while (idx < paths.length) {
-    if (val == null) {
-      return;
-    }
+  while (val != null && idx < paths.length) {
     val = val[paths[idx]];
     idx += 1;
   }
