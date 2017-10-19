a99df95fb359c165ca9e66a4324b2a7bd2b7323c
David Chambers <dc@davidchambers.me>
2017-10-19 17:35:38 +0200

    use _functionName

--- source/internal/_isPlainObject.js
+++ source/internal/_isPlainObject.js
@@ -1,4 +1,7 @@
+import _functionName from './_functionName';
+
+
 export default function _isPlainObject(x) {
   return Object.prototype.toString.call(x) === '[object Object]' &&
-         x.constructor.name === 'Object';
+         _functionName(x.constructor) === 'Object';
 }
