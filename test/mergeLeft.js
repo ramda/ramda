bac8d4f1025ee6e93a72db4fc9d73a5bc422dcb2
Harold Cooper <hrldcpr@gmail.com>
2017-10-23 16:51:56 -0400

    add R.mergeRight and R.mergeLeft

---
+++ test/mergeLeft.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+
+var R = require('..');
+var eq = require('./shared/eq');
+
+
+describe('mergeLeft', function() {
+  it('takes two objects, merges their own properties and returns a new object', function() {
+    var a = {w: 1, x: 2};
+    var b = {y: 3, z: 4};
+    eq(R.mergeLeft(a, b), {w: 1, x: 2, y: 3, z: 4});
+  });
+
+  it('overrides properties in the second object with properties in the first object', function() {
+    var a = {w: 1, x: 2};
+    var b = {w: 100, y: 3, z: 4};
+    eq(R.mergeLeft(a, b), {w: 1, x: 2, y: 3, z: 4});
+  });
+
+  it('is not destructive', function() {
+    var a = {w: 1, x: 2};
+    var res = R.mergeLeft(a, {x: 3, y: 4});
+    assert.notStrictEqual(a, res);
+    eq(res, {w: 1, x: 2, y: 4});
+  });
+
+  it('reports only own properties', function() {
+    var a = {w: 1, x: 2};
+    function Cla() {}
+    Cla.prototype.x = 5;
+    eq(R.mergeLeft(new Cla(), a), {w: 1, x: 2});
+    eq(R.mergeLeft(a, new Cla()), {w: 1, x: 2});
+  });
+
+  it('is shallow', function() {
+    var a = { x: { u: 1, v: 2 }, y: 0 };
+    var b = { x: { u: 3, w: 4 }, z: 0 };
+    var res = R.mergeLeft(a, b);
+    assert.strictEqual(a.x, res.x);
+    eq(res, { x: { u: 1, v: 2 }, y: 0, z: 0 });
+  });
+
+});
---
+++ test/mergeRight.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+
+var R = require('..');
+var eq = require('./shared/eq');
+
+
+describe('mergeRight', function() {
+  it('takes two objects, merges their own properties and returns a new object', function() {
+    var a = {w: 1, x: 2};
+    var b = {y: 3, z: 4};
+    eq(R.mergeRight(a, b), {w: 1, x: 2, y: 3, z: 4});
+  });
+
+  it('overrides properties in the first object with properties in the second object', function() {
+    var a = {w: 1, x: 2};
+    var b = {w: 100, y: 3, z: 4};
+    eq(R.mergeRight(a, b), {w: 100, x: 2, y: 3, z: 4});
+  });
+
+  it('is not destructive', function() {
+    var a = {w: 1, x: 2};
+    var res = R.mergeRight(a, {x: 5});
+    assert.notStrictEqual(a, res);
+    eq(res, {w: 1, x: 5});
+  });
+
+  it('reports only own properties', function() {
+    var a = {w: 1, x: 2};
+    function Cla() {}
+    Cla.prototype.x = 5;
+    eq(R.mergeRight(new Cla(), a), {w: 1, x: 2});
+    eq(R.mergeRight(a, new Cla()), {w: 1, x: 2});
+  });
+
+  it('is shallow', function() {
+    var a = { x: { u: 1, v: 2 }, y: 0 };
+    var b = { x: { u: 3, w: 4 }, z: 0 };
+    var res = R.mergeRight(a, b);
+    assert.strictEqual(b.x, res.x);
+    eq(res, { x: { u: 3, w: 4 }, y: 0, z: 0 });
+  });
+
+});
