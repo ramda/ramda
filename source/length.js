e8921cf74478e13715e863618c58d889934c4c57
Andrew Smith <andrewsmith@alumni.stanford.edu>
2018-01-26 14:49:57 -0800

    Update #length docs to demonstrate function as parameter.

--- source/length.js
+++ source/length.js
@@ -9,13 +9,14 @@ import _isNumber from './internal/_isNumber';
  * @memberOf R
  * @since v0.3.0
  * @category List
- * @sig [a] -> Number
- * @param {Array} list The array to inspect.
- * @return {Number} The length of the array.
+ * @sig [a] | (*… → *) -> Number
+ * @param {Array} item The array or function to inspect.
+ * @return {Number} The length of the array or the expected number of parameters for the function.
  * @example
  *
  *      R.length([]); //=> 0
  *      R.length([1, 2, 3]); //=> 3
+ *      R.length((a, b) => {}); //=> 2
  */
 var length = _curry1(function length(list) {
   return list != null && _isNumber(list.length) ? list.length : NaN;
