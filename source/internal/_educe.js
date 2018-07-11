18ff3c08e426be124d8eb163afbfbc754db77ca0
Wojciech Pawlik <woj.pawlik@gmail.com>
2018-07-11 17:52:10 +0200

    Add _educe

---
+++ source/internal/_educe.js
@@ -0,0 +1,22 @@
+import _stepCat from './_stepCat';
+
+const toArray = _stepCat([]);
+
+export default function _educe(td1, input) {
+  return {
+    transduce: td2 => _educe(xf => td1(td2(xf)), input),
+    *[Symbol.iterator]() {
+      const transformer = td1(toArray);
+      for (const item of input) {
+        const result = transformer['@@transducer/step']([], item);
+        if (result && result['@@transducer/reduced']) {
+          yield * result['@@transducer/value'];
+          break;
+        } else {
+          yield * result;
+        }
+      }
+      yield * transformer['@@transducer/result']([]);
+    }
+  };
+}
