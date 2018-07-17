acbae8f7739c89963e37ad332481a194b6701ae9
Nikhil Thomas <nikhil@nthomas.org>
2018-07-17 19:54:58 -0400

    make R.propSatisfies return false for null or undefined arguments

--- test/propSatisfies.js
+++ test/propSatisfies.js
@@ -14,4 +14,9 @@ describe('propSatisfies', function() {
     eq(R.propSatisfies(isPositive, 'y', {x: 1, y: 0}), false);
   });
 
+  it('returns false if given a null or undefined object', function() {
+    eq(R.propSatisfies(isPositive, 'y', null), false);
+    eq(R.propSatisfies(isPositive, 'y', undefined), false);
+  });
+
 });
