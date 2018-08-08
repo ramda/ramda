69d82cf733daf2c0e78f0fd3bc4b526aecba439e
GingerPlusPlus <woj.pawlik@gmail.com>
2018-08-08 15:52:29 +0200

    Remove placeholder

--- test/invariants.js
+++ test/invariants.js
@@ -14,17 +14,7 @@ describe('invariants', function() {
     }
   });
 
-  it('-- applying function f with length n (where n > 0) to R.__ gives function with length n', function() {
-    for (var prop in R) {
-      if (typeof R[prop] === 'function' && R[prop].length > 0) {
-        var result = R[prop](R.__);
-        eq(typeof result, 'function');
-        eq(result.length, R[prop].length);
-      }
-    }
-  });
-
-  it('-- applying function f with length n (where n > 1) to any value other than R.__ gives function with length n - 1', function() {
+  it('-- applying function f with length n (where n > 1) to any value gives function with length n - 1', function() {
     var testPartialApplication = function(fn, name) {
       if (fn.length > 1) {
         var result = fn(null);
