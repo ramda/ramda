aa73bd14ac29d0efad836a517ae506babafcccfc
bidhan-a <bidhan619@gmail.com>
2018-10-02 20:52:49 +0545

    Fix messages in test cases for gt, gte, and lte

--- test/gt.js
+++ test/gt.js
@@ -3,7 +3,7 @@ var eq = require('./shared/eq');
 
 
 describe('gt', function() {
-  it('reports whether one item is less than another', function() {
+  it('reports whether one item is greater than another', function() {
     eq(R.gt(3, 5), false);
     eq(R.gt(6, 4), true);
     eq(R.gt(7.0, 7.0), false);
--- test/lte.js
+++ test/lte.js
@@ -3,7 +3,7 @@ var eq = require('./shared/eq');
 
 
 describe('lte', function() {
-  it('reports whether one item is less than another', function() {
+  it('reports whether one item is less than or equal to another', function() {
     eq(R.lte(3, 5), true);
     eq(R.lte(6, 4), false);
     eq(R.lte(7.0, 7.0), true);
