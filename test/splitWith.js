d0bd12feb6e6b3635a88f401ec5665933db1d89e
JMARL1 <james.marlowe@monsanto.com>
2018-11-02 22:08:43 -0500

    clearer test

--- test/splitWith.js
+++ test/splitWith.js
@@ -2,10 +2,10 @@ var R = require('../source');
 var eq = require('./shared/eq');
 
 
-describe.only('splitWith', function() {
+describe('splitWith', function() {
   it('splits an array at every instance of predicate matching', function() {
     eq(R.splitWith(R.equals(2), [1, 2, 3, 4, 2, 1]), [[1], [3, 4], [1]]);
-    eq(R.splitWith(R.equals('a'), 'bananas'), [['b'],['n'],['n'],['s']]);
+    eq(R.splitWith(R.equals('a'), ['b','a','n','a','n','a','s']), [['b'],['n'],['n'],['s']]);
     eq(R.splitWith(R.gt(5), [1, 7, 5, 4, 3, 7, 6]), [[7, 5], [7, 6]]);
     eq(R.splitWith(R.propEq('x', 5), [{x:1}, {x:5}, {x:7}]), [[{x:1}], [{x:7}]]);
   });
