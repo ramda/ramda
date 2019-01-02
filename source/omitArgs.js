604f3e9ce6662773c2a9dea68b32e77e78111167
Juuso Mikkonen <juuso.mikkonen@reaktor.com>
2019-01-02 17:48:37 +0200

    Convert Array.from to Array.prototype.slice for better compatibility

--- source/omitArgs.js
+++ source/omitArgs.js
@@ -32,17 +32,17 @@ var omitArgs = _curry2(function omitArgs(n, fn) {
   }
   var arity = n + fn.length;
   switch (arity) {
-    case 0: return function() { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 1: return function(a0) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 2: return function(a0, a1) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 3: return function(a0, a1, a2) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 4: return function(a0, a1, a2, a3) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 5: return function(a0, a1, a2, a3, a4) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 6: return function(a0, a1, a2, a3, a4, a5) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 7: return function(a0, a1, a2, a3, a4, a5, a6) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) { return fn.apply(this, Array.from(arguments).slice(n)); };
-    case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) { return fn.apply(this, Array.from(arguments).slice(n)); };
+    case 0: return function() { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 1: return function(a0) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 2: return function(a0, a1) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 3: return function(a0, a1, a2) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 4: return function(a0, a1, a2, a3) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 5: return function(a0, a1, a2, a3, a4) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 6: return function(a0, a1, a2, a3, a4, a5) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 7: return function(a0, a1, a2, a3, a4, a5, a6) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
+    case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) { return fn.apply(this, Array.prototype.slice.call(arguments).slice(n)); };
     default: throw new Error('The increased arity of the function passed to omitArgs must be no greater than ten');
   }
 });
