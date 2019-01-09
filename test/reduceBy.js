f4c6b6232935e4ce790c34f76ee2fa1201fa19f4
beraliv <beraliv.spb@gmail.com>
2019-01-09 03:41:54 +0300

    prod: reduceBy splits into mutation-free groups (#2757)

--- test/reduceBy.js
+++ test/reduceBy.js
@@ -41,6 +41,36 @@ describe('reduceBy', function() {
     });
   });
 
+  it('splits the list into mutation-free groups', function() {
+    var grade = function(score) {
+      return (score < 65) ? 'F' : (score < 70) ? 'D' : (score < 80) ? 'C' : (score < 90) ? 'B' : 'A';
+    };
+    var students = [
+      {name: 'Abby', score: 84},
+      {name: 'Brad', score: 73},
+      {name: 'Chris', score: 89},
+      {name: 'Dianne', score: 99},
+      {name: 'Eddy', score: 58},
+      {name: 'Fred', score: 67},
+      {name: 'Gillian', score: 91},
+      {name: 'Hannah', score: 78},
+      {name: 'Irene', score: 85},
+      {name: 'Jack', score: 69}
+    ];
+    var byGrade = function(student) {return grade(student.score || 0);};
+    var collectNames = function(acc, student) {
+      acc.push(student.name);
+      return acc;
+    };
+    eq(R.reduceBy(collectNames, [], byGrade, students), {
+      A: ['Dianne', 'Gillian'],
+      B: ['Abby', 'Chris', 'Irene'],
+      C: ['Brad', 'Hannah'],
+      D: ['Fred', 'Jack'],
+      F: ['Eddy']
+    });
+  });
+
   it('returns an empty object if given an empty array', function() {
     eq(R.reduceBy(sumValues, 0, byType, []), {});
   });
@@ -54,5 +84,4 @@ describe('reduceBy', function() {
       sumInput
     ), {A: 800, B: 800, C: 500});
   });
-
 });
