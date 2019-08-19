ac01ed712e4a06e88c3f1d54e108f3c474af9dbe
mudit panwar <mudit.panwar@paytm.com>
2019-08-20 03:08:35 +0530

    fixes

--- test/splitWhenever.js
+++ test/splitWhenever.js
@@ -7,7 +7,6 @@ describe('splitWhenever', function() {
     eq(R.splitWhenever(R.equals(2), [1, 2, 3, 2, 4, 5, 2, 6, 7]), [[1], [3], [4,5], [6,7]]);
     eq(R.splitWhenever(R.equals(2), [1, 2, 1]), [[1], [1]]);
     eq(R.splitWhenever(R.equals(1), [1, 2, 1]), [[2]]);
-    eq(R.splitWhenever((e) => e % 2 === 0 ? true : false, [1, 2, 3, 2, 4, 5, 2, 6, 7]),[[1], [3], [5], [7]]);
   });
 });
 describe('splitWhenever', function() {
