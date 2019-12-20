29d2715f036984cbcaa800ee07277adefd25be17
adispring <sunnyadi@163.com>
2019-12-20 11:05:30 +0800

    optimize _createPartialApplicator to avoid passing in wrong count of arguments

--- source/internal/_createPartialApplicator.js
+++ source/internal/_createPartialApplicator.js
@@ -4,8 +4,10 @@ import _curry2 from './_curry2';
 
 export default function _createPartialApplicator(concat) {
   return _curry2(function(fn, args) {
-    return _arity(Math.max(0, fn.length - args.length), function() {
-      return fn.apply(this, concat(args, arguments));
+    var arity = Math.max(0, fn.length - args.length);
+    return _arity(arity, function() {
+      var neededArgs = Array.prototype.slice.call(arguments, 0, arity);
+      return fn.apply(this, concat(args, neededArgs));
     });
   });
 }
