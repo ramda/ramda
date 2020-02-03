51bddb437a272e65a552dcbe54b86892d1af606d
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-02-03 22:07:25 +0100

    add tests

--- test/prop.js
+++ test/prop.js
@@ -11,6 +11,36 @@ describe('prop', function() {
     eq(nm(fred), 'Fred');
   });
 
+  it('shows the same behaviour as path for a nonexistent property', function() {
+    var propResult = R.prop('incorrect', fred);
+    var pathResult = R.path(['incorrect'], fred);
+    eq(propResult, pathResult);
+  })
+
+  it('shows the same behaviour as path for an undefined property', function() {
+    var propResult = R.prop(undefined, fred);
+    var pathResult = R.path([undefined], fred);
+    eq(propResult, pathResult);
+  })
+
+  it('shows the same behaviour as path for a null property', function() {
+    var propResult = R.prop(null, fred);
+    var pathResult = R.path([null], fred);
+    eq(propResult, pathResult);
+  })
+
+  it('shows the same behaviour as path for a valid property and object', function() {
+    var propResult = R.prop('age', fred);
+    var pathResult = R.path(['age'], fred);
+    eq(propResult, pathResult);
+  })
+
+  it('shows the same behaviour as path for a null object', function() {
+    var propResult = R.prop('age', null);
+    var pathResult = R.path(['age'], null);
+    eq(propResult, pathResult);
+  })
+
   it('shows the same behaviour as path for an undefined object', function() {
     var propResult, propException, pathResult, pathException;
     try {
