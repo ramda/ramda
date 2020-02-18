bf6b250fef0ff24d56d61b608313af195ce47ea2
danillo.lisboa <danillosl@gmail.com>
2020-02-18 20:26:27 -0300

    adding _ObjectMap

--- source/internal/_clone.js
+++ source/internal/_clone.js
@@ -10,7 +10,10 @@ import type from '../type.js';
  * @param {Boolean} deep Whether or not to perform deep cloning.
  * @return {*} The copied value.
  */
-export default function _clone(value, deep, map = new Map()) {
+export default function _clone(value, deep, map) {
+
+  map || (map = new _ObjectMap());
+
   // this avoids the slower switch with a quick if decision removing some milliseconds in each run.
   if (_isPrimitive(value)) {
     return value;
@@ -23,6 +26,7 @@ export default function _clone(value, deep, map = new Map()) {
     if (cachedCopy) {
       return cachedCopy;
     }
+
     map.set(value, copiedValue);
 
     for (var key in value) {
@@ -58,3 +62,61 @@ function _isPrimitive(param) {
   var type = typeof param;
   return param == null || (type != 'object' && type != 'function');
 }
+
+function _ObjectMap() {
+  this.map = {};
+  this.length = 0;
+}
+
+_ObjectMap.prototype.set = function(key, value) {
+  const hashedKey = this.hash(key);
+
+  let bucket = this.map[hashedKey];
+  if (!bucket) {
+    this.map[hashedKey] = bucket = [];
+  }
+
+  bucket.push([key, value]);
+  this.length += 1;
+};
+
+_ObjectMap.prototype.hash = function(key) {
+  let hashedKey = [];
+  for (var value in key) {
+    hashedKey.push(key[value]);
+  }
+  return hashedKey.join();
+};
+
+_ObjectMap.prototype.get = function(key) {
+
+  /**
+   * depending on the number of objects to be cloned is faster to just iterate over the items in the map just because the hash function is so costly,
+   * on my tests this number is 180, anything above that using the hash function is faster.
+   */
+  if (this.length <= 180) {
+
+    for (const p in this.map) {
+      const bucket = this.map[p];
+
+      for (let i = 0; i < bucket.length; i += 1) {
+        const element = bucket[i];
+        if (element[0] === key) {return element[1];}
+      }
+
+    }
+    return;
+  }
+
+  const hashedKey = this.hash(key);
+  const bucket = this.map[hashedKey];
+
+  if (!bucket) {return;}
+
+  for (let i = 0; i < bucket.length; i += 1) {
+    const element = bucket[i];
+    if (element[0] === key) {return element[1];}
+  }
+
+};
+
