6bee25756641721da990d7798d6223429a90955b
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2020-03-27 20:56:41 +0100

    adds fantasy-land/filter test

--- test/filter.js
+++ test/filter.js
@@ -1,5 +1,6 @@
 var R = require('../source');
 var eq = require('./shared/eq');
+var Maybe = require('./shared/Maybe');
 
 
 describe('filter', function() {
@@ -29,6 +30,13 @@ describe('filter', function() {
   it('dispatches to passed-in non-Array object with a `filter` method', function() {
     var f = {filter: function(f) { return f('called f.filter'); }};
     eq(R.filter(function(s) { return s; }, f), 'called f.filter');
+  });
+
+  it('correctly uses fantasy-land implementations', function() {
+    var m1 = Maybe.Just(-1);
+    var m2 = R.filter(function(x) { return x > 0; } , m1);
+
+    eq(m2.isNothing, true);
   });
 
 });
