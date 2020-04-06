42d5243f624714eadf65ae6cd68d28c8995dd89a
adispring <sunnyadi@163.com>
2020-04-06 17:28:40 +0800

    optimize binary signature

--- source/binary.js
+++ source/binary.js
@@ -11,7 +11,7 @@ import nAry from './nAry';
  * @memberOf R
  * @since v0.2.0
  * @category Function
- * @sig (* -> c) -> (a, b -> c)
+ * @sig (* -> c) -> ((a, b) -> c)
  * @param {Function} fn The function to wrap.
  * @return {Function} A new function wrapping `fn`. The new function is guaranteed to be of
  *         arity 2.
