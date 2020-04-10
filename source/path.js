b2b6e3b01ad34c34b6c874858c4c64378ed3a660
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-04-09 21:30:11 -0700

    Add docs to `path`.

--- source/path.js
+++ source/path.js
@@ -2,7 +2,9 @@ import _curry2 from './internal/_curry2.js';
 import paths from './paths.js';
 
 /**
- * Retrieve the value at a given path.
+ * Retrieve the value at a given path. String `Idx` that could be parsed as
+ * non-negative integers has the same lookup behavior as their Integer
+ * counterparts. Non-negative integer `Idx` only work on an Array `obj`.
  *
  * @func
  * @memberOf R
@@ -11,7 +13,7 @@ import paths from './paths.js';
  * @typedefn Idx = String | Int | Symbol
  * @sig [Idx] -> {a} -> a | Undefined
  * @param {Array} path The path to use.
- * @param {Object} obj The object to retrieve the nested property from.
+ * @param {Object} obj The object or array to retrieve the nested property from.
  * @return {*} The data at `path`.
  * @see R.prop, R.nth
  * @example
@@ -20,6 +22,8 @@ import paths from './paths.js';
  *      R.path(['a', 'b'], {c: {b: 2}}); //=> undefined
  *      R.path(['a', 'b', 0], {a: {b: [1, 2, 3]}}); //=> 1
  *      R.path(['a', 'b', -2], {a: {b: [1, 2, 3]}}); //=> 2
+ *      R.path([2], {'2': 2}); //=> 2
+ *      R.path([-2], {'-2': 'a'}); //=> undefined
  */
 
 var path = _curry2(function path(pathAr, obj) {
