1e0e5006129f0369700308135f9f0a28fae617ed
Henry Ivry <henry.ivry@gmail.com>
2020-07-22 09:56:21 -0400

    feat: add partialObject (#3047)
    * feat: add curryN with named param support
    
    * fix: fix linting issues
    
    * fix: switch arg signature and logic
    
    * fix: switch destructure to Object assign
    
    * fix: adjust tests, use hasPaths instead of falsey
    
    * fix: remove trailing space
    
    * fix: adjust tests to work with hasPath
    
    * fix: cleanup/add property tests, wrap in curry2, drop placeholders
    
    * remove unused import
    
    * switch to partialObject and add tests
    
    * clean up test
    
    * restore package-lock from develop
    
    * fix typo
    
    Co-authored-by: Gitea <gitea@fake.local>

---
+++ test/partialObject.js
@@ -0,0 +1,35 @@
+var R = require('../source');
+var eq = require('./shared/eq');
+
+describe('partialObject', function() {
+  var discriminant = function({ a, b, c }) {
+    return b * b - 4 * a * c;
+  };
+
+  it('caches the initially supplied arguments', function() {
+    var f = R.partialObject(discriminant, { a: 3 });
+    eq(f({ b: 7, c: 4 }), 1);
+
+    var g = R.partialObject(discriminant, { a: 3, b: 7 });
+    eq(g({ c: 4 }), 1);
+  });
+
+  it('works regardless of property order', function() {
+    var f = R.partialObject(discriminant, { b: 7 });
+    eq(f({ c: 4, a: 3 }), 1);
+
+    var g = R.partialObject(discriminant, { a: 3 });
+    var h = R.partialObject(g, { c: 4 });
+    eq(h({ b: 7 }), 1);
+  });
+
+  it('merges deeply right', function() {
+    var nestedDiscriminant = function ({ a, c, d}) {
+      return d.b * d.b - 4 * a * c;
+    };
+
+    var f = R.partialObject(nestedDiscriminant, { a: 0, d: { b: 0 } });
+    var g = R.partialObject(f, { a: 3, d: { b: 7 } });
+    eq(g({ c: 4 }), 1);
+  });
+});
