60b1b80a7917598f5b3bb23c20beb8f971a8f72a
Brad Compton (he/him) <notpmoc84@hotmail.com>
2020-08-26 11:57:45 -0700

    added links between groupBy and indexBy (#3080)
    Co-authored-by: Brad Compton <bcompton@skyslope.com>

--- source/indexBy.js
+++ source/indexBy.js
@@ -18,6 +18,7 @@ import reduceBy from './reduceBy.js';
  * @param {Function} fn Function :: a -> Idx
  * @param {Array} array The array of objects to index
  * @return {Object} An object indexing each array element by the given property.
+ * @see R.groupBy
  * @example
  *
  *      const list = [{id: 'xyz', title: 'A'}, {id: 'abc', title: 'B'}];
