48a6def37115205b7dfe55ce2213ebc905f67e9b
liquanwei <liquanwei@corp.netease.com>
2020-09-23 11:07:12 +0800

    feat: testcase

--- test/memoizeLRUWith.js
+++ test/memoizeLRUWith.js
@@ -6,7 +6,7 @@ var eq = require('./shared/eq');
 describe('memoizeLRUWith', function() {
   it('calculates the value for a given input only once when not exceed lru size', function() {
     var ctr = 0;
-    var fib = memoizeLRUWith(177, R.identity, function(n) {
+    var fib = memoizeLRUWith(11, R.identity, function(n) {
       ctr += 1;
       return n < 2 ? n : fib(n - 2) + fib(n - 1);
     });
@@ -17,13 +17,21 @@ describe('memoizeLRUWith', function() {
 
   it('calculates the value for a given input will twice when exceed lru size', function() {
     var ctr = 0;
-    var fib = memoizeLRUWith(176, R.identity, function(n) {
+    var fib = memoizeLRUWith(10, R.identity, function(n) {
       ctr += 1;
       return n < 2 ? n : fib(n - 2) + fib(n - 1);
     });
     var result = fib(10);
     eq(result, 55);
     eq(ctr, 11); // fib(0), fib(1), ... fib(10), no memoization would take 177 iterations.
+
+    fib(0);
+    eq(ctr, 12); // fib(0) discard. and after fib(0) then fib(1) is discard. fib(2) still in memory
+    fib(0);
+    fib(2);
+    eq(ctr, 12);
+    fib(1);
+    eq(ctr, 13);
   });
 
   it('handles multiple parameters', function() {
