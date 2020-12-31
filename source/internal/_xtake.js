9ecb67bad471c53136dc01359d98abe5933b77e0
adispring <sunnyadi@163.com>
2020-12-31 22:18:45 +0800

    if n equals 0, _xtake use _reduced to short circuit the iteration.

--- source/internal/_xtake.js
+++ source/internal/_xtake.js
@@ -2,7 +2,6 @@ import _curry2 from './_curry2.js';
 import _reduced from './_reduced.js';
 import _xfBase from './_xfBase.js';
 
-
 function XTake(n, xf) {
   this.xf = xf;
   this.n = n;
@@ -12,7 +11,7 @@ XTake.prototype['@@transducer/init'] = _xfBase.init;
 XTake.prototype['@@transducer/result'] = _xfBase.result;
 XTake.prototype['@@transducer/step'] = function(result, input) {
   this.i += 1;
-  var ret = this.n === 0 ? result : this.xf['@@transducer/step'](result, input);
+  var ret = this.n === 0 ? _reduced(result) : this.xf['@@transducer/step'](result, input);
   return this.n >= 0 && this.i >= this.n ? _reduced(ret) : ret;
 };
 
