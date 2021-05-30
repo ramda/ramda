41433e40ebc6cb114a9456b207775c6ac543a73f
hungdt8796 <82578479+hungdaoelsevier@users.noreply.github.com>
2021-05-30 14:10:22 +0700

    Update the document

--- source/retry.js
+++ source/retry.js
@@ -8,8 +8,14 @@ import _curry3 from './internal/_curry3.js';
  * @param {Function} fn: The function to invoke. It should return a true or false.
  * @param {Number} wait: The number of milliseconds to delay.
  * @param {Object} options: Optional Options.
- *** @property {Number} options.max: With optional option `max` -> define the maximum number of times to invoke the function to get a success response.
- *** If null it will retry forever.
+ *   @property {Number} options.max: With optional option `max` -> define the maximum number of times to invoke the function to get a success response.
+ *   If null it will retry forever.
+ * @example
+ * let f = () => {
+ *   let d = new Date(); // current time
+ *   return d.getMilliseconds() % 2 == 0; // => true or false
+ * };
+ * R.retry(f, 1000, {max: 3}); // =>  true or false after max of 3 times
  */
 var retry = _curry3(function(fn, wait = 0, { max = Infinity }) {
   var maxTryTimes = max;
