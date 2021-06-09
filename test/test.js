81b8f570771c623d7202f78a3e3413b7755f5b2f
HoKangInfo <hokang.info@gmail.com>
2021-06-09 01:38:43 +0000

    add dotAll flag test case

--- test/test.js
+++ test/test.js
@@ -5,6 +5,13 @@ var eq = require('./shared/eq');
 
 
 describe('test', function() {
+  it('returns true if string matches dotAll pattern', function() {
+    eq(R.test(/x.*z/s, 'x.\nyz'), true);  /* eslint-env es2018 */
+  });
+
+  it('returns true if string matches dotAll pattern', function() {
+    eq(R.test(/x.*z/s, 'x.\nyz'), true);
+  });
 
   it('returns true if string matches pattern', function() {
     eq(R.test(/^x/, 'xyz'), true);
