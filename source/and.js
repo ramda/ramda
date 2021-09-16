d0e72a81dc077440eac176c918746d40e2199321
Kara Jovian <k@rina.wtf>
2021-09-16 21:33:15 +0300

    Move detailed spec to description section (#3170)
    * Move detailed spec to description section
    
    * Supplement description for `and`
    
    * Use detailed description for `or`

--- source/and.js
+++ source/and.js
@@ -2,7 +2,8 @@ import _curry2 from './internal/_curry2.js';
 
 
 /**
- * Returns `true` if both arguments are `true`; `false` otherwise.
+ * Returns the first argument if it is falsy, otherwise the second argument.
+ * Acts as the boolean `and` statement if both inputs are `Boolean`s.
  *
  * @func
  * @memberOf R
@@ -11,7 +12,7 @@ import _curry2 from './internal/_curry2.js';
  * @sig a -> b -> a | b
  * @param {Any} a
  * @param {Any} b
- * @return {Any} the first argument if it is falsy, otherwise the second argument.
+ * @return {Any}
  * @see R.both, R.or
  * @example
  *
