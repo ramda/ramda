f17ec0c54e9209dca1db3d3d228730dd6f47532b
ready-research <72916209+ready-research@users.noreply.github.com>
2021-09-16 20:27:37 +0530

    Update trim.js

--- source/trim.js
+++ source/trim.js
@@ -1,7 +1,7 @@
 import _curry1 from './internal/_curry1.js';
 
 
-var ws = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003' +
+var ws = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003' +
          '\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028' +
          '\u2029\uFEFF';
 var zeroWidth = '\u200b';
@@ -23,7 +23,9 @@ var hasProtoTrim = (typeof String.prototype.trim === 'function');
  */
 var trim = !hasProtoTrim || ws.trim() || !zeroWidth.trim() ?
   _curry1(function trim(str) {
-    return str.trim ? str.trim() : str.replace(/^\s${ws}+|\s${ws}+$/g, '');
+    var beginRx = new RegExp('^[' + ws + '][' + ws + ']*');
+    var endRx = new RegExp('[' + ws + '][' + ws + ']*$');
+    return str.replace(beginRx, '').replace(endRx, '');
   }) :
   _curry1(function trim(str) {
     return str.trim();
