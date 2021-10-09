39044248d18f2605dd11b03c9ae0a1e7abe6937e
Femi Oladipo <fem_o@hotmail.com>
2021-10-09 15:50:48 +0100

    Test R.equals semantics

--- test/nthEq.js
+++ test/nthEq.js
@@ -5,7 +5,7 @@ var eq = require('./shared/eq');
 
 
 describe('nthEq', function() {
-  var list1 = ['apple', 'oranges', 'pears'];
+  var list1 = ['apples', 'oranges', 'pears'];
   var list2 = ['books', 'pens', 'rulers'];
 
   it('determines whether a particular offset matches a given value for a specific list', function() {
@@ -14,17 +14,17 @@ describe('nthEq', function() {
     eq(R.nthEq(1, 'paper', list2), false);
   });
 
-  // it('has R.equals semantics', function() {
-  //   function Just(x) { this.value = x; }
-  //   Just.prototype.equals = function(x) {
-  //     return x instanceof Just && R.equals(x.value, this.value);
-  //   };
+  it('has R.equals semantics', function() {
+    function Just(x) { this.value = x; }
+    Just.prototype.equals = function(x) {
+      return x instanceof Just && R.equals(x.value, this.value);
+    };
 
-  //   eq(R.propEq('value', 0, {value: -0}), false);
-  //   eq(R.propEq('value', -0, {value: 0}), false);
-  //   eq(R.propEq('value', NaN, {value: NaN}), true);
-  //   eq(R.propEq('value', new Just([42]), {value: new Just([42])}), true);
-  // });
+    eq(R.nthEq(0, 0, [-0]), false);
+    eq(R.nthEq(0, -0, [0]), false);
+    eq(R.nthEq(0, NaN, [NaN]), true);
+    eq(R.nthEq(0, new Just([42]), [new Just([42])]), true);
+  });
 
   it('it has the same behavior as nth for null or undefined', function() {
     assert.throws(function() { R.nthEq(0, '', null); }, TypeError);
