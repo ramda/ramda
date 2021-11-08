340e4a2df7f7c31e05783a76c2263a88d9fd08de
Behram Kapadia <behram.kapadia@me.com>
2021-11-08 15:00:50 -0500

    removing parens in for val in docs

--- source/is.js
+++ source/is.js
@@ -2,9 +2,9 @@ import _curry2 from './internal/_curry2.js';
 
 
 /**
- * See if an object (`val`) is an instance of the supplied constructor. This
+ * See if an object `val` is an instance of the supplied constructor. This
  * function will check up the inheritance chain, if any. 
- * If (`val`) was created using `Object.create`, `R.is(Object, val) === true`.
+ * If `val` was created using `Object.create`, `R.is(Object, val) === true`.
  *
  * @func
  * @memberOf R
