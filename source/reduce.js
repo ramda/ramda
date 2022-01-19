a5957d0aa2a791dcfaa86efceb490fc94236ca5b
Scott Sauyet <scott@sauyet.com>
2022-01-18 20:29:18 -0500

    docs: Warn about mutating accumulators
    Fixes #3200

--- source/reduce.js
+++ source/reduce.js
@@ -18,6 +18,12 @@ import _reduce from './internal/_reduce.js';
  * on this behavior, see:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#Description
  *
+ * Be cautious of mutating and returning the accumulator. If you reuse it across
+ * invocations, it will continue to accumulate onto the same value. The general
+ * recommendation is to always return a new value. If you can't do so for
+ * performance reasons, then be sure to reinitialize the accumulator on each
+ * invocation.
+ * 
  * Dispatches to the `reduce` method of the third argument, if present. When
  * doing so, it is up to the user to handle the [`R.reduced`](#reduced)
  * shortcuting, as this is not implemented by `reduce`.
