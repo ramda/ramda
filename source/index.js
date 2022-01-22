4bb8dd81cb5d8ba522836426543f72e35cdc2b90
Lars Kölpin <LarsKoelpin@users.noreply.github.com>
2022-01-22 23:50:14 +0100

    Add isNotNil (#2818)
    * Add isNotNil
    
    In javascript projects it is often necessary to to someething if
    something is not nil. This commits adds a function "isNotNil" and its corresponding test
     which does the null and undefined checking.
    
    * chore: isNotNil: add missing extensions in import statements
    
    Co-authored-by: customcommander <hello@spinjs.com>

--- source/index.js
+++ source/index.js
@@ -99,6 +99,7 @@ export { default as invoker } from './invoker.js';
 export { default as is } from './is.js';
 export { default as isEmpty } from './isEmpty.js';
 export { default as isNil } from './isNil.js';
+export { default as isNotNil } from './isNotNil.js';
 export { default as join } from './join.js';
 export { default as juxt } from './juxt.js';
 export { default as keys } from './keys.js';
