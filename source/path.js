f0de0fd008988fea5052cfe26b2f0be646bb0d4b
Andrew Smith <andrewsmith@alumni.stanford.edu>
2022-01-22 09:53:56 -0800

    Add docs to `path`. (#3010)
    * Add docs to `path`.
    
    * docs: update path doc, signatures and related functions. address Scott comments in #3010
    
    Co-authored-by: customcommander <hello@spinjs.com>

--- source/path.js
+++ source/path.js
@@ -2,7 +2,8 @@ import _curry2 from './internal/_curry2.js';
 import paths from './paths.js';
 
 /**
- * Retrieve the value at a given path.
+ * Retrieves the value at a given path. The nodes of the path can be arbitrary strings or non-negative integers.
+ * For anything else, the value is unspecified. Integer paths are meant to index arrays, strings are meant for objects.
  *
  * @func
  * @memberOf R
@@ -10,16 +11,19 @@ import paths from './paths.js';
  * @category Object
  * @typedefn Idx = String | Int | Symbol
  * @sig [Idx] -> {a} -> a | Undefined
+ * @sig Idx = String | NonNegativeInt
  * @param {Array} path The path to use.
- * @param {Object} obj The object to retrieve the nested property from.
+ * @param {Object} obj The object or array to retrieve the nested property from.
  * @return {*} The data at `path`.
- * @see R.prop, R.nth
+ * @see R.prop, R.nth, R.assocPath, R.dissocPath
  * @example
  *
  *      R.path(['a', 'b'], {a: {b: 2}}); //=> 2
  *      R.path(['a', 'b'], {c: {b: 2}}); //=> undefined
  *      R.path(['a', 'b', 0], {a: {b: [1, 2, 3]}}); //=> 1
  *      R.path(['a', 'b', -2], {a: {b: [1, 2, 3]}}); //=> 2
+ *      R.path([2], {'2': 2}); //=> 2
+ *      R.path([-2], {'-2': 'a'}); //=> undefined
  */
 
 var path = _curry2(function path(pathAr, obj) {
