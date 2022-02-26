e77d5d2589f3a4b1f7ceb213ac1c7b9bf10e4686
Xavier Garcia Buils <xgbuils@gmail.com>
2022-02-26 20:41:06 +0100

    fix(3245): fix R.dropLasr with negative and zero param when used as transducer

--- test/dropLast.js
+++ test/dropLast.js
@@ -32,8 +32,10 @@ describe('dropLast', function() {
 
   it('can act as a transducer', function() {
     var dropLast2 = R.dropLast(2);
-    assert.deepEqual(R.into([], dropLast2, [1, 3, 5, 7, 9, 1, 2]), [1, 3, 5, 7, 9]);
-    assert.deepEqual(R.into([], dropLast2, [1]), []);
+    eq(R.into([], dropLast2, [1, 3, 5, 7, 9, 1, 2]), [1, 3, 5, 7, 9]);
+    eq(R.into([], dropLast2, [1]), []);
+    eq(R.into([], R.dropLast(0), [1, 3, 5]), [1, 3, 5]);
+    eq(R.into([], R.dropLast(-1), [1, 3, 5]), [1, 3, 5]);
   });
 
 });
