dec329da9cfc683523e8804e99121d48e5c73570
Julien Gonzalez <hello@spinjs.com>
2022-04-25 16:21:11 +0100

    refactor: `of` now works with Applicatives (#3272)
    This change addresses the issue of `of` not being able to
    work with Applicatives.
    
    Fixes #3267
    
    BREAKING CHANGE: `of` is now a binary function with a constructor as its first argument.

--- test/of.js
+++ test/of.js
@@ -1,14 +1,19 @@
+var S = require('sanctuary');
+
 var R = require('../source/index.js');
 var eq = require('./shared/eq.js');
 
 
 describe('of', function() {
   it('returns its argument as an Array', function() {
-    eq(R.of(100), [100]);
-    eq(R.of([100]), [[100]]);
-    eq(R.of(null), [null]);
-    eq(R.of(undefined), [undefined]);
-    eq(R.of([]), [[]]);
+    eq(R.of(Array, 100), [100]);
+    eq(R.of(Array, [100]), [[100]]);
+    eq(R.of(Array, null), [null]);
+    eq(R.of(Array, undefined), [undefined]);
+    eq(R.of(Array, []), [[]]);
   });
 
+  it('dispatches to an available of method', function() {
+    eq(R.of(S.Maybe, 100), S.Just(100));
+  });
 });
