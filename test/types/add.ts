54a5e3b6fd705f11a5f345fb8dc2a0697a53ee7c
somebody1234 <ehern.lee@gmail.com>
2022-05-30 07:23:54 +1000

    feat: add tsd and one test file

---
+++ test/types/add.ts
@@ -0,0 +1,8 @@
+import { expectType, expectError} from 'tsd';
+import * as R from '../../es/index';
+
+expectType<number>(R.add(2, 3)); // => 5
+expectType<number>(R.add(7)(10)); // => 17
+expectError(R.add('foo', 'bar'));
+expectError(R.add('foo')('bar'));
+
