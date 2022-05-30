183c7b79144f861591b2f910090005d22406423b
somebody1234 <ehern.lee@gmail.com>
2022-05-30 11:31:03 +1000

    fix: add testcase

--- test/types/add.ts
+++ test/types/add.ts
@@ -3,6 +3,7 @@ import * as R from '../../es/index';
 
 expectType<number>(R.add(2, 3)); // => 5
 expectType<number>(R.add(7)(10)); // => 17
+expectType<(a: number) => number>(R.add(7));
 expectError(R.add('foo', 'bar'));
 expectError(R.add('foo')('bar'));
 
