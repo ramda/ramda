b69306996a549459df82c60c21e891cf435db097
somebody1234 <ehern.lee@gmail.com>
2022-05-31 05:37:08 +1000

    chore: bump package-lock.json, minor fixes

--- test/types/add.ts
+++ test/types/add.ts
@@ -1,8 +1,8 @@
-import { expectType, expectError} from 'tsd';
+import { expectType, expectError } from 'tsd';
 import * as R from '../../es/index';
 
-expectType<number>(R.add(2, 3)); // => 5
-expectType<number>(R.add(7)(10)); // => 17
+expectType<number>(R.add(2, 3));
+expectType<number>(R.add(7)(10));
 expectType<(a: number) => number>(R.add(7));
 expectError(R.add('foo', 'bar'));
 expectError(R.add('foo')('bar'));
