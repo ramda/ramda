7f9fa28ed35923140c798ed40247d44546d2d729
jgsheppa <james.sheppard@avisio.ai>
2022-08-20 12:20:05 +0200

    add additional return type to dropRepeatsBy types

--- types/dropRepeatsBy.d.ts
+++ types/dropRepeatsBy.d.ts
@@ -1,3 +1,5 @@
-export function dropRepeatsBy<T>(fn: (a: T) => T, list: readonly T[]): T[];
-export function dropRepeatsBy<T>(fn: (a: T) => T): (list: readonly T[]) => T[];
+export function dropRepeatsBy<T, U>(fn: (a: T) => U, list: readonly T[]): T[];
+export function dropRepeatsBy<T, U>(
+  fn: (a: T) => U
+): (list: readonly T[]) => T[];
 export function dropRepeatsBy(fn: any): <T>(list: readonly T[]) => T[];
