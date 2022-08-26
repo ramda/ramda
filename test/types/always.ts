21d6ceaf9e15aa4c571735a898ad7abbad6e3392
jgsheppa <james.sheppard@avisio.ai>
2022-08-27 00:15:17 +0200

    fix type error in always test

--- test/types/always.ts
+++ test/types/always.ts
@@ -4,7 +4,9 @@ import * as R from "../../es/index";
 expectType<string>(R.always("a")());
 expectType<string[]>(R.always(["a"])());
 expectType<number>(R.always(2)());
-expectType<Record<string, number>>(R.always({ a: 2, b: 3, c: 4 })());
+expectType<{ a: number; b: number; c: number }>(
+  R.always({ a: 2, b: 3, c: 4 })()
+);
 expectType<null>(R.always(null)());
 expectType<(...args: unknown[]) => number>(R.always(1));
 
