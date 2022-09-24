ed82814c075bb5a0c0ec7dab30d8c6c3fdfe36be
jgsheppa <james.sheppard@avisio.ai>
2022-09-24 15:31:51 +0200

    revert changes to and function type and update type test

--- test/types/and.ts
+++ test/types/and.ts
@@ -1,10 +1,9 @@
 import { expectType, expectError } from "tsd";
 import * as R from "../../es/index";
 
-expectType<boolean>(R.and("a")(false));
-expectType<boolean>(R.and("true")("true"));
-expectType<boolean>(R.and(false)(true));
-expectType<boolean>(R.and(1, [2]));
-expectType<boolean>(R.and("1", [2]));
-expectType<boolean>(R.and({ a: 1, b: 2, c: 3 }, [2]));
-expectType<boolean>(R.and(null, undefined));
+expectType<string | boolean>(R.and("a")(false));
+expectType<string | boolean>(R.and("true")("true"));
+expectType<boolean | boolean>(R.and(false)(true));
+expectType<number | boolean>(R.and(1, [2]));
+expectType<number[] | boolean>(R.and([2], "1"));
+expectType<null | boolean>(R.and(null, undefined));
