a0def537def1a4574a95f9492f230ff4c68602d4
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-19 20:41:30 -0600

    finished tests

--- test/types/modify.ts
+++ test/types/modify.ts
@@ -5,11 +5,19 @@ type Point = { x: number; y: number };
 
 const p = { x: 2, y: 3 };
 
-const times2 = multiply(2);
-
-expectType<Point>(modify('x', times2, p));
-expectType<Point>(modify(__, times2, p));
+const t = modify('x', toString);
 
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', toString, p));
-
-// TODO: finish tests
+expectType<<K extends keyof Point>(prop: K) => Omit<Point, K> & Record<K, string>>(modify(__, toString, p));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, toString, p)('x'));
+expectType<<P>(fn: (a: Point['x']) => P) => Omit<Point, 'x'> & Record<'x', P>>(modify('x', __, p));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', __, p)(toString));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, __, p)('x', toString));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, __, p)('x')(toString));
+expectType<<T extends Record<'x', number>>(obj: T) => Omit<T, 'x'> & Record<'x', string>>(modify('x', (x: number) => x.toString()));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', toString)(p));
+// must manually set generics for the following to work at all
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>(__, toString)('x', p));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>(__, toString)('x')(p));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>('x')(toString, p));
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>('x')(toString)(p));
