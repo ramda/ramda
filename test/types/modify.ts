dde174e25032ca53243cead9524b056e48cb90d4
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-22 00:38:48 -0600

    way better typings for modify

--- test/types/modify.ts
+++ test/types/modify.ts
@@ -1,23 +1,35 @@
-import { expectType, expectError } from 'tsd';
-import { __, modify, toString, multiply } from '../../es/index';
+import { expectType } from 'tsd';
+import { __, modify, toString } from '../../es/index';
 
 type Point = { x: number; y: number };
 
 const p = { x: 2, y: 3 };
 
-const t = modify('x', toString);
-
+// modify(prop, fn, obj)
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', toString, p));
-expectType<<K extends keyof Point>(prop: K) => Omit<Point, K> & Record<K, string>>(modify(__, toString, p));
+
+// modify(__, fn, obj)(prop)
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, toString, p)('x'));
+
+// modify(prop, __, obj)(prop)
 expectType<<P>(fn: (a: Point['x']) => P) => Omit<Point, 'x'> & Record<'x', P>>(modify('x', __, p));
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', __, p)(toString));
+
+// modify(__, __, obj)(fn, prop)
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, __, p)('x', toString));
+// modify(__, __)(fn)(prop)
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, __, p)('x')(toString));
+
+// modify(prop, fn)(obj)
 expectType<<T extends Record<'x', number>>(obj: T) => Omit<T, 'x'> & Record<'x', string>>(modify('x', (x: number) => x.toString()));
 expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x', toString)(p));
-// must manually set generics for the following to work at all
-expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>(__, toString)('x', p));
-expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>(__, toString)('x')(p));
-expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>('x')(toString, p));
-expectType<Omit<Point, 'x'> & Record<'x', string>>(modify<Point, 'x', string>('x')(toString)(p));
+
+// modify(_, fn)(prop, obj)
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, toString)('x', p));
+// modify(_, fn)(prop)(obj)
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify(__, toString)('x')(p));
+
+// modify(prop)(fn, obj)
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x')(toString, p));
+// modify(prop)(fn)(obj)
+expectType<Omit<Point, 'x'> & Record<'x', string>>(modify('x')(toString)(p));
--- types/modify.d.ts
+++ types/modify.d.ts
@@ -2,10 +2,26 @@ import * as _ from 'ts-toolbelt';
 
 import { Placeholder } from './util/tools';
 
+// modify(prop, fn, obj)
 export function modify<T, K extends keyof T, P>(prop: K, fn: (a: T[K]) => P, obj: T): Omit<T, K> & Record<K, P>;
+// modify(__, fn, obj)(prop)
 export function modify<T, A, P>(__: Placeholder, fn: (a: A) => P, obj: T): <K extends keyof T>(prop: K) => Omit<T, K> & Record<K, P>;
+// modify(prop, __, obj)(prop)
 export function modify<T, K extends keyof T>(prop: K, __: Placeholder, obj: T): <P>(fn: (a: T[K]) => P) => Omit<T, K> & Record<K, P>;
-export function modify<T, K extends keyof T, P>(__: Placeholder, __2: Placeholder, obj: T): _.F.Curry<(prop: K, fn: (a: T[K]) => P) => Omit<T, K> & Record<K, P>>;
+// modify(__, __, obj)(fn, prop) and modify(__, __)(fn)(prop)
+export function modify<T>(__: Placeholder, __2: Placeholder, obj: T): {
+  <K extends keyof T, P>(prop: K, fn: (a: T[K]) => P): Omit<T, K> & Record<K, P>;
+  <K extends keyof T>(prop: K): <P>(fn: (a: T[K]) => P) => Omit<T, K> & Record<K, P>;
+}
+// modify(prop, fn)(obj), we can't know if prop is keyof obj is this case, but we can restrain obj to have prop as a key instead
 export function modify<K extends string, P, A>(prop: K, fn: (a: A) => P): <T extends Record<K, A>>(obj: T) => Omit<T, K> & Record<K, P>;
-export function modify<T, K extends keyof T, P>(__: Placeholder, fn: (a: T[K]) => P): _.F.Curry<(prop: K, obj: T) => Omit<T, K> & Record<K, P>>;
-export function modify<T, K extends keyof T, P>(prop: K): _.F.Curry<(fn: (a: T[K]) => P, obj: T) => Omit<T, K> & Record<K, P>>;
+// modify(_, fn)(prop, obj) and modify(_, fn)(prop)(obj)
+export function modify<T, A, P>(__: Placeholder, fn: (a: A) => P): {
+  <K extends string, T extends Record<K, A>>(prop: K, obj: T): Omit<T, K> & Record<K, P>;
+  <K extends string>(prop: K): <T extends Record<K, A>>(obj: T) => Omit<T, K> & Record<K, P>;
+}
+// modify(prop)(fn, obj) and modify(prop)(fn)(obj)
+export function modify<K extends string>(prop: K): {
+  <T extends Record<K, A>, A, P>(fn: (a: A) => P, obj: T): Omit<T, K> & Record<K, P>
+  <A, P>(fn: (a: A) => P): <T extends Record<K, A>>(obj: T) => Omit<T, K> & Record<K, P>;
+};
