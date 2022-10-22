03f51bb108589beceebb763c72a2b82dba43ce7e
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-22 02:48:12 -0600

    comments and improvements

--- test/types/adjust.ts
+++ test/types/adjust.ts
@@ -1,19 +1,56 @@
 import { expectType, expectError } from "tsd";
 import * as R from "../../es/index";
 
-expectType<string[]>(R.adjust(1, R.toUpper, ["a", "b", "c"]));
-expectType<string[]>(R.adjust(2, R.toUpper)(["c", "d", "e"]));
+const double = (n: number) => n * 2;
 
-expectType<number[]>(R.adjust(2, (n: number) => n * 2)([1, 2, 3]));
-expectType<boolean[]>(R.adjust(2, (n: boolean) => !n)([true, false, true]));
+// adjust(index, fn, list)
+expectType<string[]>(R.adjust(2, R.toUpper, ["a", "b", "c"]));
+expectType<number[]>(R.adjust(2, R.multiply(2), [1, 2, 3]));
+expectType<boolean[]>(R.adjust(2, R.not, [true, false, true]));
+// adjust(index, fn)(list)
+expectType<string[]>(R.adjust(2, R.toUpper)(["a", "b", "c"]));
+expectType<number[]>(R.adjust(2, R.multiply(2))([1, 2, 3]));
+expectType<boolean[]>(R.adjust(2, R.not)([true, false, true]));
 
+// adjust(index)(fn, list)
 expectType<string[]>(R.adjust(2)(R.toUpper, ["a", "b", "c"]));
-expectType<string[]>(R.adjust(2)(R.toUpper)(["a", "b", "c"]));
+expectType<number[]>(R.adjust(2)(R.multiply(2), [1, 2, 3]));
+expectType<boolean[]>(R.adjust(2)(R.not, [true, false, true]));
 
+// adjust(index)(__, list)(fn)
+expectType<string[]>(R.adjust(2)(R.__, ["a", "b", "c"])(R.toUpper));
+expectType<number[]>(R.adjust(2)(R.__, [1, 2, 3])(R.multiply(2)));
+expectType<boolean[]>(R.adjust(2)(R.__, [true, false, true])(R.not));
+
+// adjust(index)(fn)(list)
+expectType<string[]>(R.adjust(2)(R.toUpper)(["a", "b", "c"]));
+expectType<number[]>(R.adjust(2)(R.multiply(2))([1, 2, 3]));
+expectType<boolean[]>(R.adjust(2)(R.not)([true, false, true]));
+
+// adjust(__, fn, list)(index)
 expectType<string[]>(R.adjust(R.__, R.toUpper, ["a", "b", "c"])(1));
+expectType<number[]>(R.adjust(R.__, R.multiply(2), [1, 2, 3])(1));
+expectType<boolean[]>(R.adjust(R.__, R.not, [true, false, true])(1));
+
+// adjust(index, __, list)(fn)
 expectType<string[]>(R.adjust(1, R.__, ["a", "b", "c"])(R.toUpper));
+expectType<number[]>(R.adjust(1, R.__, [1, 2, 3])(R.multiply(2)));
+expectType<boolean[]>(R.adjust(1, R.__, [true, false, true])(R.not));
+
+// adjust(__, __, list)(index, fn)
 expectType<string[]>(R.adjust(R.__, R.__, ["a", "b", "c"])(1, R.toUpper));
+expectType<number[]>(R.adjust(R.__, R.__, [1, 2, 3])(1, R.multiply(2)));
+expectType<boolean[]>(R.adjust(R.__, R.__, [true, false, true])(1, R.not));
+
+// adjust(__, __, list)(__, fn)(index)
+expectType<string[]>(R.adjust(R.__, R.__, ["a", "b", "c"])(R.__, R.toUpper)(1));
+expectType<number[]>(R.adjust(R.__, R.__, [1, 2, 3])(R.__, R.multiply(2))(1));
+expectType<boolean[]>(R.adjust(R.__, R.__, [true, false, true])(R.__, R.not)(1));
+
+// adjust(__, __, list)(index)(fn)
 expectType<string[]>(R.adjust(R.__, R.__, ["a", "b", "c"])(1)(R.toUpper));
+expectType<number[]>(R.adjust(R.__, R.__, [1, 2, 3])(1)(R.multiply(2)));
+expectType<boolean[]>(R.adjust(R.__, R.__, [true, false, true])(1)(R.not));
 
 expectError(R.adjust(1, R.toString, [1, 2, 3]));
 expectError(R.adjust(1, R.inc, ["c", "d", "e"]));
--- types/adjust.d.ts
+++ types/adjust.d.ts
@@ -1,15 +1,29 @@
 import { Placeholder } from './util/tools';
 
+// adjust(index, fn, list)
 export function adjust<T>(index: number, fn: (a: T) => T, list: readonly T[]): T[];
+// adjust(index, fn)(list)
 export function adjust<T>(index: number, fn: (a: T) => T): (list: readonly T[]) => T[];
+// adjust(index)
 export function adjust(index: number): {
+  // adjust(index)(fn, list)
   <T>(fn: (a: T) => T, list: readonly T[]): T[];
+  // adjust(index)(__, list)(fn)
+  <T>(__: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
+  // adjust(index)(fn)(list)
   <T>(fn: (a: T) => T): (list: readonly T[]) => T[];
 }
 
+// adjust(__, fn, list)(index)
 export function adjust<T>(__: Placeholder, fn: (a: T) => T, list: readonly T[]): (index: number) => T[];
+// adjust(index, __, list)(fn)
 export function adjust<T>(index: number, __: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
+// adjust(__, __, list)
 export function adjust<T>(__: Placeholder, __2: Placeholder, list: readonly T[]): {
+  // adjust(__, __, list)(index, fn)
   (index: number, fn: (a: T) => T): T[];
+  // adjust(__, __, list)(__, fn)(index)
+  (__3: Placeholder, fn: (a: T) => T): (index: number) => T[];
+  // adjust(__, __, list)(index)(fn)
   (index: number): (fn: (a: T) => T) => T[];
 }
