04038fddff30463adc5e0ce6fe7631d6cc8e79a9
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-22 02:31:36 -0600

    comments and improvements

--- test/types/pickAll.ts
+++ test/types/pickAll.ts
@@ -10,13 +10,18 @@ const obj = {
 
 const t = pickAll(['a', 'b'], obj);
 
-// when all names are part of obj, return Pick<T, K> for best typings
-expectType<Pick<Obj, 'a' | 'b'>>(pickAll(['a', 'b'], obj));
-// else, returns unknown
-expectType<unknown>(pickAll(['a', 'b', 'c'], obj));
-// else, or whatever is set in the generic
-expectType<{ a: string; b: number; c: Function }>(pickAll<Obj, { a: string; b: number; c: Function }>(['a', 'b', 'c'], obj));
-
+// pickAll (__, obj)(names) when all names are keyof obj
 expectType<Pick<Obj, 'a' | 'b'>>(pickAll(__, obj)(['a', 'b']));
+// pickAll (__, obj)(names) if not all names are keyof obj
 expectType<unknown>(pickAll(__, obj)(['a', 'b', 'c']));
 expectType<Function>(pickAll(__, obj)<Function>(['a', 'b', 'c']));
+
+// pickAll(names, obj) when all names are keyof obj
+expectType<Pick<Obj, 'a' | 'b'>>(pickAll(['a', 'b'], obj));
+// pickAll(names, obj) if not all names are keyof obj
+expectType<unknown>(pickAll(['a', 'b', 'c'], obj));
+
+// pickAll(names)(obj) test names if all are keyof obj to return best type
+expectType<Pick<Obj, 'a' | 'b'>>(pickAll(['a', 'b'], obj));
+expectType<unknown>(pickAll(['a', 'b', 'c'], obj));
+expectType<{ a: string; b: number; c: Function }>(pickAll<Obj, { a: string; b: number; c: Function }>(['a', 'b', 'c'], obj));
--- types/pickAll.d.ts
+++ types/pickAll.d.ts
@@ -1,10 +1,19 @@
 import { Placeholder } from './util/tools';
 
-export function pickAll<T, K extends keyof T>(names: readonly K[], obj: T): Pick<T, K>;
-export function pickAll<T, U>(names: readonly string[], obj: T): U;
-export function pickAll(names: readonly string[]): <T, U>(obj: T) => U;
-
+// pickAll (__, obj)
 export function pickAll<T>(__: Placeholder, obj: T): {
+  // pickAll (__, obj)(names) when all names are keyof obj
   <K extends keyof T>(names: readonly K[]): Pick<T, K>;
+  // pickAll (__, obj)(names) if not all names are keyof obj
   <U>(names: string[]): U;
 }
+
+// pickAll(names, obj) when all names are keyof obj
+export function pickAll<T, K extends keyof T>(names: readonly K[], obj: T): Pick<T, K>;
+// pickAll(names, obj) if not all names are keyof obj
+export function pickAll<T, U>(names: readonly string[], obj: T): U;
+
+// pickAll(names)(obj) test names if all are keyof obj to return best type
+export function pickAll<K extends readonly string[]>(names: K):
+  <U, T = unknown>(obj: T) => K extends keyof T ? Pick<T, K> : U;
+
