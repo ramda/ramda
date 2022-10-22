78818f6a70b0dc9e028036b9ee5bf036a7250d84
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-21 22:39:33 -0600

    tests for R.all's 'dispatch to all method' case

--- test/types/all.ts
+++ test/types/all.ts
@@ -17,6 +17,11 @@ expectType<boolean>(
 expectType<(fn: (a: number) => boolean) => boolean>(R.all(R.__, [3, 3, 3]));
 expectType<boolean>(R.all(R.__, [3, 3, 3])(R.equals(3)));
 
+expectType<boolean>(R.all(R.equals(3), new TestType([3, 3, 3])));
+expectType<boolean>(R.all(R.equals(3))(new TestType([3, 3, 3])));
+expectType<(fn: (a: number) => boolean) => boolean>(R.all(R.__, new TestType([3, 3, 3])));
+expectType<boolean>(R.all(R.__, new TestType([3, 3, 3]))(R.equals(3)));
+
 expectError(R.all((n: number) => n, [1, 3, 4]));
 expectError(R.all((n: string) => n, [5, 6, 7]));
 expectError(R.all((n: null | undefined) => n, [null, undefined, null]));
