2c82afa3c372f393dc4e9162f8fc4c530f982738
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-21 21:24:45 -0600

    added types for placeholders and added tests

--- test/types/adjust.ts
+++ test/types/adjust.ts
@@ -7,6 +7,15 @@ expectType<string[]>(R.adjust(2, R.toUpper)(["c", "d", "e"]));
 expectType<number[]>(R.adjust(2, (n: number) => n * 2)([1, 2, 3]));
 expectType<boolean[]>(R.adjust(2, (n: boolean) => !n)([true, false, true]));
 
+expectType<string[]>(R.adjust(2)(R.toUpper, ["a", "b", "c"]));
+expectType<string[]>(R.adjust(2)(R.toUpper)(["a", "b", "c"]));
+
+expectType<string[]>(R.adjust(R.__, R.toUpper, ["a", "b", "c"])(1));
+expectType<string[]>(R.adjust(1, R.__, ["a", "b", "c"])(R.toUpper));
+expectType<string[]>(R.adjust(R.__, R.__, ["a", "b", "c"])(1, R.toUpper));
+expectType<string[]>(R.adjust(R.__, R.__, ["a", "b", "c"])(1)(R.toUpper));
+
 expectError(R.adjust(1, R.toString, [1, 2, 3]));
+expectError(R.adjust(1, R.inc, ["c", "d", "e"]));
 expectError(R.adjust("1", R.toUpper, ["c", "d", "e"]));
 expectError(R.adjust(2, R.isNil)([2, 3, 4]));
--- types/adjust.d.ts
+++ types/adjust.d.ts
@@ -1,2 +1,15 @@
+import { Placeholder } from './util/tools';
+
 export function adjust<T>(index: number, fn: (a: T) => T, list: readonly T[]): T[];
 export function adjust<T>(index: number, fn: (a: T) => T): (list: readonly T[]) => T[];
+export function adjust(index: number): {
+  <T>(fn: (a: T) => T, list: readonly T[]): T[];
+  <T>(fn: (a: T) => T): (list: readonly T[]) => T[];
+}
+
+export function adjust<T>(__: Placeholder, fn: (a: T) => T, list: readonly T[]): (index: number) => T[];
+export function adjust<T>(index: number, __: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
+export function adjust<T>(__: Placeholder, __2: Placeholder, list: readonly T[]): {
+  (index: number, fn: (a: T) => T): T[];
+  (index: number): (fn: (a: T) => T) => T[];
+}
