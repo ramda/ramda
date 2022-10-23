f5751a53bf8ee86289630e0b528f4a8a5fbb973d
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-22 20:18:56 -0600

    R.andThen :: added curry type and added tests

---
+++ test/types/andThen.ts
@@ -0,0 +1,13 @@
+import { expectType, expectError } from "tsd";
+import { __, andThen, Placeholder } from "../../es/index";
+
+const aToB = (a: number): string => a.toString();
+const aToPb = (a: number): Promise<string> => Promise.resolve(a.toString());
+
+expectType<Promise<string>>(andThen(aToB, Promise.resolve(2)));
+expectType<Promise<string>>(andThen(__, Promise.resolve(2))(aToB));
+expectType<Promise<string>>(andThen(aToB)(Promise.resolve(2)));
+
+expectType<Promise<string>>(andThen(aToPb, Promise.resolve(2)));
+expectType<Promise<string>>(andThen(__, Promise.resolve(2))(aToPb));
+expectType<Promise<string>>(andThen(aToPb)(Promise.resolve(2)));
--- types/andThen.d.ts
+++ types/andThen.d.ts
@@ -1,2 +1,5 @@
+import { Placeholder } from './util/tools';
+
 export function andThen<A, B>(onSuccess: (a: A) => B | Promise<B>, promise: Promise<A>): Promise<B>;
+export function andThen<A>(__: Placeholder, promise: Promise<A>): <B>(onSuccess: (a: A) => B | Promise<B>) => Promise<B>;
 export function andThen<A, B>(onSuccess: (a: A) => B | Promise<B>): (promise: Promise<A>) => Promise<B>;
