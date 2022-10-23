ceca962708f72ca17c7bfbec6bdc2da27758262f
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-22 20:27:00 -0600

    added curry type for R.any

--- types/any.d.ts
+++ types/any.d.ts
@@ -1,2 +1,5 @@
+import { Placeholder } from './util/tools';
+
+export function any<T>(__: Placeholder, list: readonly T[]): (fn: (a: T) => boolean) => boolean;
 export function any<T>(fn: (a: T) => boolean, list: readonly T[]): boolean;
 export function any<T>(fn: (a: T) => boolean): (list: readonly T[]) => boolean;
