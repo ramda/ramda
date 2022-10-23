4708cf960a09ae99e1d45fde98ced094d8919175
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-23 01:06:19 -0600

    add curry typings

--- types/append.d.ts
+++ types/append.d.ts
@@ -1,2 +1,5 @@
+import { Placeholder } from './util/tools';
+
+export function append<T>(__: Placeholder, list: readonly T[]): (el: T) => T[];
 export function append<T>(el: T, list: readonly T[]): T[];
 export function append<T>(el: T): (list: readonly T[]) => T[];
