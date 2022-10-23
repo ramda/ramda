c8b7aec2c0ee93589095334cee9c6301ee76b59b
Harris Miller <harrismillerconsulting@gmail.com>
2022-10-23 01:38:16 -0600

    added curry type for R.apply

--- types/apply.d.ts
+++ types/apply.d.ts
@@ -1,2 +1,9 @@
+import { Placeholder } from './util/tools';
+
+// apply(args, fn)
+// only way to automatically constrain `fn` here is if `args` was an `[] as const`
+export function apply<A extends readonly any[]>(__: Placeholder, args: A): <F extends (...args: readonly any[]) => any>(fn: F) => ReturnType<F>;
+// apply(args, fn)
 export function apply<F extends (...args: readonly any[]) => any>(fn: F, args: Parameters<F>): ReturnType<F>;
+// apply(args)(fn)
 export function apply<F extends (...args: readonly any[]) => any>(fn: F): (args: Parameters<F>) => ReturnType<F>;
