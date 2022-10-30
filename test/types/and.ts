5db381f7f7b0a4307350e53774b82b9e9fd81790
jgsheppa <james.sheppard@avisio.ai>
2022-10-30 12:30:37 +0100

    improve type test for AND function

--- test/types/and.ts
+++ test/types/and.ts
@@ -1,9 +1,9 @@
 import { expectType, expectError } from "tsd";
 import * as R from "../../es/index";
 
-expectType<string | boolean>(R.and("a")(false));
-expectType<string | boolean>(R.and("true")("true"));
+expectType<string | false>(R.and("a")(false));
+expectType<string>(R.and("true")("true"));
 expectType<boolean | boolean>(R.and(false)(true));
-expectType<number | boolean>(R.and(1, [2]));
-expectType<number[] | boolean>(R.and([2], "1"));
-expectType<null | boolean>(R.and(null, undefined));
+expectType<1 | number[]>(R.and(1, [2]));
+expectType<number[] | "1">(R.and([2], "1"));
+expectType<null | undefined>(R.and(null, undefined));
