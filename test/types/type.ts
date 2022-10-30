07ed906aad7abf98ec067fae18ef3e5a40e5707b
jgsheppa <james.sheppard@avisio.ai>
2022-10-30 13:02:32 +0100

    Create type test for TYPE function

---
+++ test/types/type.ts
@@ -0,0 +1,16 @@
+import { expectType, expectError } from "tsd";
+import { type } from "../../es/index";
+import { TypeFunctionReturnTypes } from "../../types/type";
+
+expectType<TypeFunctionReturnTypes>(type({ a: 1 }));
+expectType<TypeFunctionReturnTypes>(type(1));
+expectType<TypeFunctionReturnTypes>(type(true));
+expectType<TypeFunctionReturnTypes>(type("string"));
+expectType<TypeFunctionReturnTypes>(type(null));
+expectType<TypeFunctionReturnTypes>(type([1, 2, 3]));
+expectType<TypeFunctionReturnTypes>(type(new RegExp("ab+c")));
+expectType<TypeFunctionReturnTypes>(type(() => 1 + 2));
+expectType<TypeFunctionReturnTypes>(type(undefined));
+expectType<TypeFunctionReturnTypes>(type(Symbol("test")));
+expectType<TypeFunctionReturnTypes>(type(new Error("System failure!")));
+expectType<TypeFunctionReturnTypes>(type(new Date()));
--- types/type.d.ts
+++ types/type.d.ts
@@ -1,15 +1,14 @@
-export function type(
-    val: any,
-):
-    | 'Object'
-    | 'Number'
-    | 'Boolean'
-    | 'String'
-    | 'Null'
-    | 'Array'
-    | 'RegExp'
-    | 'Function'
-    | 'Undefined'
-    | 'Symbol'
-    | 'Error'
-    | 'Date';
+export type TypeFunctionReturnTypes =
+  | "Object"
+  | "Number"
+  | "Boolean"
+  | "String"
+  | "Null"
+  | "Array"
+  | "RegExp"
+  | "Function"
+  | "Undefined"
+  | "Symbol"
+  | "Error"
+  | "Date";
+export function type(val: any): TypeFunctionReturnTypes;
