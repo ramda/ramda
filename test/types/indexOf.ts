11dfa83e4e8300e59d3d688220a2173b7aaae7c0
jgsheppa <james.sheppard@avisio.ai>
2022-11-01 23:48:54 +0100

    write type test for indexOf function

---
+++ test/types/indexOf.ts
@@ -0,0 +1,10 @@
+import { expectType } from "tsd";
+import { indexOf } from "../../es/index";
+
+expectType<number>(indexOf(2, ["1", 2, "3", null, undefined, NaN, true]));
+expectType<number>(indexOf("1", ["1", 2, "3", null, undefined, NaN, true]));
+expectType<number>(indexOf(null, ["1", 2, "3", null, undefined, NaN, true]));
+expectType<number>(indexOf(undefined, ["1", 2, "3", null, undefined, NaN, true]));
+expectType<number>(indexOf(NaN, ["1", 2, "3", null, undefined, NaN, true]));
+expectType<number>(indexOf(true, ["1", 2, "3", null, undefined, NaN, true]));
+
