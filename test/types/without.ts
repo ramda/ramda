2071f2bdff01e89130c3ecf94b1b9c16e12d6e69
jgsheppa <james.sheppard@avisio.ai>
2022-11-01 23:44:55 +0100

    write type test for WITHOUT function

---
+++ test/types/without.ts
@@ -0,0 +1,13 @@
+import { expectType, expectError } from "tsd";
+import { without } from "../../es/index";
+
+expectType<(list2: readonly number[]) => number[]>(without([1, 2, 3]));
+expectType<number[]>(without([1, 2, 3], [1]));
+expectType<string[]>(without(["1", "2", "3"], ["1"]));
+expectType<Array<string | number | null>>(without(["1", 2, "3", null], ["1"]));
+
+expectError<(list2: readonly number[]) => string[]>(without([1, 2, 3]));
+expectError<(list2: readonly string[]) => number[]>(without([1, 2, 3]));
+expectType<string[]>(without([1, 2, 3], [1]));
+
+
