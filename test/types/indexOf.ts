85342964bd26f4d0ec4a72cad2ffcf42f8a7a456
jgsheppa <james.sheppard@avisio.ai>
2022-11-02 00:02:29 +0100

    add two test cases for type test indexOf

--- test/types/indexOf.ts
+++ test/types/indexOf.ts
@@ -8,3 +8,7 @@ expectType<number>(indexOf(undefined, ["1", 2, "3", null, undefined, NaN, true])
 expectType<number>(indexOf(NaN, ["1", 2, "3", null, undefined, NaN, true]));
 expectType<number>(indexOf(true, ["1", 2, "3", null, undefined, NaN, true]));
 
+expectType<number>(indexOf(2)([1, 2, 3]));
+expectType<(list: readonly boolean[]) => number>(indexOf(true));
+
+
