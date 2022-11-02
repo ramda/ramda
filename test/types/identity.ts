cfb44f501b9c31eb8d3ad6c1c55182056dd03c5d
jgsheppa <james.sheppard@avisio.ai>
2022-11-02 21:48:43 +0100

    fix type errors from TS tests

--- test/types/identity.ts
+++ test/types/identity.ts
@@ -4,7 +4,7 @@ import { identity } from "../../es/index";
 expectType<1>(identity(1));
 expectType<"a">(identity("a"));
 expectType<true>(identity(true));
-expectType<object>(identity({}));
+expectType<{a: "a"}>(identity({a: "a"}));
 expectType<[1]>(identity([1]));
 expectType<typeof NaN>(identity(NaN));
 expectType<undefined>(identity(undefined));
--- test/types/without.ts
+++ test/types/without.ts
@@ -8,6 +8,6 @@ expectType<Array<string | number | null>>(without(["1", 2, "3", null], ["1"]));
 
 expectError<(list2: readonly number[]) => string[]>(without([1, 2, 3]));
 expectError<(list2: readonly string[]) => number[]>(without([1, 2, 3]));
-expectType<string[]>(without([1, 2, 3], [1]));
+expectError<string[]>(without([1, 2, 3], [1]));
 
 
