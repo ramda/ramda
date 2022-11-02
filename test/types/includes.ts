af5d4fe59659b2137dd116e54bdc5be3be25add9
jgsheppa <james.sheppard@avisio.ai>
2022-11-02 22:02:54 +0100

    add two test cases for INCLUDES with placeholders

--- test/types/includes.ts
+++ test/types/includes.ts
@@ -1,11 +1,13 @@
 import { expectType } from "tsd";
-import { includes } from "../../es/index";
+import { includes, __ } from "../../es/index";
 
 expectType<boolean>(includes(2, ["1", 2, "3", null, undefined, NaN]));
 expectType<boolean>(includes("1", ["1", 2, "3", null, undefined, NaN]));
 expectType<boolean>(includes(null, ["1", 2, "3", null, undefined, NaN]));
 expectType<boolean>(includes(undefined, ["1", 2, "3", null, undefined, NaN]));
 expectType<boolean>(includes(NaN, ["1", 2, "3", null, undefined, NaN]));
+expectType<boolean>(includes(__, ["1", 2, "3", null, undefined, NaN])(NaN));
 expectType<(list: readonly number[]) => boolean>(includes(1));
+expectType<(target: string | number | null | undefined) => boolean>(includes(__, ["1", 2, "3", null, undefined, NaN]));
 expectType<boolean>(includes(1)([1, 2, 3]));
 
