d5ca6afc29faa927f43c88c0d5313bcd06060958
harris-miller <harrismillerconsulting@gmail.com>
2023-04-10 00:48:43 -0600

    _concat use predetermined array length

--- source/internal/_concat.js
+++ source/internal/_concat.js
@@ -15,16 +15,16 @@ export default function _concat(set1, set2) {
   var idx;
   var len1 = set1.length;
   var len2 = set2.length;
-  var result = [];
+  var result = Array(len1 + len2);
 
   idx = 0;
   while (idx < len1) {
-    result[result.length] = set1[idx];
+    result[idx] = set1[idx];
     idx += 1;
   }
   idx = 0;
   while (idx < len2) {
-    result[result.length] = set2[idx];
+    result[len1 + idx] = set2[idx];
     idx += 1;
   }
   return result;
