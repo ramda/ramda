9c2310c6854727c299391e4c91098c402401840c
Chris Hammock <35451169+crshmk@users.noreply.github.com>
2023-05-16 21:33:18 +0700

    version es dist folder (#3382)

--- scripts/addModulePackageScope.js
+++ scripts/addModulePackageScope.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const { version } = require('../package.json');
 
 const PACKAGE_SCOPE_PATH = path.join(__dirname, '..', 'es', 'package.json');
 
-fs.writeFileSync(PACKAGE_SCOPE_PATH, JSON.stringify({ type: 'module' }));
+fs.writeFileSync(PACKAGE_SCOPE_PATH, JSON.stringify({ type: 'module', version }));
