f7a0ff2e1ef68f1c23b0246600378d15290c98ae
gleguizamon <gonzaforce@gmail.com>
2023-08-16 11:24:05 -0300

    fix: modify curry doc

--- source/curry.js
+++ source/curry.js
@@ -29,22 +29,6 @@ import curryN from './curryN.js';
  * Please note that default parameters don't count towards a [function arity](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length)
  * and therefore `curry` won't work well with those:
  *
- * ```
- * const h = R.curry((a, b, c = 2) => a + b + c);
- *
- * h(40);
- * //=> function (waits for `b`)
- *
- * h(39)(1);
- * //=> 42
- *
- * h(1)(2, 3);
- * //=> 6
- *
- * h(1)(2)(7);
- * //=> Error! (`3` is not a function!)
- * ```
- *
  * @func
  * @memberOf R
  * @since v0.1.0
@@ -56,11 +40,13 @@ import curryN from './curryN.js';
  * @example
  *
  *      const addFourNumbers = (a, b, c, d) => a + b + c + d;
- *
  *      const curriedAddFourNumbers = R.curry(addFourNumbers);
  *      const f = curriedAddFourNumbers(1, 2);
  *      const g = f(3);
  *      g(4); //=> 10
+ *
+ *      const h = R.curry((a, b, c = 2) => a + b + c);
+ *      h(1)(2)(7); //=> Error! (`3` is not a function!)
  */
 var curry = _curry1(function curry(fn) {
   return curryN(fn.length, fn);
