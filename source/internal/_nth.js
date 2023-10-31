995df6d73f380fef11eba538497c6a6cd2ece87c
Berndy <al-berndt@gmx.de>
2023-10-31 14:56:47 +0100

    export and naming of _nth and _path are now more consistent with other internal functions

--- source/internal/_nth.js
+++ source/internal/_nth.js
@@ -1,7 +1,6 @@
 import _isString from './_isString.js';
 
-function nth(offset, list) {
+export default function _nth(offset, list) {
   var idx = offset < 0 ? list.length + offset : offset;
   return _isString(list) ? list.charAt(idx) : list[idx];
 }
-export default nth;
--- source/internal/_path.js
+++ source/internal/_path.js
@@ -1,7 +1,7 @@
 import _isInteger from './_isInteger.js';
 import _nth from './_nth.js';
 
-function path(pathAr, obj) {
+export default function _path(pathAr, obj) {
   var val = obj;
   for (var i = 0; i < pathAr.length; i += 1) {
     if (val == null) {
@@ -17,5 +17,3 @@ function path(pathAr, obj) {
   }
   return val;
 }
-
-export default path;
