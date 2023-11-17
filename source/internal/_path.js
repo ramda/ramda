df30b41d49fecc7edefbebcbcbcae89a01be4ca1
Ridge Kimani <ridgekimani@outlook.com>
2023-11-17 23:06:40 +0300

    chore: reorder null check

--- source/internal/_path.js
+++ source/internal/_path.js
@@ -2,10 +2,12 @@ import prop from '../prop.js';
 
 export default function _path(pathAr, obj) {
   var val = obj;
+
+  if (val == null) {
+    return undefined;
+  }
+
   for (var i = 0; i < pathAr.length; i += 1) {
-    if (val == null) {
-      return undefined;
-    }
     val = prop(pathAr[i], val);
   }
   return val;
