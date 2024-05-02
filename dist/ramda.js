b38bb93412835b5f09ece25a53f864415e81f342
harris-miller <harrismillerconsulting@gmail.com>
2024-05-01 19:11:58 -0600

    npm run build

--- dist/ramda.js
+++ dist/ramda.js
@@ -205,6 +205,49 @@
   }
 
   /**
+   * Optimized internal three-arity curry function.
+   *
+   * @private
+   * @category Function
+   * @param {Function} fn The function to curry.
+   * @return {Function} The curried function.
+   */
+  function _curry3(fn) {
+    return function f3(a, b, c) {
+      switch (arguments.length) {
+        case 0:
+          return f3;
+        case 1:
+          return _isPlaceholder(a) ? f3 : _curry2(function (_b, _c) {
+            return fn(a, _b, _c);
+          });
+        case 2:
+          return _isPlaceholder(a) && _isPlaceholder(b) ? f3 : _isPlaceholder(a) ? _curry2(function (_a, _c) {
+            return fn(_a, b, _c);
+          }) : _isPlaceholder(b) ? _curry2(function (_b, _c) {
+            return fn(a, _b, _c);
+          }) : _curry1(function (_c) {
+            return fn(a, b, _c);
+          });
+        default:
+          return _isPlaceholder(a) && _isPlaceholder(b) && _isPlaceholder(c) ? f3 : _isPlaceholder(a) && _isPlaceholder(b) ? _curry2(function (_a, _b) {
+            return fn(_a, _b, c);
+          }) : _isPlaceholder(a) && _isPlaceholder(c) ? _curry2(function (_a, _c) {
+            return fn(_a, b, _c);
+          }) : _isPlaceholder(b) && _isPlaceholder(c) ? _curry2(function (_b, _c) {
+            return fn(a, _b, _c);
+          }) : _isPlaceholder(a) ? _curry1(function (_a) {
+            return fn(_a, b, c);
+          }) : _isPlaceholder(b) ? _curry1(function (_b) {
+            return fn(a, _b, c);
+          }) : _isPlaceholder(c) ? _curry1(function (_c) {
+            return fn(a, b, _c);
+          }) : fn(a, b, c);
+      }
+    };
+  }
+
+  /**
    * Internal curryN function.
    *
    * @private
@@ -284,10 +327,16 @@
    *      g(4); //=> 10
    */
   var curryN = _curry2(function curryN(length, fn) {
-    if (length === 1) {
-      return _curry1(fn);
+    switch (length) {
+      case 1:
+        return _curry1(fn);
+      case 2:
+        return _curry2(fn);
+      case 3:
+        return _curry3(fn);
+      default:
+        _arity(length, _curryN(length, [], fn));
     }
-    return _arity(length, _curryN(length, [], fn));
   });
 
   /**
@@ -365,49 +414,6 @@
       return fn.apply(this, args);
     });
   });
-
-  /**
-   * Optimized internal three-arity curry function.
-   *
-   * @private
-   * @category Function
-   * @param {Function} fn The function to curry.
-   * @return {Function} The curried function.
-   */
-  function _curry3(fn) {
-    return function f3(a, b, c) {
-      switch (arguments.length) {
-        case 0:
-          return f3;
-        case 1:
-          return _isPlaceholder(a) ? f3 : _curry2(function (_b, _c) {
-            return fn(a, _b, _c);
-          });
-        case 2:
-          return _isPlaceholder(a) && _isPlaceholder(b) ? f3 : _isPlaceholder(a) ? _curry2(function (_a, _c) {
-            return fn(_a, b, _c);
-          }) : _isPlaceholder(b) ? _curry2(function (_b, _c) {
-            return fn(a, _b, _c);
-          }) : _curry1(function (_c) {
-            return fn(a, b, _c);
-          });
-        default:
-          return _isPlaceholder(a) && _isPlaceholder(b) && _isPlaceholder(c) ? f3 : _isPlaceholder(a) && _isPlaceholder(b) ? _curry2(function (_a, _b) {
-            return fn(_a, _b, c);
-          }) : _isPlaceholder(a) && _isPlaceholder(c) ? _curry2(function (_a, _c) {
-            return fn(_a, b, _c);
-          }) : _isPlaceholder(b) && _isPlaceholder(c) ? _curry2(function (_b, _c) {
-            return fn(a, _b, _c);
-          }) : _isPlaceholder(a) ? _curry1(function (_a) {
-            return fn(_a, b, c);
-          }) : _isPlaceholder(b) ? _curry1(function (_b) {
-            return fn(a, _b, c);
-          }) : _isPlaceholder(c) ? _curry1(function (_c) {
-            return fn(a, b, _c);
-          }) : fn(a, b, c);
-      }
-    };
-  }
 
   /**
    * Applies a function to the value at the given index of an array, returning a
