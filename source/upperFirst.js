972cd9c15695fb61a28a695887a013b677dd699f
Nhan Nguyen <nhannt136@fpt.com>
2024-09-06 15:58:47 +0700

    chore: change description

--- source/upperFirst.js
+++ source/upperFirst.js
@@ -2,7 +2,7 @@ import _curry1 from './internal/_curry1.js';
 import _isString from './internal/_isString.js';
 
 /**
- * The upper first case version of a string.
+ * Converts the first character of string to upper case.
  *
  * @func
  * @memberOf R
