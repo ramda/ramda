7ee3d7a3b0f646a9d91c583c93734740fb4c8933
kedashoe <kwallace@gmail.com>
2025-06-22 09:18:57 -0700

    Version 0.31.3

--- dist/ramda.js
+++ dist/ramda.js
@@ -1,4 +1,4 @@
-//  Ramda v0.31.2
+//  Ramda v0.31.3
 //  https://github.com/ramda/ramda
 //  (c) 2013-2025 Scott Sauyet, Michael Hurley, and David Chambers
 //  Ramda may be freely distributed under the MIT license.
