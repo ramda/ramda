dbe0121893a9697295cac0341912fefd722e9311
Hashim Khan <sardarhashim30@gmail.com>
2026-07-23 00:52:17 +0500

    docs: clarify unionWith keeps the first equal item
    Document first-item preference in the same wording as uniqWith and
    update the example so the kept object properties are visible.
    
    Fixes #2901

--- source/unionWith.js
+++ source/unionWith.js
@@ -6,8 +6,8 @@ import uniqWith from './uniqWith.js';
 /**
  * Combines two lists into a set (i.e. no duplicates) composed of the elements
  * of each list. Duplication is determined according to the value returned by
- * applying the supplied predicate to two list elements. If an element exists
- * in both lists, the first element from the first list will be used.
+ * applying the supplied predicate to two list elements. Prefers the first item
+ * if two items compare equal based on the predicate.
  *
  * @func
  * @memberOf R
@@ -22,9 +22,10 @@ import uniqWith from './uniqWith.js';
  * @see R.union
  * @example
  *
- *      const l1 = [{a: 1}, {a: 2}];
- *      const l2 = [{a: 1}, {a: 4}];
- *      R.unionWith(R.eqBy(R.prop('a')), l1, l2); //=> [{a: 1}, {a: 2}, {a: 4}]
+ *      const cmp = (x, y) => x.a === y.a;
+ *      const l1 = [{a: 1, b: 1}, {a: 2}];
+ *      const l2 = [{a: 1, b: 2}, {a: 3}];
+ *      R.unionWith(cmp, l1, l2); //=> [{a: 1, b: 1}, {a: 2}, {a: 3}]
  */
 var unionWith = _curry3(function unionWith(pred, list1, list2) {
   return uniqWith(pred, _concat(list1, list2));
